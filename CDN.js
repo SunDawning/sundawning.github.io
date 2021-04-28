e17c1279b2c302ad918d2faae6dcf44a68e1e28d
SunDawning <dpmeichen@gmail.com>
2021-04-28 08:09:33 +0800

    新增 测试同时使用在线CDN和本地CDN

--- CDN.js
+++ CDN.js
@@ -3,7 +3,7 @@
  * 一个CDN地址即是一个仓库的地址，在这个地址中可以找到JavaScript文件
  * @param input.uri CDN的地址，是一个字符串：
  * - 不填的话，默认使用import引用CDN.js的地址作为CDN的地址。
- * - 可以与import引用的CDN.js的地址不同，
+ * - 可以与import引用的CDN.js的地址不同
  * @return output.import 动态import CDN上的JavaScript文件
  * @returun output.set 替换CDN的地址
  */

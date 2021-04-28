a33a36ca63f434708e87da8513e195008ec4758c
SunDawning <dpmeichen@gmail.com>
2021-04-28 08:14:17 +0800

    优化 第一个参数直接使用uri，不再使用input。

--- CDN.js
+++ CDN.js
@@ -1,16 +1,13 @@
 /**
  * 配置CDN地址，便于动态import CDN上的JavaScript文件
  * 一个CDN地址即是一个仓库的地址，在这个地址中可以找到JavaScript文件
- * @param input.uri CDN的地址，是一个字符串：
+ * @param uri CDN的地址，是一个字符串：
  * - 不填的话，默认使用import引用CDN.js的地址作为CDN的地址。
  * - 可以与import引用的CDN.js的地址不同
  * @return output.import 动态import CDN上的JavaScript文件
  * @returun output.set 替换CDN的地址
  */
-export function CDN(input){
-    if(input===undefined){
-        input={};
-    }
+export function CDN(uri){
     let output={};
     /**
      * 动态import CDN上的JavaScript文件
@@ -28,6 +25,6 @@ export function CDN(input){
             output.uri=uri;
         }
     }
-    output.set(input.uri);
+    output.set(uri);
     return output;
 }

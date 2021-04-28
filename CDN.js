5076b349c1fae87af166949af77758a0a10c92db
SunDawning <dpmeichen@gmail.com>
2021-04-28 08:18:37 +0800

    优化 考虑到JSON的使用，让CDN只接受一个自变量对象，并从中取出uri属性作为CDN的地址，还原到上一次修改的状态。

--- CDN.js
+++ CDN.js
@@ -1,13 +1,16 @@
 /**
  * 配置CDN地址，便于动态import CDN上的JavaScript文件
  * 一个CDN地址即是一个仓库的地址，在这个地址中可以找到JavaScript文件
- * @param uri CDN的地址，是一个字符串：
+ * @param input.uri CDN的地址，是一个字符串：
  * - 不填的话，默认使用import引用CDN.js的地址作为CDN的地址。
  * - 可以与import引用的CDN.js的地址不同
  * @return output.import 动态import CDN上的JavaScript文件
  * @returun output.set 替换CDN的地址
  */
-export function CDN(uri){
+export function CDN(input){
+    if(input===undefined){
+        input={};
+    }
     let output={};
     /**
      * 动态import CDN上的JavaScript文件
@@ -25,6 +28,6 @@ export function CDN(uri){
             output.uri=uri;
         }
     }
-    output.set(uri);
+    output.set(input.uri);
     return output;
 }

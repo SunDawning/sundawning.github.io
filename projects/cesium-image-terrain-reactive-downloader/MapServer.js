d1ee6601fbcc1499875b3723ef2e819c00ff8ae5
SunDawning <dpmeichen@gmail.com>
2021-07-28 21:52:06 +0800

    修复 暂时（<2021-07-28 Wed 21:52:17 UTC+08:00>）移除不能有效起作用的地形下载

--- projects/cesium-image-terrain-reactive-downloader/MapServer.js
+++ projects/cesium-image-terrain-reactive-downloader/MapServer.js
@@ -16,19 +16,6 @@ let fileServer=new FileServer({
             }catch(error){
                 console.log(file,error);
             }
-        }else if(file.search(/1\/(\d*)\/(\d*)\/(\d*).terrain/)>-1){//访问不存在的地形
-            let[match,z,y,x]=file.match(/1\/(\d*)\/(\d*)\/(\d*).terrain/);
-            let headers=new Headers();
-            headers.set("Authorization","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkYTBhN2U4Ny1hODkzLTQzMWItODBjNy0xNzMzZjdkOWU3MzEiLCJpZCI6MjU5LCJhc3NldHMiOnsiMSI6eyJ0eXBlIjoiVEVSUkFJTiIsImV4dGVuc2lvbnMiOlt0cnVlLHRydWUsdHJ1ZV0sInB1bGxBcGFydFRlcnJhaW4iOmZhbHNlfX0sInNyYyI6IjUwMDY3NWFiLTk2OWMtNDZlZC1hYmRkLWNhODg3MDhiMTM0YyIsImlhdCI6MTYyNzQ3NDgzNywiZXhwIjoxNjI3NDc4NDM3fQ.A6QkTvRgioGVBF1miilfCXI0F-7dkovxmj4WnPW3LCk")
-            try{
-                await download(`https://assets.cesium.com/1/${z}/${y}/${x}.terrain?extensions=octvertexnormals-watermask-metadata&v=1.2.0`,file,{headers:headers})
-            }catch(error){
-                console.log(file,error);
-            }
-        }else if(file.search("1/layer.json")>-1){//访问地形的索引文件
-            let headers=new Headers();
-            headers.set("Authorization","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkYTBhN2U4Ny1hODkzLTQzMWItODBjNy0xNzMzZjdkOWU3MzEiLCJpZCI6MjU5LCJhc3NldHMiOnsiMSI6eyJ0eXBlIjoiVEVSUkFJTiIsImV4dGVuc2lvbnMiOlt0cnVlLHRydWUsdHJ1ZV0sInB1bGxBcGFydFRlcnJhaW4iOmZhbHNlfX0sInNyYyI6IjUwMDY3NWFiLTk2OWMtNDZlZC1hYmRkLWNhODg3MDhiMTM0YyIsImlhdCI6MTYyNzQ3NDgzNywiZXhwIjoxNjI3NDc4NDM3fQ.A6QkTvRgioGVBF1miilfCXI0F-7dkovxmj4WnPW3LCk")
-            await download(`https://assets.cesium.com/1/layer.json`,file,{headers:headers})
         }
     }
 });

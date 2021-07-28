fc71eb77398a99aab0af3146e6ebaba2041aa7bb
SunDawning <dpmeichen@gmail.com>
2021-07-28 20:47:40 +0800

    新增 可以离线下载地形

--- projects/cesium-image-terrain-reactive-downloader/MapServer.js
+++ projects/cesium-image-terrain-reactive-downloader/MapServer.js
@@ -6,13 +6,21 @@ import{download}from"./mod.js";
 let fileServer=new FileServer({
     port:8088,
     cors:true,
-    onNotExistsFile:async function(file){
-        //访问不存在的地图
-        if(file.search("World_Imagery/MapServer/tile")>-1){
+    onNotExistsFile:function(file){
+        if(file.search("World_Imagery/MapServer/tile")>-1){//访问不存在的地图
             let[match,z,y,x]=file.match(/World_Imagery\/MapServer\/tile\/(\d*)\/(\d*)\/(\d*)/);
             if(z===undefined){return;}
             if(z>17){return;}
-            await download(`https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/${z}/${y}/${x}`,file);
+            download(`https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/${z}/${y}/${x}`,file);
+        }else if(file.search(/1\/(\d*)\/(\d*)\/(\d*).terrain/)>-1){//访问不存在的地形
+            let[match,z,y,x]=file.match(/1\/(\d*)\/(\d*)\/(\d*).terrain/);
+            let headers=new Headers();
+            headers.set("Authorization","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkYTBhN2U4Ny1hODkzLTQzMWItODBjNy0xNzMzZjdkOWU3MzEiLCJpZCI6MjU5LCJhc3NldHMiOnsiMSI6eyJ0eXBlIjoiVEVSUkFJTiIsImV4dGVuc2lvbnMiOlt0cnVlLHRydWUsdHJ1ZV0sInB1bGxBcGFydFRlcnJhaW4iOmZhbHNlfX0sInNyYyI6IjUwMDY3NWFiLTk2OWMtNDZlZC1hYmRkLWNhODg3MDhiMTM0YyIsImlhdCI6MTYyNzQ3NDgzNywiZXhwIjoxNjI3NDc4NDM3fQ.A6QkTvRgioGVBF1miilfCXI0F-7dkovxmj4WnPW3LCk")
+            download(`https://assets.cesium.com/1/${z}/${y}/${x}.terrain?extensions=octvertexnormals-watermask-metadata&v=1.2.0`,file,{headers:headers})
+        }else if(file.search("1/layer.json")>-1){//访问地形的索引文件
+            let headers=new Headers();
+            headers.set("Authorization","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkYTBhN2U4Ny1hODkzLTQzMWItODBjNy0xNzMzZjdkOWU3MzEiLCJpZCI6MjU5LCJhc3NldHMiOnsiMSI6eyJ0eXBlIjoiVEVSUkFJTiIsImV4dGVuc2lvbnMiOlt0cnVlLHRydWUsdHJ1ZV0sInB1bGxBcGFydFRlcnJhaW4iOmZhbHNlfX0sInNyYyI6IjUwMDY3NWFiLTk2OWMtNDZlZC1hYmRkLWNhODg3MDhiMTM0YyIsImlhdCI6MTYyNzQ3NDgzNywiZXhwIjoxNjI3NDc4NDM3fQ.A6QkTvRgioGVBF1miilfCXI0F-7dkovxmj4WnPW3LCk")
+            download(`https://assets.cesium.com/1/layer.json`,file,{headers:headers})
         }
     }
 });

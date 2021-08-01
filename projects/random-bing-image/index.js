bdb51a31f5b6298e15da3c36f74df9ae55e513be
SunDawning <dpmeichen@gmail.com>
2021-08-01 11:48:27 +0800

    优化 将当日获取到的图片缓存到localStorage

--- projects/random-bing-image/index.js
+++ projects/random-bing-image/index.js
@@ -1,21 +1,27 @@
 async function randomBingImage(){
     let apiURL="https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10";
+    let localStorageKey="bingImages";
+    let data=JSON.parse(window.localStorage.getItem(localStorageKey));
     function randomItem(array){
         return array[Math.floor(Math.random()*array.length)];
     }
-    let corsServers=[
-        /* "https://jsonp.afeld.me/?url=",
-         * "https://api.allorigins.win/raw?url=", */
-        "https://salty-earth-46109.herokuapp.com/",
-        "https://eerovil-cors-proxy.herokuapp.com/",
-        "https://lazyguy-nhl-proxy.herokuapp.com/",
-        "https://cors-anywhere.herokuapp.com/"
-    ];
-    let headers=new window.Headers();
-    headers.set("Origin","origin");
-    let response=await window.fetch(randomItem(corsServers)+apiURL,{headers:headers});
-    let data=await response.json();
-    console.log("data",data);
+    if(data===null){
+        let corsServers=[
+            /* "https://jsonp.afeld.me/?url=",
+             * "https://api.allorigins.win/raw?url=", */
+            "https://salty-earth-46109.herokuapp.com/",
+            "https://eerovil-cors-proxy.herokuapp.com/",
+            "https://lazyguy-nhl-proxy.herokuapp.com/",
+            "https://cors-anywhere.herokuapp.com/"
+        ];
+        let headers=new window.Headers();
+        headers.set("Origin","origin");
+        let response=await window.fetch(randomItem(corsServers)+apiURL,{headers:headers});
+        data=await response.json();
+        if(data){
+            window.localStorage.setItem(localStorageKey,JSON.stringify(data));
+        }        
+    }
     function getImgCreate(options){
         let img=document.createElement("img");
         Object.assign(img,options);

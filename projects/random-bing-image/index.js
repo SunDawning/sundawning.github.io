ca3386014a83e78a72cad98f673efb22fb182f81
SunDawning <dpmeichen@gmail.com>
2021-08-01 12:08:00 +0800

    优化 每隔24小时随即更新localeStorage里的数据

--- projects/random-bing-image/index.js
+++ projects/random-bing-image/index.js
@@ -1,11 +1,12 @@
 async function randomBingImage(){
     let apiURL="https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10";
     let localStorageKey="bingImages";
-    let data=JSON.parse(window.localStorage.getItem(localStorageKey));
+    let bingImages=JSON.parse(window.localStorage.getItem(localStorageKey));
+    if(bingImages===null){bingImages={};}
     function randomItem(array){
         return array[Math.floor(Math.random()*array.length)];
     }
-    if(data===null){
+    if((bingImages.data===null)||((new Date().getTime()-bingImages.update)>24*60*60*1000)){
         let corsServers=[
             /* "https://jsonp.afeld.me/?url=",
              * "https://api.allorigins.win/raw?url=", */
@@ -17,17 +18,18 @@ async function randomBingImage(){
         let headers=new window.Headers();
         headers.set("Origin","origin");
         let response=await window.fetch(randomItem(corsServers)+apiURL,{headers:headers});
-        data=await response.json();
-        if(data){
-            window.localStorage.setItem(localStorageKey,JSON.stringify(data));
-        }        
+        bingImages.data=await response.json();
+        if(bingImages.data){
+            bingImages.update=new Date().getTime();
+            window.localStorage.setItem(localStorageKey,JSON.stringify(bingImages));
+        }
     }
     function getImgCreate(options){
         let img=document.createElement("img");
         Object.assign(img,options);
         return img;
     }
-    return getImgCreate({"src":new window.URL(randomItem(data.images).url,apiURL).href,"data":data});
+    return getImgCreate({"src":new window.URL(randomItem(bingImages.data.images).url,apiURL).href,"data":bingImages.data});
 }
 /**
  * 在当前DOM的后面添加img网页元素

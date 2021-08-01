d96d883ca91743eb2d864f929e8f0c7b753b8557
SunDawning <dpmeichen@gmail.com>
2021-08-01 12:19:18 +0800

    优化 分离出＂getBingImages＂来单独从Bing每日图片的API https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10 处获取数据

--- projects/random-bing-image/index.js
+++ projects/random-bing-image/index.js
@@ -1,24 +1,30 @@
+function randomItem(array){
+    return array[Math.floor(Math.random()*array.length)];
+}
+/**
+ * Bing每日图片的API
+ */
+let bingImagesAPIURL="https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10";
+async function getBingImages(){
+    let corsServers=[
+        /* "https://jsonp.afeld.me/?url=",
+         * "https://api.allorigins.win/raw?url=", */
+        "https://salty-earth-46109.herokuapp.com/",
+        "https://eerovil-cors-proxy.herokuapp.com/",
+        "https://lazyguy-nhl-proxy.herokuapp.com/",
+        "https://cors-anywhere.herokuapp.com/"
+    ];
+    let headers=new window.Headers();
+    headers.set("Origin","origin");
+    let response=await window.fetch(randomItem(corsServers)+bingImagesAPIURL,{headers:headers});
+    return await response.json();
+}
 async function randomBingImage(){
-    let apiURL="https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10";
     let localStorageKey="bingImages";
     let bingImages=JSON.parse(window.localStorage.getItem(localStorageKey));
     if(bingImages===null){bingImages={};}
-    function randomItem(array){
-        return array[Math.floor(Math.random()*array.length)];
-    }
     if((bingImages.data===undefined)||((new Date().getTime()-bingImages.update)>24*60*60*1000)){
-        let corsServers=[
-            /* "https://jsonp.afeld.me/?url=",
-             * "https://api.allorigins.win/raw?url=", */
-            "https://salty-earth-46109.herokuapp.com/",
-            "https://eerovil-cors-proxy.herokuapp.com/",
-            "https://lazyguy-nhl-proxy.herokuapp.com/",
-            "https://cors-anywhere.herokuapp.com/"
-        ];
-        let headers=new window.Headers();
-        headers.set("Origin","origin");
-        let response=await window.fetch(randomItem(corsServers)+apiURL,{headers:headers});
-        bingImages.data=await response.json();
+        bingImages.data=await getBingImages();
         if(bingImages.data){
             bingImages.update=new Date().getTime();
             window.localStorage.setItem(localStorageKey,JSON.stringify(bingImages));
@@ -29,7 +35,7 @@ async function randomBingImage(){
         Object.assign(img,options);
         return img;
     }
-    return getImgCreate({"src":new window.URL(randomItem(bingImages.data.images).url,apiURL).href,"data":bingImages.data});
+    return getImgCreate({"src":new window.URL(randomItem(bingImages.data.images).url,bingImagesAPIURL).href,"data":bingImages.data});
 }
 /**
  * 在当前DOM的后面添加img网页元素

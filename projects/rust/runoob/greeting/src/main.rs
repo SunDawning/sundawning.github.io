19e4cd2cafef5ea2ee3dc9278d56073ad87f9d91
SunDawning <dpmeichen@gmail.com>
2021-08-02 21:05:06 +0800

    新增 Rust基础语法

--- projects/rust/runoob/greeting/src/main.rs
+++ projects/rust/runoob/greeting/src/main.rs
@@ -5,4 +5,20 @@ fn main(){
     let a=1;
     let b=2;
     println!("{},{0},{},{{}}",a,b);
+    let mut c=1;
+    println!("{}",c);
+    c=5;
+    println!("{}",c);
+    let d=2;
+    println!("{}",d);
+    let d=5.5;
+    println!("{}",d);
+    let e:u64=1;
+    println!("{}",e);
+    const F:i32=123;
+    println!("{}",F);
+    let g=5;
+    println!("{}",g);
+    let g=g+10;
+    println!("{}",g);
 }

06f9ea627c942a11706bc37eb6646baa241c219e
SunDawning <dpmeichen@gmail.com>
2021-08-02 20:53:10 +0800

    新增 Rust输出到命令行

--- projects/rust/runoob/greeting/src/main.rs
+++ projects/rust/runoob/greeting/src/main.rs
@@ -1,6 +1,8 @@
 /*
  * Rust 教程 | 菜鸟教程: https://www.runoob.com/rust/rust-tutorial.html
  */
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    let a=1;
+    let b=2;
+    println!("{},{0},{},{{}}",a,b);
 }

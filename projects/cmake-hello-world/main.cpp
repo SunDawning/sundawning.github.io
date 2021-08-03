96c88766252ca42e45fcd9cfa4edd9e8161edefe
SunDawning <dpmeichen@gmail.com>
2021-08-03 22:16:59 +0800

    新增 projects/cmake-hello-world

---
+++ projects/cmake-hello-world/main.cpp
@@ -0,0 +1,7 @@
+// cmake编译c++程序_zhusf的博客-CSDN博客_cmake编译c++: https://blog.csdn.net/u012604810/article/details/79585344
+// "C:\Program Files\CMake\bin\cmake.exe" .
+#include <iostream>
+int main(){
+  std::cout << "拼" << std::endl;
+  return 0;
+}

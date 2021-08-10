ab560738dafa1c345e07a68cc80991a49cb1ad41
SunDawning <dpmeichen@gmail.com>
2021-08-10 21:43:59 +0800

    新增 projects/hello-Tensorflow.js/index.html

---
+++ projects/hello-Tensorflow.js/index.js
@@ -0,0 +1,7 @@
+var a=tf.scalar(2);//创建了一个张量
+var b=tf.tensor([2,2]);//将数组转换为张量
+var c=b.shape;//张量的大小
+var d=tf.zeros([2,2]);//创建特定大小的张量
+a.print();
+tf.tensor([1,2,3]).square().square();
+d.dispose();

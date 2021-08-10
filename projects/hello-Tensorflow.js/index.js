47463b18179b9d5af1148a84c068c0984ba546dc
SunDawning <dpmeichen@gmail.com>
2021-08-10 21:50:55 +0800

    优化 合并f(x)到minimize里

--- projects/hello-Tensorflow.js/index.js
+++ projects/hello-Tensorflow.js/index.js
@@ -1,16 +1,16 @@
-// f（x）=x⁶+2x⁴+3x²+ x + 1
-function f(x){
-    return x.pow(tf.scalar(6,"int32")).add(
-        x.pow(tf.scalar(4,"int32")).mul(tf.scalar(2))
-    ).add(
-        x.pow(tf.scalar(2,"int32")).mul(tf.scalar(3))
-    ).add(
-        x
-    ).add(
-        tf.scalar(1)
-    );
-}
 function minimize(epochs,learningRate){
+    // f（x）=x⁶+2x⁴+3x²+ x + 1
+    function f(x){
+        return x.pow(tf.scalar(6,"int32")).add(
+            x.pow(tf.scalar(4,"int32")).mul(tf.scalar(2))
+        ).add(
+            x.pow(tf.scalar(2,"int32")).mul(tf.scalar(3))
+        ).add(
+            x
+        ).add(
+            tf.scalar(1)
+        );
+    }
     var y=tf.variable(tf.scalar(0));
     var optim=tf.train.adam(learningRate);
     for(var c=0;c<epochs;c=c+1){

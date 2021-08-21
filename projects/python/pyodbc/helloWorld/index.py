77c336a1c7467037e869dc927717b1f89d779758
SunDawning <dpmeichen@gmail.com>
2021-08-21 08:33:12 +0800

    新增 在windows下生成可执行exe文件

--- projects/python/pyodbc/helloWorld/index.py
+++ projects/python/pyodbc/helloWorld/index.py
@@ -1,9 +1,13 @@
-# python pyodbcä½¿ç¨æ¹æ³ https://blog.csdn.net/huwei2003/article/details/107387919
+# python pyodbc https://blog.csdn.net/huwei2003/article/details/107387919
+# https://blog.csdn.net/naichager/article/details/111378060
+# pyinstaller -F index.py
 import pyodbc
-print(pyodbc.drivers())
+print("支持的数据库：{}".format(pyodbc.drivers()))
 # ['SQL Server', 'SQL Server Native Client 11.0', 'ODBC Driver 17 for SQL Server', 'SQL Server Native Client RDA 11.0']
 connection=pyodbc.connect("Driver={SQL Server Native Client 11.0};Server=localhost;Database=master;UID=test;PwD=test")
 cursor=connection.cursor()
 cursor.execute("select * from [master].[dbo].[spt_fallback_db]")
 row=cursor.fetchone()
-print(row)
+print("第一行数据：{}".format(row))
+import os
+os.system("pause")

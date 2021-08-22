65cb568f7010cb327cae0604b903be81e02279d2
SunDawning <dpmeichen@gmail.com>
2021-08-22 09:48:16 +0800

    优化 整理出class SQLServer来连接SQL Server数据库、查询和处理所查到的数据

--- projects/python/pyodbc/loopQuery/index.py
+++ projects/python/pyodbc/loopQuery/index.py
@@ -28,26 +28,48 @@ def readFile(path):
         f.close()
     return content
 """
-连接SQL Server数据库
+连接SQL Server数据库、查询和处理所查到的数据
+sqlServer=SQLServer()
+sqlServer.connect()
 """
-def connect():
-    import pyodbc
-    print("Supported Drivers: {}".format(pyodbc.drivers()))
-    connection=pyodbc.connect("Driver={SQL Server};Server=localhost;Database=master;UID=test;PwD=test")
-    cursor=connection.cursor()
-    return cursor
-"""
-查询数据库
-"""
-def query(cursor,sql):
-    cursor.execute(sql)
-    rows=cursor.fetchall()
-    return rows
-"""
-生成SQL语句
-"""
-def getSQL(updateTime):
-    return "select * from [master].[dbo].[spt_fallback_db]"
+class SQLServer:
+    def __init__(SELF,driver="SQL Server",server="localhost",database="master",uid="sa",pwd="test"):
+        SELF.connectString="Driver={{{}}};Server={};Database={};UID={};PWD={}".format(driver,server,database,uid,pwd)
+        SELF.columns=[]
+        SELF.json=[]
+    """
+    连接SQL Server数据库
+    """
+    def connect(SELF):
+        import pyodbc
+        SELF.connection=pyodbc.connect(SELF.connectString)
+        SELF.cursor=SELF.connection.cursor()
+    """
+    查询数据库
+    """
+    def query(SELF,sql):
+        SELF.cursor.execute(sql)
+        SELF.rows=SELF.cursor.fetchall()
+    """
+    生成SQL语句
+    """
+    def getSQL(SELF,updateTime):
+        return "select * from [master].[dbo].[spt_fallback_db]"
+    """
+    获取所有列的名字
+    https://exceptionshub.com/output-pyodbc-cursor-results-as-python-dictionary.html
+    """
+    def getColumns(SELF):
+        for column in SELF.cursor.description:
+            SELF.columns.append(column[0])
+        return SELF.columns
+    """
+    多行转成JSON对象
+    """
+    def rowsToJSON(SELF):
+        for row in SELF.rows:
+            SELF.json.append(dict(zip(SELF.columns,row)))
+        return SELF.json
 """
 上传数据
 """
@@ -55,23 +77,7 @@ def upload(data):
     import requests
     response=requests.post(url="https://httpbin.org/post",headers={"Content-Type":"application/json"},data=data)
     return response.text
-"""
-获取所有列的名字
-https://exceptionshub.com/output-pyodbc-cursor-results-as-python-dictionary.html
-"""
-def getColumns(cursor):
-    output=[]
-    for column in cursor.description:
-        output.append(column[0])
-    return output
-"""
-多行转成JSON对象
-"""
-def rowsToJSON(columns,rows):
-    output=[]
-    for row in rows:
-        output.append(dict(zip(columns,row)))
-    return output
+
 
 UPDATETIMEFILE="./updateTime.txt"
 if isFileExists(UPDATETIMEFILE)==False:
@@ -79,14 +85,16 @@ if isFileExists(UPDATETIMEFILE)==False:
 
 UPDATETIME=readFile(UPDATETIMEFILE)
 print("Previous updateTime: {}".format(UPDATETIME))
-CURSOR=connect()
-print("CURSOR: {}".format(CURSOR))
-rows=query(CURSOR,getSQL(UPDATETIME))
-print("columns: {}".format(getColumns(CURSOR)))
-print("Query Data: {}".format(rowsToJSON(getColumns(CURSOR),rows)))
+sqlServer=SQLServer()
+sqlServer.connectString="Driver={SQL Server Native Client 11.0};Server=localhost;Database=master;UID=test;PwD=test"
+sqlServer.connect()
+print("CURSOR: {}".format(sqlServer.cursor))
+rows=sqlServer.query(sqlServer.getSQL(UPDATETIME))
+print("columns: {}".format(sqlServer.getColumns()))
+print("Query Data: {}".format(sqlServer.rowsToJSON()))
 def loop():
     global UPDATETIME
-    data=query(CURSOR,getSQL(UPDATETIME))
+    data=sqlServer.query(sqlServer.getSQL(UPDATETIME))
     if len(data)==0:
         import threading
         threading.Timer(3,loop).start()
@@ -99,4 +107,4 @@ def loop():
         writeToFile(UPDATETIMEFILE,UPDATETIME)
         import threading
         threading.Timer(3,loop).start()
-loop()
+# loop()

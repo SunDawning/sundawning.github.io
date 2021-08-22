187271aa58b8733e2dca00b5ef727943f0b9006b
SunDawning <dpmeichen@gmail.com>
2021-08-22 08:54:33 +0800

    新增 new file:   projects/python/class/index.py

---
+++ projects/python/class/index.py
@@ -0,0 +1,12 @@
+"""
+Python 类和对象: https://www.w3school.com.cn/python/python_classes.asp
+"""
+class Foo:
+    def __init__(SELF):
+        SELF.a=1
+    def b(SELF):
+        return SELF.a
+foo=Foo()
+foo.a=2
+print(foo)
+print(foo.b())

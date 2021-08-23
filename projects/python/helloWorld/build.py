f1a252a5a6bfc18825c57d7095f0916fc9a7e385
SunDawning <dpmeichen@gmail.com>
2021-08-23 21:00:23 +0800

    优化 index.py自带生成可执行文件的功能

--- projects/python/helloWorld/build.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-users=[
-    "sgs",
-    "simi"
-]
-for user in users:
-    import subprocess
-    subprocess.run(["pyinstaller","-F","{}.py".format(user)])
--- projects/python/helloWorld/index.py
+++ projects/python/helloWorld/index.py
@@ -1,4 +1,54 @@
+"""
+将字符串写到文件里
+"""
+def writeToFile(path,string):
+    f=open(path,"w")
+    f.write(string)
+    f.close()
+    return
+"""
+使用上面的功能
+"""
 def index(options):
-    print("Hello, {}".format(options["name"]))
+    print("Hello, {}".format(options["say"]))
     import os
     os.system("pause")
+    return
+"""
+生成可执行文件等
+"""
+def build():
+    writeToFile("build.bat","python index.py")
+    users=[
+        {
+            "name":"sgs",
+            "config":{
+                "say":"sgs"
+            }
+        },
+        {
+            "name":"simi",
+            "config":{
+                "say":"simi"
+            }
+        }
+    ]
+    version="0.0.2"
+    for user in users:
+        name=user["name"]
+        print("name {}".format(name))
+        path="{}.py".format(name)
+        print("path {}".format(path))
+        config=user["config"]
+        print("config {}".format(config))
+        string="""
+from index import *
+index({})
+    """.format(config)
+        print("string {}".format(string))
+        writeToFile(path,string)
+        exeName="HelloWorld-v{}-win10-64-{}".format(version,name)
+        import subprocess
+        subprocess.Popen(["pyinstaller","-F","--name",exeName,path.format(user)])
+if __name__=="__main__":
+    build()

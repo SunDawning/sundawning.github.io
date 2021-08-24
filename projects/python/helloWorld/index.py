9808ccead4f5a0846588f43b47d41d5e55d09933
SunDawning <dpmeichen@gmail.com>
2021-08-24 19:36:31 +0800

    优化 替换build为make，即只生成代码，但不生成EXE。

--- projects/python/helloWorld/index.py
+++ projects/python/helloWorld/index.py
@@ -1,9 +1,11 @@
+def formatTime(datetime):
+    return datetime.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
 """
 获取当前时间
 """
 def now():
-    import time
-    return time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
+    import datetime
+    return formatTime(datetime.datetime.now())
 """
 将字符串写到文件里
 """
@@ -27,6 +29,19 @@ def readFile(path):
 def isFileExists(path):
     import os
     return os.path.exists(path)
+def mkdir(directory):
+    """
+    创建文件夹
+    """
+    if(isFileExists(directory)==False):
+        import os
+        os.mkdir(directory)
+        pass
+    return
+def copyFile(source,destination):
+    import shutil
+    shutil.copyfile(source,destination)
+    return
 """
 使用上面的功能
 """
@@ -39,18 +54,6 @@ def index(options):
 """
 生成源代码
 """
-def buildSource():
-    content=readFile("./index.py")
-    if(isFileExists("build")==False):
-        import os
-        os.mkdir("build")
-        pass
-    if(isFileExists("./build/source")==False):
-        import os
-        os.mkdir("./build/source")
-        pass
-    writeToFile("./build/source/index.py",content)
-    return
 
 """
 生成可执行文件等：
@@ -59,8 +62,15 @@ def buildSource():
 - 生成build-v{version}-{os}-{machine}-{name}.bat
 - 生成HelloWorld-v{version}-{os}-{machine}-{name}.exe
 """
-def build():
-    writeToFile("build.bat","python index.py")
+def make():
+    version="0.0.9"
+    buildSourceDirectory="./build/source/{}".format(version)
+    mkdir("./build")
+    mkdir("./build/source")
+    mkdir(buildSourceDirectory)
+    copyFile("./index.py","{}/index.py".format(buildSourceDirectory))
+    ico="index.ico"
+    copyFile(ico,"{}/{}".format(buildSourceDirectory,ico))
     users=[
         {
             "name":"sgs",
@@ -74,16 +84,14 @@ def build():
             "config":'{"say":"simi","timestamp":now()}'
         }
     ]
-    version="0.0.8"
     import platform
     os=platform.platform()
     machine=platform.machine()
-    ico="index.ico"
     for user in users:
         name=user["name"]
         print("name {}".format(name))
-        path="{}.py".format(name)
-        print("path {}".format(path))
+        pyScript="{}.py".format(name)
+        print(".py {}".format(pyScript))
         config=user["config"]
         print("config {}".format(config))
         import json
@@ -92,34 +100,28 @@ from index import *
 index({})
 """.format(config)
         print("string {}".format(string))
-        writeToFile(path,string)
+        writeToFile("{}/{}".format(buildSourceDirectory,pyScript),string)
         exeName="HelloWorld-v{}-{}-{}-{}".format(version,os,machine,name)
         print("exeName {}.exe".format(exeName))
-        cmd=["pyinstaller","-F","--name",exeName,"-i",ico,path]
+        cmd=["pyinstaller","-F","--name",exeName,"-i",ico,pyScript]
         print("cmd {}".format(cmd))
-        import subprocess
-        subprocess.Popen(cmd)
-        bat="build-v{}-{}-{}-{}.bat".format(version,os,machine,name)
+        bat="{}/build-{}-{}-{}.bat".format(buildSourceDirectory,os,machine,name)
         print("bat {}".format(bat))
         cmdString=" ".join(cmd)
         print("cmd string {}".format(cmdString))
         writeToFile(bat,cmdString)
+        continue
     return
 """
 以命令行的方式使用
 """
 def main():
-    import sys
-    arguments=sys.argv
-    print("sys.argv: {}".format(arguments))
-    if("buildSource" in arguments):
-        buildSource()
-        print("builded source")
-    else:
-        print("""
-buildSource build source directory
-        """)
-    return
+    try:
+        make()
+    except Exception as error:
+        print(error)
+        import os
+        os.system("pause")
 if(__name__=="__main__"):
     main()
-        
+

a1a56dcb28d2da1663b6244715d98c7b3ee7f816
SunDawning <dpmeichen@gmail.com>
2021-08-26 19:44:46 +0800

    优化 重命名＂Loop＂为＂ThreadLoop＂

--- projects/python/threading/index.py
+++ projects/python/threading/index.py
@@ -1,29 +1,29 @@
-class Loop:
+class ThreadLoop:
     """
     一个循环
     - 使用多线程来执行
     - 可以停止或开始
     """
     def __init__(self,function=False):
-        self.inWhile=True
+        self.enabled=True
         self.timer=False
         self.function=function
         pass
     def start(self):
-        self.inWhile=True
-        def childThreading():
-            while self.inWhile==True:
+        self.enabled=True
+        def loop():
+            while self.enabled==True:
                 if self.function:
                     self.function()
                     pass
                 continue
             return
         import threading
-        self.timer=threading.Timer(0,childThreading)
+        self.timer=threading.Timer(0,loop)
         self.timer.start()
         return
     def stop(self):
-        self.inWhile=False
+        self.enabled=False
         self.timer.cancel()
         return
     pass
@@ -32,6 +32,6 @@ def foo():
     import time
     time.sleep(1)
     return
-loop=Loop(foo)
-loop.start()
-loop.stop()
+threadLoop=ThreadLoop(foo)
+threadLoop.start()
+threadLoop.stop()

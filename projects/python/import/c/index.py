9877d111dfedb5b437e566e7941ee4361937fe0e
SunDawning <dpmeichen@gmail.com>
2021-08-31 20:51:36 +0800

    优化 from sys import path as sysPath

--- projects/python/import/c/index.py
+++ projects/python/import/c/index.py
@@ -3,11 +3,11 @@ def addToImportPath(path):
     将.py添加到模块路径里
     Import Python Script Into Another? - Stack Overflow: https://stackoverflow.com/questions/15696461/import-python-script-into-another
     """
-    import sys
-    if((path in sys.path)==False):
-        sys.path.append(path)
+    from sys import path as sysPath
+    if((path in sysPath)==False):
+        sysPath.append(path)
         pass
-    return sys.path
+    return sysPath
 addToImportPath("..")
 from a.index import fromA
 fromA()

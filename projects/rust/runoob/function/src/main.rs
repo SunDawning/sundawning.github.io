3ed403ec9b475c8b912c917934c6c26332f9b964
SunDawning <dpmeichen@gmail.com>
2021-09-02 22:24:55 +0800

    新增 使用chrono来让＂projects/rust/runoob/function＂项目能输出带有时间戳的信息

--- projects/rust/runoob/function/src/main.rs
+++ projects/rust/runoob/function/src/main.rs
@@ -1,3 +1,24 @@
+extern crate chrono;
+/**
+ * rust - Format std::time output - Stack Overflow: https://stackoverflow.com/questions/38957718/format-stdtime-output
+ * https://crates.io/crates/chrono
+ * format in std - Rust: https://doc.rust-lang.org/std/macro.format.html
+ * 关于rust：将本地字符串作为切片返回(＆str) | 码农家园: https://www.codenong.com/29428227/
+ * chrono::format::strftime - Rust: https://docs.rs/chrono/0.4.19/chrono/format/strftime/index.html#specifiers
+ */
+fn now()->String{
+    return format!("{}",chrono::Local::now().format("%Y-%m-%d %H:%M:%S%.3f"));
+}
+/**
+ * Rust 输出到命令行 | 菜鸟教程: https://www.runoob.com/rust/rust-println.html
+ */
+fn print_time(string:String){
+    println!("[{}] {}",now(),string);
+}
+fn launch(){
+    print_time(format!("{}","程序启动"));
+    print_time(format!("当前时间：{}",now()))
+}
 fn main() {
-    println!("函数");
+    launch();
 }

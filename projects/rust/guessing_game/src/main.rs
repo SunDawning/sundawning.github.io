316aa69e135f8b2f717c7706fbd6bf8eeb44e9a2
SunDawning <dpmeichen@gmail.com>
2021-09-04 15:20:21 +0800

    新增 版本＂0.1.2＂：得到1到100的随机数。

--- projects/rust/guessing_game/src/main.rs
+++ projects/rust/guessing_game/src/main.rs
@@ -1,6 +1,9 @@
 use std::io;
+use rand::Rng;
 fn main() {
     println!("开始猜数字游戏");
+    let secret_number=rand::thread_rng().gen_range(1..101);
+    println!("密码是：{}",secret_number);
     println!("请输入任意一个数字");
     let mut guess=String::new();
     io::stdin().read_line(&mut guess).expect("无法读取当前输入");

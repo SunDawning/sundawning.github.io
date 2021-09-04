dfce7259b53a55f7d8ebc916113f41afd4e18f3c
SunDawning <dpmeichen@gmail.com>
2021-09-04 15:34:22 +0800

    新增 版本＂0.1.3＂：可以猜一次数字

--- projects/rust/guessing_game/src/main.rs
+++ projects/rust/guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
     println!("开始猜数字游戏");
     let secret_number=rand::thread_rng().gen_range(1..101);
@@ -7,5 +8,11 @@ fn main() {
     println!("请输入任意一个数字");
     let mut guess=String::new();
     io::stdin().read_line(&mut guess).expect("无法读取当前输入");
+    let guess:u32=guess.trim().parse().expect("无法处理当前的输入");
     println!("已输入数字：{}",guess);
+    match guess.cmp(&secret_number){
+	Ordering::Less=>println!("猜小了"),
+	Ordering::Greater=>println!("猜大了"),
+	Ordering::Equal=>println!("猜对了"),
+    }
 }

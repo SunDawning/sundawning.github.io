40f63f9b12036d6ca9e2e35696ee966dc48e3ec9
SunDawning <dpmeichen@gmail.com>
2021-09-04 15:45:08 +0800

    新增 版本＂0.1.6＂：没有输入数字时，处理无效输入，提示＂无法处理当前的输入＂。

--- projects/rust/guessing_game/src/main.rs
+++ projects/rust/guessing_game/src/main.rs
@@ -9,7 +9,13 @@ fn main() {
 	println!("请输入任意一个数字");
 	let mut guess=String::new();
 	io::stdin().read_line(&mut guess).expect("无法读取当前输入");
-	let guess:u32=guess.trim().parse().expect("无法处理当前的输入");
+	let guess:u32 = match guess.trim().parse(){
+	    Ok(num)=>num,
+	    Err(_)=>{
+		println!("无法处理当前的输入");
+		continue;
+	    },
+	};
 	println!("已输入数字：{}",guess);
 	match guess.cmp(&secret_number){
 	    Ordering::Less=>println!("猜小了"),

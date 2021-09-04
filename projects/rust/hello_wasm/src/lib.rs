2227c8d5843cb92abcb861bc96d1e6089baae192
SunDawning <dpmeichen@gmail.com>
2021-09-04 16:22:20 +0800

    新增 版本＂0.1.1＂：wasm-pack build

--- projects/rust/hello_wasm/src/lib.rs
+++ projects/rust/hello_wasm/src/lib.rs
@@ -1,7 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+// 编译 Rust 为 WebAssembly - WebAssembly | MDN: https://developer.mozilla.org/zh-CN/docs/WebAssembly/Rust_to_wasm
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+#[wasm_bindgen]
+extern{
+    pub fn alert(s:&str);
+}
+#[wasm_bindgen]
+pub fn greet(name:&str){
+    alert(&format!("Hello,{}",name));
 }

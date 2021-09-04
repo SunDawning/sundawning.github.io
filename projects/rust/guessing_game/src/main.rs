848370f1adf9d764bce2978b3ceb91a16d046a2f
SunDawning <dpmeichen@gmail.com>
2021-09-04 14:58:39 +0800

    新增 版本＂0.1.1＂：从键盘获取输入并打印出来。

--- projects/rust/guessing_game/src/main.rs
+++ projects/rust/guessing_game/src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("开始猜数字游戏");
+    println!("请输入任意一个数字");
+    let mut guess=String::new();
+    io::stdin().read_line(&mut guess).expect("无法读取当前输入");
+    println!("已输入数字：{}",guess);
 }

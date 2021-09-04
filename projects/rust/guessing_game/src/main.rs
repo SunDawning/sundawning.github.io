997511771c658d101f77d3be2771fe2e13d36c4d
SunDawning <dpmeichen@gmail.com>
2021-09-04 15:59:03 +0800

    新增 版本＂1.0.1＂：替换Tab缩进为空格缩进

--- projects/rust/guessing_game/src/main.rs
+++ projects/rust/guessing_game/src/main.rs
@@ -6,24 +6,24 @@ fn main() {
     let secret_number=rand::thread_rng().gen_range(1..101);
     // println!("密码是：{}",secret_number);
     loop{
-	println!("请输入任意一个数字");
-	let mut guess=String::new();
-	io::stdin().read_line(&mut guess).expect("无法读取当前输入");
-	let guess:u32 = match guess.trim().parse(){
-	    Ok(num)=>num,
-	    Err(_)=>{
-		println!("无法处理当前的输入");
-		continue;
-	    },
-	};
-	println!("已输入数字：{}",guess);
-	match guess.cmp(&secret_number){
-	    Ordering::Less=>println!("猜小了"),
-	    Ordering::Greater=>println!("猜大了"),
-	    Ordering::Equal=>{
-		println!("猜对了");
-		break;
-	    },
-	}
+        println!("请输入任意一个数字");
+        let mut guess=String::new();
+        io::stdin().read_line(&mut guess).expect("无法读取当前输入");
+        let guess:u32 = match guess.trim().parse(){
+            Ok(num)=>num,
+            Err(_)=>{
+                println!("无法处理当前的输入");
+                continue;
+            },
+        };
+        println!("已输入数字：{}",guess);
+        match guess.cmp(&secret_number){
+            Ordering::Less=>println!("猜小了"),
+            Ordering::Greater=>println!("猜大了"),
+            Ordering::Equal=>{
+                println!("猜对了");
+                break;
+            },
+        }
     }
 }

688507ebebb1564cb0ec41c650dfe8d41ae4b77c
SunDawning <dpmeichen@gmail.com>
2021-09-13 20:11:22 +0800

    新增 可以计算一个项目的Git提交记录数量

--- projects/rust/git_commit_count/src/main.rs
+++ projects/rust/git_commit_count/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Git Commit Count!");
+    let args="rev-list --all --count".split(" ");
+    let current_dir="c:/Users/sgs/AppData/Roaming/literate-programming";
+    let command_output=std::process::Command::new("git").args(args).current_dir(current_dir).output().expect("Error: Get git commit count.");
+    let command_string=String::from_utf8_lossy(&command_output.stdout);
+    let count:u32=command_string.trim().parse().expect("Error: parse.");
+    println!("{}",count);
 }

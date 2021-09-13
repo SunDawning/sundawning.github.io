c0aa08688d7a8552ce8da2f8146e7a70ac2b7089
SunDawning <dpmeichen@gmail.com>
2021-09-13 20:55:29 +0800

    优化 使用＂../print_time＂模块来输出带有时间的内容

--- projects/rust/git_commit_count/src/main.rs
+++ projects/rust/git_commit_count/src/main.rs
@@ -1,9 +1,10 @@
+use print_time::print_time;
 fn main() {
-    println!("Welcome to Git Commit Count!");
+    print_time(format!("Welcome to Git Commit Count!"));
     let args="rev-list --all --count".split(" ");
     let current_dir="c:/Users/sgs/AppData/Roaming/literate-programming";
     let command_output=std::process::Command::new("git").args(args).current_dir(current_dir).output().expect("Error: Get git commit count.");
     let command_string=String::from_utf8_lossy(&command_output.stdout);
     let count:u32=command_string.trim().parse().expect("Error: parse.");
-    println!("{}",count);
+    print_time(format!("{}",count));
 }

1d180b5072c2af4b4ea0ed374402b28acebc9add
SunDawning <dpmeichen@gmail.com>
2021-09-13 21:20:21 +0800

    新增 可以查询多个Git仓库的提交记录

--- projects/rust/git_commit_count/src/main.rs
+++ projects/rust/git_commit_count/src/main.rs
@@ -1,10 +1,15 @@
 use print_time::print_time;
-fn main() {
+fn main(){
     print_time(format!("Welcome to Git Commit Count!"));
-    let args="rev-list --all --count".split(" ");
-    let current_dir="c:/Users/sgs/AppData/Roaming/literate-programming";
-    let command_output=std::process::Command::new("git").args(args).current_dir(current_dir).output().expect("Error: Get git commit count.");
-    let command_string=String::from_utf8_lossy(&command_output.stdout);
-    let count:u32=command_string.trim().parse().expect("Error: parse.");
-    print_time(format!("{}",count));
+    let args=["rev-list","--all","--count"];
+    let repositories=[
+	"c:/Users/sgs/AppData/Roaming/literate-programming",
+	"."
+    ];
+    for current_dir in repositories.iter(){
+	let command_output=std::process::Command::new("git").args(args).current_dir(current_dir).output().expect("Error: Get git commit count.");
+	let command_string=String::from_utf8_lossy(&command_output.stdout);
+	let count:u32=command_string.trim().parse().expect("Error: parse.");
+	print_time(format!("{} {}",count,current_dir));
+    }
 }

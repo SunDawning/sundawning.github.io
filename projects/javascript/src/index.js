5c7d300ee1bfdc2e9121c1adaac91a89fb047159
SunDawning <dpmeichen@gmail.com>
2021-09-14 21:35:14 +0800

    新增 await import

--- projects/javascript/src/index.js
+++ projects/javascript/src/index.js
@@ -1,3 +1,8 @@
-import{Scene}from"three";
-let scene=new Scene();
-document.write(JSON.stringify(scene));
+import"regenerator-runtime/runtime";
+async function init(){
+    let{Scene}=await import("three");
+    let scene=new Scene();
+    document.write(JSON.stringify(scene));
+}
+init();
+

68d6c383315b2c7c861fd9ae71deba97262bec1b
SunDawning <dpmeichen@gmail.com>
2021-09-16 08:45:37 +0800

    新增 一些常见的print语句

--- projects/Rust-Programming-Cookbook/hello-world/src/main.rs
+++ projects/Rust-Programming-Cookbook/hello-world/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    println!("下面开始显示几行内容：");
+    println!();
+    println!("欢迎你进入Rust的世界");
+    println!("再次欢迎{}进入{}的世界","你","Rust");
+    print!("Rust世界欢迎");
+    println!("你");
 }

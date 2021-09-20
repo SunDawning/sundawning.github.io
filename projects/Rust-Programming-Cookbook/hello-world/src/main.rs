d40299552b85cc1be82b06b9bb8e2406860bb1e5
SunDawning <dpmeichen@gmail.com>
2021-09-20 10:32:45 +0800

    新增 println!输出到数据流

--- projects/Rust-Programming-Cookbook/hello-world/src/main.rs
+++ projects/Rust-Programming-Cookbook/hello-world/src/main.rs
@@ -5,4 +5,12 @@ fn main() {
     println!("再次欢迎{}进入{}的世界","你","Rust");
     print!("Rust世界欢迎");
     println!("你");
+    // 更多组合
+    println!("根据位置：{1},{0}；{0},{1}。","Rust","世界");
+    println!("别名：name={name},place={place}",name="Rust",place="世界");
+    println!("数字PI的值是{0:.100}，{0:.2}",std::f64::consts::PI);
+    println!("更多数字：{0:>0距离$}={0:>距离$}={0:#x}",1535,距离=6);
+    use std::io::{self,Write};
+    let _=write!(&mut io::stdout(),"写入数据流");
+    println!();
 }

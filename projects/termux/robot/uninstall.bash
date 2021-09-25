7b8af5917705a8332e3863a6f9465dfa975ad1bd
SunDawning <dpmeichen@gmail.com>
2021-09-25 18:54:53 +0800

    优化 使用函数重写uninstall.bash

--- projects/termux/robot/uninstall.bash
+++ projects/termux/robot/uninstall.bash
@@ -1,6 +1,15 @@
-echo "removing ~/index.bash";
-rm ~/index.bash;
-echo "removing ~/index.js";
-rm ~/index.js;
-echo "deleting reference in ~/.bashrc";
-sed -ie '/bash ~\/index.bash/d' ~/.bashrc
+function remove_index_bash(){
+    echo "removing ~/index.bash";
+    rm ~/index.bash;
+    echo "removing ~/index.js";
+    rm ~/index.js;
+}
+function remove_reference_index_bash(){
+    echo "deleting reference in ~/.bashrc";
+    sed -ie '/bash ~\/index.bash/d' ~/.bashrc
+}
+function index(){
+    remove_reference_index_bash;
+    remove_index_bash;
+}
+index;

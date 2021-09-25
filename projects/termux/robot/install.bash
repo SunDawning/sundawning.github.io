a7d5a300b907bb9ff7e7d1580fd63257c69fac52
SunDawning <dpmeichen@gmail.com>
2021-09-25 17:50:30 +0800

    新增 projects/termux/robot/install.bash

---
+++ projects/termux/robot/install.bash
@@ -0,0 +1,3 @@
+curl https://gitee.com/sundawning/sundawning.gitee.io/raw/master/projects/termux/robot/index.bash -o ~/index.bash
+curl https://gitee.com/sundawning/sundawning.gitee.io/raw/master/projects/termux/robot/index.js -o ~/index.js
+echo "bash ~/index.bash" >> ~/.bashrc

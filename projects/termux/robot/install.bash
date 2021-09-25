69617e592ad5d36358e5d7b1684d2bb81571d8fc
SunDawning <dpmeichen@gmail.com>
2021-09-25 18:10:29 +0800

    修复 确保能创建~/.bashrc

--- projects/termux/robot/install.bash
+++ projects/termux/robot/install.bash
@@ -1,3 +1,15 @@
 curl https://gitee.com/sundawning/sundawning.gitee.io/raw/master/projects/termux/robot/index.bash -o ~/index.bash
 curl https://gitee.com/sundawning/sundawning.gitee.io/raw/master/projects/termux/robot/index.js -o ~/index.js
-echo "bash ~/index.bash" >> ~/.bashrc
+if test -z "$(find ~ -name '.bashrc')";then
+    echo "Not exist ~/.bashrc";
+    echo "" > ~/.bashrc;
+else
+    echo "Exist ~/.bashrc";
+fi
+if test -z "$(grep 'bash ~/index.bash' ~/.bashrc)";then
+    echo "Not Install";
+    echo "Installing";
+    echo "bash ~/index.bash" >> ~/.bashrc;
+else
+    echo "Installed ~/index.bash";
+fi

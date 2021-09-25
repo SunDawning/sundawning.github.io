db9a530a8fc92f29ab8f6a4cc81deced616e34bd
SunDawning <dpmeichen@gmail.com>
2021-09-25 18:52:31 +0800

    优化 使用函数来写install.bash

--- projects/termux/robot/install.bash
+++ projects/termux/robot/install.bash
@@ -1,16 +1,28 @@
-echo "Downloading ~/index.bash";
-curl https://gitee.com/sundawning/sundawning.gitee.io/raw/master/projects/termux/robot/index.bash -o ~/index.bash
-echo "Downloading ~/index.js";
-curl https://gitee.com/sundawning/sundawning.gitee.io/raw/master/projects/termux/robot/index.js -o ~/index.js
-if test -z "$(find ~ -name '.bashrc')";then
-    echo "Not exist ~/.bashrc";
-    echo "Creating ~/.bashrc";
-    echo "" > ~/.bashrc;
-fi
-echo "Existed ~/.bashrc";
-if test -z "$(grep 'bash ~/index.bash' ~/.bashrc)";then
-    echo "Not Install reference in ~/.bashrc";
-    echo "Installing reference in ~/.bashrc";
-    echo "bash ~/index.bash" >> ~/.bashrc;
-fi
-echo "Installed reference in ~/.bashrc";
+function ensure_bashrc(){
+    if test -z "$(find ~ -name '.bashrc')";then
+	echo "Not exist ~/.bashrc";
+	echo "Creating ~/.bashrc";
+	echo "" > ~/.bashrc;
+    fi
+    echo "Existed ~/.bashrc";
+}
+function ensure_reference_index_bash(){
+    if test -z "$(grep 'bash ~/index.bash' ~/.bashrc)";then
+	echo "Not Install reference in ~/.bashrc";
+	echo "Installing reference in ~/.bashrc";
+	echo "bash ~/index.bash" >> ~/.bashrc;
+    fi
+    echo "Installed reference in ~/.bashrc";
+}
+function download_latest_index_bash(){
+    echo "Downloading ~/index.bash";
+    curl https://gitee.com/sundawning/sundawning.gitee.io/raw/master/projects/termux/robot/index.bash -o ~/index.bash
+    echo "Downloading ~/index.js";
+    curl https://gitee.com/sundawning/sundawning.gitee.io/raw/master/projects/termux/robot/index.js -o ~/index.js
+}
+function index(){
+    ensure_bashrc;
+    ensure_reference_index_bash;
+    download_latest_index_bash;    
+}
+index;

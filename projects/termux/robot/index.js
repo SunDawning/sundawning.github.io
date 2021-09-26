ad5baf9ba986bbfe2ce68b03fff15646be4cd16d
SunDawning <dpmeichen@gmail.com>
2021-09-26 20:02:06 +0800

    新增 termux-notification命令行支持的参数

--- projects/termux/robot/index.js
+++ projects/termux/robot/index.js
@@ -127,8 +127,10 @@ function create_crontab_tasks(tasks){
 }
 /**
  * 使用termux-notification发送消息到下拉消息栏
+ * 命令：
+ * termux-notification --id "test" --content "内容" --title "标题" --button1-action "termux-open-url https://bing.com" --button1 "必应搜索" --button2-action "am start -a android.settings.BLUETOOTH_SETTINGS" --button2 "设置Bluetooth" --led-color 00A4FF --led-on 500
+ * 对应为：
  * ```JavaScript
- * // termux-notification --id "test" --content "内容" --title "标题" --button1-action "termux-open-url https://bing.com" --button1 "必应搜索" --button2-action "am start -a android.settings.BLUETOOTH_SETTINGS" --button2 "设置Bluetooth" --led-color 00A4FF --led-on 500
  * termux_notification({
  *     "--id":"test",
  *     "--content":"内容",
@@ -140,6 +142,52 @@ function create_crontab_tasks(tasks){
  *     "--led-color":"00A4FF",
  *     "--led-on":"500"
  * })
+ * ```
+ * termux-notification命令行支持的参数：
+ * ```text
+ * termux-notification --help
+ * Usage: termux-notification [options]
+ * Display a system notification. Content text is specified using -c/--content or read from stdin.
+ * Please read --help-actions for help with action arguments.
+ *   --action action          action to execute when pressing the notification
+ *   --alert-once             do not alert when the notification is edited
+ *   --button1 text           text to show on the first notification button
+ *   --button1-action action  action to execute on the first notification button
+ *   --button2 text           text to show on the second notification button
+ *   --button2-action action  action to execute on the second notification button
+ *   --button3 text           text to show on the third notification button
+ *   --button3-action action  action to execute on the third notification button
+ *   -c/--content content     content to show in the notification. Will take
+ *                            precedence over stdin. If content is not passed as
+ *                            an argument or with stdin, then there will be a 3s delay.
+ *   --group group            notification group (notifications with the same
+ *                            group are shown together)
+ *   -h/--help                show this help
+ *   --help-actions           show the help for actions
+ *   -i/--id id               notification id (will overwrite any previous notification
+ *                            with the same id)
+ *   --icon icon-name         set the icon that shows up in the status bar. View
+ *                            available icons at https://material.io/resources/icons/
+ *                            (default icon: event_note)
+ *   --image-path path        absolute path to an image which will be shown in the
+ *                            notification
+ *   --led-color rrggbb       color of the blinking led as RRGGBB (default: none)
+ *   --led-off milliseconds   number of milliseconds for the LED to be off while
+ *                            it's flashing (default: 800)
+ *   --led-on milliseconds    number of milliseconds for the LED to be on while
+ *                            it's flashing (default: 800)
+ *   --on-delete action       action to execute when the the notification is cleared
+ *   --ongoing                pin the notification
+ *   --priority prio          notification priority (high/low/max/min/default)
+ *   --sound                  play a sound with the notification
+ *   -t/--title title         notification title to show
+ *   --vibrate pattern        vibrate pattern, comma separated as in 500,1000,200
+ *   --type type              notification style to use (default/media)
+ * Media actions (available with --type "media"):
+ *   --media-next             action to execute on the media-next button
+ *   --media-pause            action to execute on the media-pause button
+ *   --media-play             action to execute on the media-play button
+ *   --media-previous         action to execute on the media-previous button
  * ```
  */
 function termux_notification(options){

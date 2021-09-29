705a5cfb387e124d3aa158abffcaf2d0dba3d616
SunDawning <dpmeichen@gmail.com>
2021-09-30 07:43:44 +0800

    优化 切换为rollup来处理index.js

--- projects/termux/robot/rollup.config.js
+++ projects/termux/robot/rollup.config.js
@@ -1,4 +1,9 @@
+import{
+    install_require_module
+}from"./src/utility.js";
+let{terser}=install_require_module("rollup-plugin-terser");
 export default{
     input:"src/index.js",
     output:{file:"index.js",format:"umd",name:""},
+    plugins:[terser()]
 };

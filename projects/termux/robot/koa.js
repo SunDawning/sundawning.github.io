95f585d275c2f3ab331f0051dad520e8e7e21cac
SunDawning <dpmeichen@gmail.com>
2021-09-30 19:42:44 +0800

    新增 koa.js

---
+++ projects/termux/robot/koa.js
@@ -0,0 +1,12 @@
+import{install_require_module}from"./utility.js";
+async function index(){
+    let Koa=install_require_module("koa");
+    let app=new Koa();
+    app.use(function(context,next){
+        context.body="Koa";
+    });
+    let port=8080;
+    app.listen(port);
+    console.log("启动网页服务器：",`http://localhost:${port}`);
+}
+index();

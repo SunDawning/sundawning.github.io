71c61d6016ca85de6cb5699e6d0caba9e3c72786
SunDawning <dpmeichen@gmail.com>
2021-09-30 19:45:17 +0800

    新增 生成UMD格式的JavaScript文件

--- projects/termux/robot/rollup.config.js
+++ projects/termux/robot/rollup.config.js
@@ -1,9 +1,19 @@
+/**
+ * 生成UMD格式的JavaScript文件
+ */
 import{
     install_require_module
 }from"./utility.js";
 let{terser}=install_require_module("rollup-plugin-terser");
-export default{
-    input:"index.js",
-    output:{file:"index.min.js",format:"umd",name:""},
-    plugins:[terser()]
-};
+export default[
+    {
+        input:"index.js",
+        output:{file:"index.min.js",format:"umd",name:""},
+        plugins:[terser()]
+    },
+    {
+        input:"koa.js",
+        output:{file:"koa.min.js",format:"umd",name:""},
+        plugins:[terser()]
+    }
+];

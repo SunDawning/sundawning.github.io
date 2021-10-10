8b4da62930a02ff003bc6ef2992732c112e6d5f7
SunDawning <dpmeichen@gmail.com>
2021-10-10 15:45:03 +0800

    优化 暂时只启动任务：sshd和crond

--- projects/termux/robot/index.js
+++ projects/termux/robot/index.js
@@ -7,24 +7,6 @@ import{
 }from"./utility.js";
 async function index(){
     hello();
-    // pm2 start mysqld
-    if(executable_find("mysqld")===undefined){
-        child_process_exec_sync("pkg install mariadb -y");
-    }
-    [
-        ["pnpm","npm install pnpm -g"],
-        ["pm2","pnpm add pm2 -g"],
-    ].forEach(function(item){
-        if(executable_find(item[0])===undefined){
-            child_process_exec_sync(item[1]);        
-        }        
-    });    
-    child_process_exec_sync("pm2 start mysqld");
-    // pm2 start koa.min.js
-    if(is_file_exists("koa.min.js")===false){
-        child_process_exec_sync("curl https://gitee.com/sundawning/sundawning.gitee.io/raw/master/projects/termux/robot/koa.min.js -o koa.min.js");
-    }
-    child_process_exec_sync("pm2 start koa.min.js");
     // sshd -p 8022
     if(executable_find("sshd")===undefined){
         child_process_exec_sync("pkg install openssh -y");

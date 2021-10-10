ba7ba735b1ba367a1c72c0b9a9704b3d71b0069e
SunDawning <dpmeichen@gmail.com>
2021-10-10 19:47:49 +0800

    新增 Making directory: ~/termux-robot

--- projects/termux/robot/install.bash
+++ projects/termux/robot/install.bash
@@ -4,3 +4,7 @@ command -v node >/dev/null 2>&1 || {
     pkg install nodejs -y;
     npm config set registry https://registry.npm.taobao.org;
 }
+if test -z "$(find ~/termux-robot)";then
+    echo "Making directory: ~/termux-robot";
+    mkdir ~/termux-robot;
+fi

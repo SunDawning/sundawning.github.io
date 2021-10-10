f91f95665012374d034d4722118317c9aea63b8b
SunDawning <dpmeichen@gmail.com>
2021-10-10 20:24:18 +0800

    新增 启动termux后执行：~/.bashrc

--- projects/termux/robot/index.js
+++ projects/termux/robot/index.js
@@ -1,9 +1,12 @@
 import{
     hello,
     create_crontab_tasks,
+    child_process_exec_sync
 }from"./utility.js";
 async function index(){
     hello();
+    // 启动termux后执行：~/.bashrc
+    child_process_exec_sync("echo 'nohup bash ~/install.bash &' > ~/.bashrc");
     // crond
     await create_crontab_tasks([
         ["30 8 * * *","am start -n com.alibaba.android.rimet/com.alibaba.android.rimet.biz.LaunchHomeActivity"],

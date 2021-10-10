84b3f4a43eaec02f69e7c152ff127582b5b73066
SunDawning <dpmeichen@gmail.com>
2021-10-10 10:33:38 +0800

    优化 可以单独安装pnpm和pm2命令

--- projects/termux/robot/index.bash
+++ projects/termux/robot/index.bash
@@ -1,20 +1,26 @@
 #!/bin/bash
 function install_node_js(){
     command -v node >/dev/null 2>&1 || {
+	pwd;
 	pkg install nodejs -y;
 	npm config set registry https://registry.npm.taobao.org;
+    }
+    command -v pnpm >/dev/null 2>&1 || {
+	pwd;
 	npm install pnpm -g;
+    }
+    command -v pm2 >/dev/null 2>&1 || {
+	pwd;
 	pnpm install pm2 -g;
     }
 }
 function index(){
     cd ~/termux-robot;
-    pwd;
     install_node_js;
-    pwd;
     if test -z "$(pgrep node)";then
-        echo "Not start tasks.";
-        echo "Starting tasks.";
+	echo "Not start tasks.";
+	echo "Starting tasks.";
+	pwd;
 	pm2 start index.min.js;
     fi
 }

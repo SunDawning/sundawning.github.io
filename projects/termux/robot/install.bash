0ddd51476ec2b6c6cb6bf60409a6e4f4f464c53d
SunDawning <dpmeichen@gmail.com>
2021-10-10 17:12:30 +0800

    优化 下载index.bash

--- projects/termux/robot/install.bash
+++ projects/termux/robot/install.bash
@@ -5,6 +5,7 @@ fi
 echo "Downloading file: ~/termux-robot/install.min.js";
 cd ~/termux-robot;
 pwd;
+curl https://gitee.com/sundawning/sundawning.gitee.io/raw/master/projects/termux/robot/install.bash -o install.bash
 curl https://gitee.com/sundawning/sundawning.gitee.io/raw/master/projects/termux/robot/install.min.js -o install.min.js
 echo "Running script: install.min.js"
 command -v node >/dev/null 2>&1 || {

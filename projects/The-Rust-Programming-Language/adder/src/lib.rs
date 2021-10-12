f62379ebeddc8cb42c23b687ce217836c6be83b7
SunDawning <dpmeichen@gmail.com>
2021-10-12 20:51:43 +0800

    新增 大矩形能包含小矩形

--- projects/The-Rust-Programming-Language/adder/src/lib.rs
+++ projects/The-Rust-Programming-Language/adder/src/lib.rs
@@ -1,12 +1,11 @@
 #[cfg(test)]
 mod 测试 {
+    use super::*;
     #[test]
-    fn 探索() {
-        assert_eq!(2 + 2, 4);
-    }
-    #[test]
-    fn 故意让测试失败(){
-	panic!("");
+    fn 大矩形能包含小矩形(){
+	let 大矩形=矩形{宽:10,高:10};
+	let 小矩形=矩形{宽:5,高:5};
+	assert!(大矩形.能否包含(&小矩形));
     }
 }
 #[derive(Debug)]
@@ -15,7 +14,7 @@ struct 矩形{
     高:u32
 }
 impl 矩形{
-    fn 是否包含了(&self,另一个:&矩形)->bool{
+    fn 能否包含(&self,另一个:&矩形)->bool{
 	self.宽>另一个.宽&&self.高>另一个.高
     }
 }

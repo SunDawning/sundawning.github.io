8870226198c984d8be6dab978dc27804b090f7eb
SunDawning <dpmeichen@gmail.com>
2021-10-14 20:39:17 +0800

    新增 替换默认测试，处理可变性。

--- projects/Rust-Programming-Cookbook/data-types/src/lib.rs
+++ projects/Rust-Programming-Cookbook/data-types/src/lib.rs
@@ -1,7 +1,17 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn 一些数学计算(){
+	assert_eq!(2+2,4);
+	assert_eq!(3.14+22.86,26_f32);
+	assert_eq!(2_i32.pow(2),4);
+	assert_eq!(4_f32.sqrt(),2_f32);
+	let a:u64=32;
+	let b:u64=64;
+	assert_eq!(b-a,32);
+	assert_eq!(a.overflowing_sub(b),(18446744073709551584,true));
+	let mut c=100;
+	c+=1;
+	assert_eq!(c,101);
     }
 }

e731f08bd1000d56c1bb212417077ab0c702bfc7
SunDawning <dpmeichen@gmail.com>
2021-10-15 08:48:25 +0800

    新增 添加＂fn 条件句＂

--- projects/Rust-Programming-Cookbook/execution-flow/src/lib.rs
+++ projects/Rust-Programming-Cookbook/execution-flow/src/lib.rs
@@ -4,4 +4,15 @@ mod tests {
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+    #[test]
+    fn 条件句(){
+        let i=20;
+        if i<2{
+            assert!(i<2);
+        }else if i>2{
+            assert!(i>2);
+        }else{
+            assert_eq!(i,2);
+        }
+    }
 }

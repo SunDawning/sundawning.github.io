049125550b542f9b1bc70ba7c6af4e739d73f8d6
SunDawning <dpmeichen@gmail.com>
2021-10-15 08:55:27 +0800

    新增 条件句做一些pattern matching

--- projects/Rust-Programming-Cookbook/execution-flow/src/lib.rs
+++ projects/Rust-Programming-Cookbook/execution-flow/src/lib.rs
@@ -15,4 +15,20 @@ mod tests {
             assert_eq!(i,2);
         }
     }
+    #[test]
+    fn 更多条件句(){
+        let 选项一=Some(10);
+        if let Some(unpacked)=选项一{
+            assert_eq!(unpacked,10);
+        }
+        let mut 选项二=Some(2);
+        while let Some(unpacked)=选项二{
+            选项二=if unpacked>0{
+                Some(unpacked-1)
+            }else{
+                None
+            }
+        }
+        assert_eq!(选项二,None);
+    }
 }

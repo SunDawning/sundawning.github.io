f4a1c7168f84b3d045fb2cc37f45e2062b3d81fd
SunDawning <dpmeichen@gmail.com>
2021-10-18 20:02:08 +0800

    新增 projects/node/npm/sqlite3
    在node里使用数据库sqlite3

---
+++ projects/node/npm/sqlite3/index.js
@@ -0,0 +1,16 @@
+/**
+ * @see SQLite快速上手 — Node.js小册: https://nodejs.fasionchan.com/zh_CN/latest/database/sqlite/index.html
+ */
+let sqlite3=require("sqlite3").verbose();
+let fs=require("fs");
+let dbFile="./user.db";
+if(fs.existsSync(dbFile)===false){
+    fs.writeFileSync(dbFile,"");
+};
+let db=new sqlite3.Database(dbFile,sqlite3.OPEN_READWRITE,function(error){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("已连接数据库");
+    }
+});

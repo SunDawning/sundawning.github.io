cc7dfcbb959ce9668229863da7fa86756c17a41c
SunDawning <dpmeichen@gmail.com>
2021-10-22 08:38:29 +0800

    优化 批量导入dbDirectory里的csv文件到mongodb时，如果已经导入到mongodb，则移动csv文件到dbDirectoryBackup文件夹。

--- projects/node/LianjiaRentplatHouseShenzhen/index.py
+++ projects/node/LianjiaRentplatHouseShenzhen/index.py
@@ -1,29 +1,46 @@
 import os
-from pymongo import MongoClient
-import csv
-
 host = os.environ.get('MONGODB_HOST', '127.0.0.1')  # 本地数据库
 port = os.environ.get('MONGODB_PORT', '27017')  # 数据库端口
 mongo_url = 'mongodb://{}:{}'.format(host, port)
 mongo_db = os.environ.get('MONGODB_DATABASE', 'Lianjia')
 print(mongo_db)
+from pymongo import MongoClient
 client = MongoClient(mongo_url)
 print(client)
 db = client[mongo_db]
 db['zufang'].create_index('m_url', unique=True)  # 以m端链接为主键进行去重
-dbDirectory="dbDirectory" # 处理CSV文件夹
+# 读取CSV文件夹
+dbDirectory="dbDirectory"
+dbDirectoryBackup="{}Backup".format(dbDirectory)
+if(os.path.exists(dbDirectoryBackup)==False):
+    os.makedirs(dbDirectoryBackup)
+    pass
 files=os.listdir(dbDirectory)
 for file in files:
     path="{}/{}".format(dbDirectory,file)
-    print("处理文件：{}".format(path))
-    name=os.path.splitext(file)[0]
-    with open(path,'r',encoding='utf-8')as csvfile:
-        reader=csv.DictReader(csvfile)
-        for item in reader:
-            item["city"]="深圳";
-            item["type"]="整租";
-            item["timestamp"]=name;
-            db['zufang'].update_one({'m_url': item['m_url']}, {'$set': item}, upsert=True)
+    # 不处理正在下载数据的CSV文件
+    from time import time
+    if((time()-os.path.getmtime(path))>120):
+        print("Import csv to mongodb: {}".format(path))
+        name=os.path.splitext(file)[0]
+        # 导入csv文件里的数据到mongodb数据库
+        with open(path,'r',encoding='utf-8')as csvfile:
+            from csv import DictReader
+            reader=DictReader(csvfile)
+            for item in reader:
+                item["city"]="深圳";
+                item["type"]="整租";
+                item["timestamp"]=name;
+                db['zufang'].update_one({'m_url': item['m_url']}, {'$set': item}, upsert=True)
+                pass
             pass
+        backupPath="{}/{}".format(dbDirectoryBackup,file)
+        print("Move {} => {}".format(path,backupPath))
+        from shutil import move
+        move(path,backupPath)
+        pass
+    else:
+        print("Locked: {}".format(path))
         pass
     pass
+

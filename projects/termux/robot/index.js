abbabae0732a086d6f593a13306c42a80096750a
SunDawning <dpmeichen@gmail.com>
2021-10-31 19:28:22 +0800

    新增 http-server命令

--- projects/termux/robot/index.js
+++ projects/termux/robot/index.js
@@ -5,6 +5,7 @@ import{
     child_process_exec,
     start_sshd,
     install_emacs,
+    pnpm_add_g
 }from"./utility.js";
 async function index(){
     hello();
@@ -15,5 +16,7 @@ async function index(){
         ["0 18 * * *","am start -n com.alibaba.android.rimet/com.alibaba.android.rimet.biz.LaunchHomeActivity"]
     ]);
     install_emacs();
+    // 下载、安装npm模块http-server来便于启动静态网页服务器，在多个设备之间以http的方式来共享文件。
+    pnpm_add_g("http-server");
 }
 index();

593becdb7f7f54eeb31a00e12efd7deaa7aa4f84
SunDawning <dpmeichen@gmail.com>
2021-11-10 19:02:00 +0800

    新增 同步运行两个循环

--- projects/python/asyncio/index.py
+++ projects/python/asyncio/index.py
@@ -13,13 +13,41 @@ async def echo(websocket, path):
         pass
     pass
 
-async def main():
+async def websocketServe():
     """
     https://pypi.org/project/websockets/
+    启动WebSocket服务器
     """
     async with serve(echo, "localhost", 8765):
         await asyncio.Future()  # run forever
         pass
+    pass
+
+async def broadcast():
+    """
+    广播通知所有用户
+    """
+    while True:
+        if(len(websockets)>0):
+            tasks=[]
+            for websocket in websockets:
+                from datetime import datetime
+                tasks.append(asyncio.create_task(websocket.send("{}".format(datetime.now()))))
+                pass
+            await asyncio.gather(*tasks)
+            pass
+        await asyncio.sleep(1)
+        pass
+    pass
+
+async def main():
+    """
+    主入口
+    """
+    tasks=[]
+    tasks.append(asyncio.create_task(websocketServe()))
+    tasks.append(asyncio.create_task(broadcast()))
+    await asyncio.gather(*tasks)
     pass
 
 asyncio.run(main())

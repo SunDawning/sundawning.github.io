52123ce0e335b89dab64be864c92ea2bad59bb70
SunDawning <dpmeichen@gmail.com>
2021-11-10 18:47:01 +0800

    新增 python asyncio

---
+++ projects/python/asyncio/index.py
@@ -0,0 +1,21 @@
+import asyncio
+from websockets import serve
+
+websockets=set()
+
+async def echo(websocket, path):
+    websockets.add(websocket)
+    await websocket.send("来自Python里的WebSockets服务器")
+    print("在线：{}".format(len(websockets)))
+    async for message in websocket:
+        print("收到信息：{}".format(message))
+        await websocket.send(message)
+
+async def main():
+    """
+    https://pypi.org/project/websockets/
+    """
+    async with serve(echo, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())

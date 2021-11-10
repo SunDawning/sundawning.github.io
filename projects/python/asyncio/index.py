66fca0eab2761a2b8ecbd74b107b64062442957b
SunDawning <dpmeichen@gmail.com>
2021-11-10 20:31:53 +0800

    新增 多线程与协程共用时的问题

--- projects/python/asyncio/index.py
+++ projects/python/asyncio/index.py
@@ -1,5 +1,6 @@
 import asyncio
 from websockets import serve
+from datetime import datetime
 
 websockets=set()
 
@@ -8,10 +9,8 @@ async def echo(websocket, path):
     await websocket.send("来自Python里的WebSockets服务器")
     print("在线：{}".format(len(websockets)))
     async for message in websocket:
-        from time import sleep
-        sleep(10)
         print("收到信息：{}".format(message))
-        print("websocket: {}".format(asyncio.get_event_loop()))
+        print("{} websocket: {}".format(datetime.now(),asyncio.get_event_loop()))
         await websocket.send(message)
         pass
     pass
@@ -34,44 +33,26 @@ async def broadcast():
         if(len(websockets)>0):
             tasks=[]
             for websocket in websockets:
-                from datetime import datetime
                 tasks.append(asyncio.create_task(websocket.send("{}".format(datetime.now()))))
                 pass
             await asyncio.gather(*tasks)
             pass
-        print("broadcast: {}".format(asyncio.get_event_loop()))
+        print("{} broadcast: {}".format(datetime.now(),asyncio.get_event_loop()))
         from time import sleep
         sleep(1)
         pass
     pass
 
 def broadcastThreadTarget():
-    loop=asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(broadcast())
+    asyncio.run(broadcast())
     pass
 from threading import Thread
 broadcastThread=Thread(target=broadcastThreadTarget,daemon=True)
 broadcastThread.start()
 
 def websocketServeThreadTarget():
-    loop=asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(websocketServe())
+    asyncio.run(websocketServe())
     pass
 from threading import Thread
 websocketServeThread=Thread(target=websocketServeThreadTarget,daemon=True)
 websocketServeThread.start()
-
-async def loop():
-    while True:
-        await asyncio.sleep(1)
-        pass
-    pass
-async def main():
-    """
-    Python黑魔法asyncio之----在多线程中启用多个事件循环 - 简书: https://www.jianshu.com/p/29ffdbd65679
-    """
-    await loop()
-    pass
-asyncio.run(main())

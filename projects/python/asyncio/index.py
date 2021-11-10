5548daf0c51cf7d3151a48cc34317a6e349528f0
SunDawning <dpmeichen@gmail.com>
2021-11-10 20:04:40 +0800

    新增 在主线程里同时运行多个子线程和协程

--- projects/python/asyncio/index.py
+++ projects/python/asyncio/index.py
@@ -8,7 +8,10 @@ async def echo(websocket, path):
     await websocket.send("来自Python里的WebSockets服务器")
     print("在线：{}".format(len(websockets)))
     async for message in websocket:
+        from time import sleep
+        sleep(10)
         print("收到信息：{}".format(message))
+        print("websocket: {}".format(asyncio.get_event_loop()))
         await websocket.send(message)
         pass
     pass
@@ -36,18 +39,39 @@ async def broadcast():
                 pass
             await asyncio.gather(*tasks)
             pass
-        await asyncio.sleep(1)
+        print("broadcast: {}".format(asyncio.get_event_loop()))
+        from time import sleep
+        sleep(1)
         pass
     pass
 
+def broadcastThreadTarget():
+    loop=asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(broadcast())
+    pass
+from threading import Thread
+broadcastThread=Thread(target=broadcastThreadTarget,daemon=True)
+broadcastThread.start()
+
+def websocketServeThreadTarget():
+    loop=asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(websocketServe())
+    pass
+from threading import Thread
+websocketServeThread=Thread(target=websocketServeThreadTarget,daemon=True)
+websocketServeThread.start()
+
+async def loop():
+    while True:
+        await asyncio.sleep(1)
+        pass
+    pass
 async def main():
     """
-    主入口
+    Python黑魔法asyncio之----在多线程中启用多个事件循环 - 简书: https://www.jianshu.com/p/29ffdbd65679
     """
-    tasks=[]
-    tasks.append(asyncio.create_task(websocketServe()))
-    tasks.append(asyncio.create_task(broadcast()))
-    await asyncio.gather(*tasks)
+    await loop()
     pass
-
 asyncio.run(main())

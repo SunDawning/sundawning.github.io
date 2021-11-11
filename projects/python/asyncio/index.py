cf34b4dc7c54fdb5a2f19e7bc34292cb97e0c9f4
SunDawning <dpmeichen@gmail.com>
2021-11-11 08:05:54 +0800

    修复 给Python websocket server服务器新增一个Python client客户端来应对网页client 客户端index.html频繁接收消息时会卡一会后再出现批量消息的情况

--- projects/python/asyncio/index.py
+++ projects/python/asyncio/index.py
@@ -3,6 +3,7 @@
 # python -m kernprof -l -v index.py
 
 import asyncio
+from websockets import connect
 from websockets import serve
 from datetime import datetime
 from time import sleep
@@ -44,23 +45,38 @@ async def broadcast():
     print("{} broadcast: {}".format(datetime.now(),asyncio.get_event_loop()))
     pass
 
-@profile
+async def websocketClient():
+    async with connect('ws://localhost:8765') as websocket:
+        async for message in websocket:
+            print("{} 收到信息：{}".format(datetime.now(),message))
+            pass
+        pass
+    pass
+
 def broadcastThreadTarget():
     asyncio.run(broadcast())
     pass
 
-@profile
 def websocketServeThreadTarget():
     asyncio.run(websocketServe())
     pass
+
+def websocketClientThreadTarget():
+    asyncio.run(websocketClient())
+    pass
+
 from threading import Thread
 websocketServeThread=Thread(target=websocketServeThreadTarget,daemon=True)
 websocketServeThread.start()
 
+# 给Python websocket server服务器新增一个Python client客户端来应对网页client 客户端index.html频繁接收消息时会卡一会后再出现批量消息的情况
+from threading import Thread
+websocketClientThread=Thread(target=websocketClientThreadTarget,daemon=True)
+websocketClientThread.start()
+
 while True:
-    from random import randint
     sleep(1)
     from threading import Thread
     broadcastThread=Thread(target=broadcastThreadTarget,daemon=True)
     broadcastThread.start()
-    
+    pass

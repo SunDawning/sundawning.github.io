160b38144c0d847d26e180c76d5d6b179a21d041
SunDawning <dpmeichen@gmail.com>
2021-11-20 17:32:57 +0800

    新增 输出所猜的数字

--- projects/Rust程序设计语言简体中文版/guessing_game/src/main.rs
+++ projects/Rust程序设计语言简体中文版/guessing_game/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    println!("已进入猜数字游戏");
+    println!("你先猜一个数字：");
+    let guessed_number = "1";
+    println!("你猜的数字是：{}",guessed_number);
 }

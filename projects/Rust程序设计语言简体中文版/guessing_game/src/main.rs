2e4c6006a9c322b15050314ae5d60ba8aa24481d
SunDawning <dpmeichen@gmail.com>
2021-11-20 17:38:17 +0800

    新增 允许玩家输入猜测的数字

--- projects/Rust程序设计语言简体中文版/guessing_game/src/main.rs
+++ projects/Rust程序设计语言简体中文版/guessing_game/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
     println!("已进入猜数字游戏");
     println!("你先猜一个数字：");
-    let guessed_number = "1";
+    // "1"
+    let mut guessed_number = String::new();
+    std::io::stdin()
+        .read_line(&mut guessed_number)
+        .expect("无法获取所猜的数字");
     println!("你猜的数字是：{}", guessed_number);
 }

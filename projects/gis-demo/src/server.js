5cea8505b7608539ea87ecc14a2588bac3071c62
SunDawning <dpmeichen@gmail.com>
2021-11-24 21:08:31 +0800

    新增 离线下载图片并在网页里能显示

--- projects/gis-demo/src/server.js
+++ projects/gis-demo/src/server.js
@@ -5,11 +5,24 @@
  */
 let child_process=require("child_process");
 let Koa=require("koa");
+let fs=require("fs");
+let axios=require("axios");
 let proxy_port=9081;
 let http_server_process=child_process.exec(`npx http-server -p 9080 --proxy http://localhost:${proxy_port}`);
 http_server_process.stdout.pipe(process.stdout);
 let app=new Koa();
-app.use(function(context,next){
-    console.log(context.method,context.url);
+app.use(async function(context,next){
+    let url=context.url;
+    console.log(context.method,url);
+    if(context.url.search("^/assets/")>-1){
+        // "/assets/61.144.226.124:10000/AddressWeb/favicon.ico"
+        let online_url=url.replace("/assets/","http://");
+        let response=await axios.get(online_url,{
+            responseType:"arraybuffer"
+        });
+        fs.writeFileSync("favicon.ico",response.data);
+        context.set("content-type",response.headers["content-type"]);
+        context.body=response.data;
+    }
 });
 app.listen(proxy_port);

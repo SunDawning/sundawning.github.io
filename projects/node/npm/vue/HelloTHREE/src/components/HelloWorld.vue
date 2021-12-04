b090c14dfc1dfa33e18764640600d82ef8b57e67
SunDawning <dpmeichen@gmail.com>
2021-12-04 21:53:40 +0800

    新增 在页面里能显示THREE场景

--- projects/node/npm/vue/HelloTHREE/src/components/HelloWorld.vue
+++ projects/node/npm/vue/HelloTHREE/src/components/HelloWorld.vue
@@ -1,40 +1,47 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted } from "vue";
 
-defineProps({
-  msg: String
-})
+import * as THREE from "three";
 
-const count = ref(0)
+/**
+ * @see https://threejs.org/docs/index.html#manual/zh/introduction/Creating-a-scene
+ */
+function init() {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  const animate = function () {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+  };
+
+  animate();
+}
+
+onMounted(init);
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
-</template>
+<template></template>
 
 <style scoped>
-a {
-  color: #42b983;
-}
 </style>

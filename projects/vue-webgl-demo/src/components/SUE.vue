836f65bcef8f5290d57bd5bd02404c3ff432af26
SunDawning <dpmeichen@gmail.com>
2021-12-05 22:48:22 +0800

    新增 往THREE查看器里添加辅助坐标轴和辅助平面

--- projects/vue-webgl-demo/src/components/SUE.vue
+++ projects/vue-webgl-demo/src/components/SUE.vue
@@ -1,6 +1,12 @@
 <script setup>
 import { ref, onMounted } from "vue";
-import { Scene, PerspectiveCamera, WebGLRenderer } from "three";
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  AxesHelper,
+  GridHelper,
+} from "three";
 /**
  * THREE查看器
  * @param {HTMLElement} container
@@ -8,15 +14,22 @@ import { Scene, PerspectiveCamera, WebGLRenderer } from "three";
 function THREEViewer(container) {
   let SELF = {};
   SELF.scene = new Scene();
+  SELF.scene.add(new AxesHelper(100));
+  SELF.scene.add(new GridHelper(100, 10));
+  SELF.canvasSize = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
   SELF.camera = new PerspectiveCamera(
     45,
-    container.clientWidth / container.clientHeight,
-    0,
+    SELF.canvasSize.width / SELF.canvasSize.height,
+    1,
     1000
   );
-  SELF.camera.position.set(0, 10, 0);
+  SELF.camera.position.set(10, 10, 10);
+  SELF.camera.lookAt(0, 0, 0);
   SELF.renderer = new WebGLRenderer();
-  SELF.renderer.setSize(container.clientWidth, container.clientHeight);
+  SELF.renderer.setSize(SELF.canvasSize.width, SELF.canvasSize.height);
   container.appendChild(SELF.renderer.domElement);
   SELF.animation = {};
   SELF.animation.start = function () {
@@ -44,9 +57,9 @@ onMounted(function () {
 
 <style scoped>
 .container {
-  position:absolute;
-  left:0;
-  top:0;
+  position: absolute;
+  left: 0;
+  top: 0;
   width: 100%;
   height: 100%;
 }

1de589d5daca699cc65c1b41bf27f0836c285e52
SunDawning <dpmeichen@gmail.com>
2021-12-05 11:12:20 +0800

    新增 调整vite以正常显示Cesium地球

--- projects/node/npm/vue/HelloCesium/vite.config.js
+++ projects/node/npm/vue/HelloCesium/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 一分钟搭建 Vite + Cesium 开发环境 - 知乎: https://zhuanlan.zhihu.com/p/354856692
+import cesium from "vite-plugin-cesium"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), cesium()]
 })

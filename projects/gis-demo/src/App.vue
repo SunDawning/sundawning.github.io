cfd333ad6e75814d7fff14801565216c63c91476
SunDawning <dpmeichen@gmail.com>
2021-12-06 17:23:53 +0800

    优化 能使用vue

---
+++ projects/gis-demo/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import{ref,onMounted}from"vue";
+  import{Viewer}from"cesium";
+  function init(container){
+      let viewer=new Viewer(container);
+  }
+  onMounted(function(){
+      init(document.querySelector("#container"));
+  });
+</script>
+
+<template>
+  <div id="container"></div>
+</template>
+
+<style>
+  #container{
+      position:absolute;
+      left:0;
+      top:0;
+      width:100%;
+      height:100%;      
+  }
+</style>

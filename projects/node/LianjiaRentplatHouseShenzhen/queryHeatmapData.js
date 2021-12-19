5e7f97a4636956f51838e07e6d790590e546476f
SunDawning <dpmeichen@gmail.com>
2021-12-19 20:39:39 +0800

    优化 设置条件为＂一室一卫一厨＂

--- projects/node/LianjiaRentplatHouseShenzhen/queryHeatmapData.js
+++ projects/node/LianjiaRentplatHouseShenzhen/queryHeatmapData.js
@@ -29,8 +29,12 @@ async function index(){
         let collection=db.collection(collectionName);
         for(let item of heatmapData){
             let m_url=item["m_url"];
+            // 一室一卫一厨
             let exists=await collection.find({
-                'm_url': m_url
+                'm_url': m_url,
+                'frame_bedroom_num': '1', 
+                'frame_bathroom_num': '1', 
+                'frame_kitchen_num': '1'
             },{
                 sort:{
                     'timestamp': -1
@@ -38,6 +42,8 @@ async function index(){
                 limit:1
             }).toArray();
             let one=exists[0];
+            // 不存在符合要求的数据
+            if(one===undefined){return;}
             let rent_price_listing=one.rent_price_listing;
             item["lng"]=parseFloat(item["longitude"]);
             delete item["longitude"];

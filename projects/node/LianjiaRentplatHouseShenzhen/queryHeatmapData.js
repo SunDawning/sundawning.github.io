5c0caa9401ab980daa6d5f105c5dfacce80b21dd
SunDawning <dpmeichen@gmail.com>
2021-12-19 21:06:20 +0800

    优化 调整热力图的筛选条件

--- projects/node/LianjiaRentplatHouseShenzhen/queryHeatmapData.js
+++ projects/node/LianjiaRentplatHouseShenzhen/queryHeatmapData.js
@@ -24,17 +24,19 @@ async function index(){
         }).toArray();
         heatmapData=exists;
     }
+    let filteredHeatmapData=[];
     {
         let collectionName=`index`;
         let collection=db.collection(collectionName);
         for(let item of heatmapData){
             let m_url=item["m_url"];
-            // 一室一卫一厨
+            // 一室一卫一厨，25-50平米，租金在2500以下。
             let exists=await collection.find({
                 'm_url': m_url,
-                'frame_bedroom_num': '1', 
-                'frame_bathroom_num': '1', 
-                'frame_kitchen_num': '1'
+                'frame_bedroom_num': '1',
+                'frame_bathroom_num': '1',
+                // 'frame_kitchen_num': '1',
+                "rent_area":{"$gte":"25","$lte":"50"},
             },{
                 sort:{
                     'timestamp': -1
@@ -43,19 +45,22 @@ async function index(){
             }).toArray();
             let one=exists[0];
             // 不存在符合要求的数据
-            if(one===undefined){return;}
-            let rent_price_listing=one.rent_price_listing;
+            if(one===undefined){continue;}
+            let rent_price_listing=parseFloat(one.rent_price_listing);
+            if(rent_price_listing>2500){continue;}
             item["lng"]=parseFloat(item["longitude"]);
             delete item["longitude"];
             item["lat"]=parseFloat(item["latitude"]);
             delete item["latitude"];
-            item["count"]=parseFloat(rent_price_listing);
+            item["count"]=rent_price_listing;
             delete item["m_url"];
+            filteredHeatmapData.push(item);
+            console.log(item);
         }
     }
     let fs=require("fs-extra");
     fs.writeFile(`heatmapData.js`,`
-var heatmapData=${JSON.stringify(heatmapData)}
+var heatmapData=${JSON.stringify(filteredHeatmapData)}
 `);
     mongoClient.close();
 }

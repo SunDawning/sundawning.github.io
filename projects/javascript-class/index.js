04a7b63f020175fa0302175b2135c395e1d679d6
SunDawning <dpmeichen@gmail.com>
2021-12-24 21:05:07 +0800

    替换为抽象的类

--- projects/javascript-class/index.js
+++ projects/javascript-class/index.js
@@ -1,9 +1,16 @@
+// JavaScript 进阶教程(1)--面向对象编程_AlbertYang的博客-CSDN博客_js编程: https://blog.csdn.net/qq_23853743/article/details/108034430
 // 封装、继承、多态
-
-var student1={name:"张三",score:98};
-var student2={name:"李四",score:81};
-function printScore(student){
-    console.log("姓名：",student.name,"成绩：",student.score);
+// 1. 抽象出Class
+function Student(name,score){
+    this.name=name;
+    this.score=score;
 }
-printScore(student1);
-printScore(student2);
+Student.prototype.printScore=function(){
+    console.log("姓名：",this.name,"成绩：",this.score);
+};
+// 2. 根据Class创建Instance
+var student1=new Student("张三",98);
+var student2=new Student("李四",81);
+// 3. 指挥Instance得结果
+student1.printScore();
+student2.printScore();

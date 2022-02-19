e88cf8658aebc9d590296a11e8b0b8b43b6118bd
SunDawning <dpmeichen@gmail.com>
2022-02-19 20:21:09 +0800

    给行政区域添加文本标签

--- projects/2022-02-19-租房/src/index.js
+++ projects/2022-02-19-租房/src/index.js
@@ -1,86 +1,119 @@
-Cesium.Ion.defaultAccessToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkYWFkMDIyOC0zZjQyLTRlMzUtYjQ4YS0xNjBhMDgwNTE4YTAiLCJpZCI6NzY5MDcsImlhdCI6MTY0NTI2NDUzMH0.N9v17oLP08jFvYtSns4GXGAXNf6pPqdKI_FpQx8QADA";
+Cesium.Ion.defaultAccessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkYWFkMDIyOC0zZjQyLTRlMzUtYjQ4YS0xNjBhMDgwNTE4YTAiLCJpZCI6NzY5MDcsImlhdCI6MTY0NTI2NDUzMH0.N9v17oLP08jFvYtSns4GXGAXNf6pPqdKI_FpQx8QADA";
 
-const container=document.createElement("div");
-container.className="container";
+const container = document.createElement("div");
+container.className = "container";
 document.body.appendChild(container);
-const viewer=new Cesium.Viewer(container);
+const viewer = new Cesium.Viewer(container);
 
-function getDistrictRectangleByAmap(){
-    // https://zhuanlan.zhihu.com/p/409983169
-    // 深圳区域
-    // https://lbs.amap.com/api/webservice/guide/api/district
-    // https://lbs.amap.com/service/api/restapi?keywords=440300&subdistrict=0&extensions=all
-    const longitudeLatitude=temp1.districts[0].polyline.split(";").map(function(item){
-        return item.split(",").map(function(c){
-            return Number.parseFloat(c);
-        });
+function getDistrictRectangleByAmap() {
+  // https://zhuanlan.zhihu.com/p/409983169
+  // 深圳区域
+  // https://lbs.amap.com/api/webservice/guide/api/district
+  // https://lbs.amap.com/service/api/restapi?keywords=440300&subdistrict=0&extensions=all
+  const longitudeLatitude = temp1.districts[0].polyline
+    .split(";")
+    .map(function (item) {
+      return item.split(",").map(function (c) {
+        return Number.parseFloat(c);
+      });
     });
-    const maxX=Math.max.apply(null,longitudeLatitude.map(function(item){
-        return item[0];
-    }));
-    const maxY=Math.max.apply(null,longitudeLatitude.map(function(item){
-        return item[1];
-    }));
-    const minX=Math.min.apply(null,longitudeLatitude.map(function(item){
-        return item[0];
-    }));
-    const minY=Math.min.apply(null,longitudeLatitude.map(function(item){
-        return item[1];
-    }));
-    console.log(JSON.stringify([minX,minY,maxX,maxY]));
+  const maxX = Math.max.apply(
+    null,
+    longitudeLatitude.map(function (item) {
+      return item[0];
+    })
+  );
+  const maxY = Math.max.apply(
+    null,
+    longitudeLatitude.map(function (item) {
+      return item[1];
+    })
+  );
+  const minX = Math.min.apply(
+    null,
+    longitudeLatitude.map(function (item) {
+      return item[0];
+    })
+  );
+  const minY = Math.min.apply(
+    null,
+    longitudeLatitude.map(function (item) {
+      return item[1];
+    })
+  );
+  console.log(JSON.stringify([minX, minY, maxX, maxY]));
 }
 {
-    // 飞到指定范围 https://www.cnblogs.com/xym0710/p/14949107.html
-    // 某地的行政区域 https://www.zhoulujun.cn/html/GIS/WebGIS/8155.html
-    viewer.camera.setView({destination:new Cesium.Rectangle.fromDegrees(113.751453,22.396344,114.628466,22.861748)});
+  // 飞到指定范围 https://www.cnblogs.com/xym0710/p/14949107.html
+  // 某地的行政区域 https://www.zhoulujun.cn/html/GIS/WebGIS/8155.html
+  viewer.camera.setView({
+    destination: new Cesium.Rectangle.fromDegrees(
+      113.751453,
+      22.396344,
+      114.628466,
+      22.861748
+    ),
+  });
 }
-function importAmapDistrict(){
-    const src="../public/district_440300.js";
-    {
-        const baseURL=window.location.href;
-        const fullSrc=new URL(src,baseURL).href;
-        const isAppend=(Object.values(document.scripts).filter(function(item){
-            return item.src===fullSrc;
-        }).length>0);
-        if(isAppend===true){
-            console.log("script load",src);
-        }else{
-            const script=document.createElement("script");
-            script.type="text/javascript";
-            script.src=src;
-            document.head.appendChild(script);
-            script.addEventListener("load",function(event){
-                console.log("script load",script);
-                {
-                    const polyline=district_440300.districts[0].polyline.split(/[,;]/).map(function(item){
-                        return Number.parseFloat(item);
-                    });
-                    let hierarchy=Cesium.Cartesian3.fromDegreesArray(polyline);
-                    hierarchy=hierarchy.slice(0,hierarchy.length-100);
-                    viewer.entities.add({
-                        name:"district_440300",
-                        polygon:{
-                            hierarchy:hierarchy,
-                            outline:true,
-                            outlineColor:Cesium.Color.RED,
-                            fill:false,
-                        },                        
-                    });
-                }                
+function importAmapDistrict() {
+  const src = "../public/district_440300.js";
+  {
+    const baseURL = window.location.href;
+    const fullSrc = new URL(src, baseURL).href;
+    const isAppend =
+      Object.values(document.scripts).filter(function (item) {
+        return item.src === fullSrc;
+      }).length > 0;
+    if (isAppend === true) {
+      console.log("script load", src);
+    } else {
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = src;
+      document.head.appendChild(script);
+      script.addEventListener("load", function (event) {
+        console.log("script load", script);
+        {
+          const polyline = district_440300.districts[0].polyline
+            .split(/[,;]/)
+            .map(function (item) {
+              return Number.parseFloat(item);
             });
+          let hierarchy = Cesium.Cartesian3.fromDegreesArray(polyline);
+          hierarchy = hierarchy.slice(0, hierarchy.length - 100);
+          viewer.entities.add({
+            name: "district_440300",
+            polygon: {
+              hierarchy: hierarchy,
+              outline: true,
+              outlineColor: Cesium.Color.RED,
+              fill: false,
+            },
+          });
         }
+      });
     }
+  }
 }
 {
-    // https://zhuanlan.zhihu.com/p/344395928
-    // https://github.com/TangSY/echarts-map-demo
-    // https://hxkj.vip/demo/echartsMap/
-    // https://sandcastle.cesium.com/index.html?src=GeoJSON%20and%20TopoJSON.html
-    viewer.dataSources.add(
-        Cesium.GeoJsonDataSource.load(
-            "../public/440300.geoJson",{
-                fill: new Cesium.Color(0,0,0,0),
-            }
-        )
-    );
+  // https://zhuanlan.zhihu.com/p/344395928
+  // https://github.com/TangSY/echarts-map-demo
+  // https://hxkj.vip/demo/echartsMap/
+  // https://sandcastle.cesium.com/index.html?src=GeoJSON%20and%20TopoJSON.html
+  Cesium.GeoJsonDataSource.load("../public/440300.geoJson", {
+    fill: new Cesium.Color(0, 0, 0, 0),
+  }).then(function (dataSource) {
+    viewer.dataSources.add(dataSource);
+    dataSource.entities.values.forEach(function (entity) {
+      entity.position = Cesium.Cartesian3.fromDegrees.apply(
+        null,
+        entity.properties.centroid._value
+      );
+      entity.label = {
+        text: entity.name,
+        scaleByDistance: new Cesium.NearFarScalar(100000, 1, 1000000, 0),
+      };
+    });
+  });
 }

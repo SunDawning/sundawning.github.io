5313fb2530cae0b666e4054687b502aabe162cd6
SunDawning <dpmeichen@gmail.com>
2022-02-20 10:01:59 +0800

    添加高德路网注记地图

--- projects/2022-02-19-租房/src/index.js
+++ projects/2022-02-19-租房/src/index.js
@@ -18,16 +18,17 @@ const viewer = new Cesium.Viewer(container);
     })
   );
 }
+const rectangle = Cesium.Rectangle.fromDegrees(
+  113.751453,
+  22.396344,
+  114.628466,
+  22.861748
+);
 {
   // 飞到指定范围 https://www.cnblogs.com/xym0710/p/14949107.html
   // 某地的行政区域 https://www.zhoulujun.cn/html/GIS/WebGIS/8155.html
   viewer.camera.setView({
-    destination: new Cesium.Rectangle.fromDegrees(
-      113.751453,
-      22.396344,
-      114.628466,
-      22.861748
-    ),
+    destination: rectangle,
   });
 }
 {
@@ -52,4 +53,33 @@ const viewer = new Cesium.Viewer(container);
       });
     });
   });
+}
+{
+  //   // 1.影像图
+  // https://webst01.is.autonavi.com/appmaptile?x=54658&y=26799&z=16&style=6
+  // // 2.道路纯图
+  // https://wprd01.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x=54658&y=26799&z=16&scl=1&ltype=2
+  // // 3.道路简图
+  // http://webrd01.is.autonavi.com/appmaptile?x=54658&y=26799&z=16&size=1&scale=1&style=7
+  // // 4.道路详图
+  // http://webrd01.is.autonavi.com/appmaptile?x=54658&y=26799&z=16&lang=zh_cn&size=1&scale=1&style=8
+  // // 5.纯道路图
+  // http://wprd01.is.autonavi.com/appmaptile?x=54658&y=26799&z=16&lang=zh_cn&size=1&scl=1&style=8&ltype=11
+  // // 6.纯地标图
+  // https://wprd01.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=8&x=54658&y=26799&z=16&scl=1&ltype=4
+  // // 7.路网注记图
+  // http://webst01.is.autonavi.com/appmaptile?x=54658&y=26799&z=16&lang=zh_cn&size=1&scale=1&style=8
+
+  // ————————————————
+  // 版权声明：本文为CSDN博主「右弦」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+  // 原文链接：https://blog.csdn.net/weixin_45782925/article/details/122984175
+
+  viewer.imageryLayers.addImageryProvider(
+    new Cesium.UrlTemplateImageryProvider({
+      url: "http://webst01.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=1&style=8",
+      minimumLevel: 3,
+      maximumLevel: 18,
+      rectangle: rectangleg,
+    })
+  );
 }

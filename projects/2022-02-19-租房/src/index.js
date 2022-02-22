8eb7bb15eadac9bf0935b0b00a775fb8caed5b84
SunDawning <dpmeichen@gmail.com>
2022-02-22 21:20:45 +0800

    Cesium场景导出为图片功能 https://blog.csdn.net/xietao20/article/details/109401838

--- projects/2022-02-19-租房/src/index.js
+++ projects/2022-02-19-租房/src/index.js
@@ -339,3 +339,30 @@ function setGlobeToRectangle(viewer) {
     });
   });
 }
+function snapshot() {
+  // Cesium场景导出为图片功能 https://blog.csdn.net/xietao20/article/details/109401838
+  function saveToFile(scene) {
+    let canvas = scene.canvas;
+    let image = canvas
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
+    let link = document.createElement("a");
+    let blob = dataURLtoBlob(image);
+    let objurl = URL.createObjectURL(blob);
+    link.download = "scene.png";
+    link.href = objurl;
+    link.click();
+  }
+  function dataURLtoBlob(dataurl) {
+    let arr = dataurl.split(","),
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]),
+      n = bstr.length,
+      u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], { type: mime });
+  }
+  saveToFile(viewer.scene);
+}

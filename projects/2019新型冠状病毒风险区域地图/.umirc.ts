16a3c355f984800a799f4dc8144e334cfee8bcbf
SunDawning <dpmeichen@gmail.com>
2022-03-05 13:56:39 +0800

    分离出Cesium的配置

--- projects/2019新型冠状病毒风险区域地图/.umirc.ts
+++ projects/2019新型冠状病毒风险区域地图/.umirc.ts
@@ -1,13 +1,27 @@
 import { defineConfig } from 'umi';
-export default defineConfig({
+const config = {
   nodeModulesTransform: {
     type: 'none',
   },
   fastRefresh: {},
-  copy: [
-    // Cesum文件
-    { from: 'node_modules/cesium/Build/Cesium/Workers', to: 'Cesium/Workers' },
-    { from: 'node_modules/cesium/Build/Cesium/Assets', to: 'Cesium/Assets' },
-    { from: 'node_modules/cesium/Build/Cesium/Widgets', to: 'Cesium/Widgets' },
-  ],
-});
+};
+// 配置Cesium
+{
+  const CESIUM_BASE_URL = 'Cesium/';
+  // 定义Cesium的内部变量CESIUM_BASE_URL
+  if (config.define === undefined) {
+    config.define = {};
+  }
+  config.define.CESIUM_BASE_URL = CESIUM_BASE_URL;
+  // 复制Cesium的静态图片等到文件夹下
+  if (config.copy === undefined) {
+    config.copy = [];
+  }
+  ['Workers', 'Assets', 'Widgets'].forEach(function (item) {
+    config.copy.push({
+      from: 'node_modules/cesium/Build/Cesium/' + item,
+      to: CESIUM_BASE_URL + item,
+    });
+  });
+}
+export default defineConfig(config);
--- projects/2019新型冠状病毒风险区域地图/src/components/CesiumJSDefaultViewer/index.tsx
+++ projects/2019新型冠状病毒风险区域地图/src/components/CesiumJSDefaultViewer/index.tsx
@@ -1,19 +1,15 @@
 import { useEffect } from 'react';
-import { Viewer } from 'cesium';
 import styles from './index.less';
+import { Viewer } from 'cesium';
 import 'cesium/Build/Cesium/Widgets/widgets.css';
 /**
  * CesiumJS默认的界面
  */
 export default function IndexPage() {
   const className = styles.container;
-  window.CESIUM_BASE_URL = 'Cesium';
   useEffect(function () {
     const container = document.querySelector('.' + className);
     const viewer = new Viewer(container);
-    return function () {
-      delete window.CESIUM_BASE_URL;
-    };
   }, []);
   return <div className={className}></div>;
 }

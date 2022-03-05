18ee5c22611eaa12c7d4d86b3c30ddd5982e42a7
SunDawning <dpmeichen@gmail.com>
2022-03-05 12:38:37 +0800

    修复Module ./cesium/Build/Cesium/Widgets/widgets.css does not exist in container. while loading ./cesium/Build/Cesium/Widgets/widgets.css from webpack/container/reference/mf

--- projects/2019新型冠状病毒风险区域地图/src/components/CesiumJSDefaultViewer/index.tsx
+++ projects/2019新型冠状病毒风险区域地图/src/components/CesiumJSDefaultViewer/index.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react';
-import styles from './index.less';
 import { Viewer } from 'cesium';
+import styles from './index.less';
+import '@/../node_modules/cesium/Build/Cesium/Widgets/widgets.css';
 /**
  * CesiumJS默认的界面
  */
 export default function IndexPage() {
   const className = styles.container;
-  window.CESIUM_BASE_URL = '/Cesium';
+  window.CESIUM_BASE_URL = 'Cesium';
   useEffect(function () {
     const container = document.querySelector('.' + className);
     const viewer = new Viewer(container);
@@ -14,14 +15,5 @@ export default function IndexPage() {
       delete window.CESIUM_BASE_URL;
     };
   }, []);
-  return (
-    <div className={styles.root}>
-      <link
-        type="text/css"
-        rel="stylesheet"
-        href="Cesium/Widgets/widgets.css"
-      ></link>
-      <div className={className}></div>
-    </div>
-  );
+  return <div className={className}></div>;
 }

c95101222ad453220b012d43ed0c337df489998b
SunDawning <dpmeichen@gmail.com>
2022-03-06 11:08:07 +0800

    组件TemporaryCesiumWidget里增加按钮“全球”

--- projects/2019新型冠状病毒风险区域地图/src/components/TemporaryCesiumWidget/index.tsx
+++ projects/2019新型冠状病毒风险区域地图/src/components/TemporaryCesiumWidget/index.tsx
@@ -1,12 +1,15 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './index.less';
 import createWidget from '@/modules/createDefaultWidget';
 import ArcGIS_World_Imagery from '@/modules/ArcGIS_World_Imagery';
 import Sky_Atmosphere from '@/modules/Sandcastle_Sky_Atmosphere';
+import { Button } from 'antd';
 /**
  * 基于CesiumJS默认的简化界面所配置的界面
  */
 export default function IndexPage() {
+  let scene;
+  let camera;
   const className = styles.container;
   useEffect(function () {
     const container = document.querySelector('.' + className);
@@ -16,12 +19,23 @@ export default function IndexPage() {
       return;
     }
     const widget = createWidget(container);
-    const { scene } = widget;
+    scene = widget.scene;
+    camera = widget.camera;
     scene.imageryLayers.addImageryProvider(ArcGIS_World_Imagery);
     scene.skyAtmosphere = Sky_Atmosphere;
     return function () {
       widget.destroy();
     };
   }, []);
-  return <div className={className}></div>;
+  function onClick(event) {
+    console.log('onClick', event, event.target.innerText);
+    camera.flyHome();
+  }
+  return (
+    <div className={className}>
+      <Button type="primary" style={{ position: 'absolute' }} onClick={onClick}>
+        全球
+      </Button>
+    </div>
+  );
 }

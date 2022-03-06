f2464e3f5adbb0bc323ead69d579bf8cc8f3c038
SunDawning <dpmeichen@gmail.com>
2022-03-06 11:47:18 +0800

    在右侧边栏里选择查看的区域

--- projects/2019新型冠状病毒风险区域地图/src/components/TemporaryCesiumWidget/index.tsx
+++ projects/2019新型冠状病毒风险区域地图/src/components/TemporaryCesiumWidget/index.tsx
@@ -3,14 +3,14 @@ import styles from './index.less';
 import createWidget from '@/modules/createDefaultWidget';
 import ArcGIS_World_Imagery from '@/modules/ArcGIS_World_Imagery';
 import Sky_Atmosphere from '@/modules/Sandcastle_Sky_Atmosphere';
-import { Button } from 'antd';
+import { Drawer, Button } from 'antd';
+let scene;
+let camera;
+const className = styles.container;
 /**
  * 基于CesiumJS默认的简化界面所配置的界面
  */
 export default function IndexPage() {
-  let scene;
-  let camera;
-  const className = styles.container;
   useEffect(function () {
     const container = document.querySelector('.' + className);
     // Argument of type 'Element | null' is not assignable to parameter of type 'Element'.
@@ -27,15 +27,28 @@ export default function IndexPage() {
       widget.destroy();
     };
   }, []);
+  const [visible, setVisible] = useState(false);
   function onClick(event) {
     console.log('onClick', event, event.target.innerText);
+    setVisible(true);
+  }
+  function onClose(event) {
+    setVisible(false);
+  }
+  function onClickOne(event) {
+    setVisible(false);
     camera.flyHome();
   }
   return (
     <div className={className}>
       <Button type="primary" style={{ position: 'absolute' }} onClick={onClick}>
-        全球
+        选择查看的区域
       </Button>
+      <Drawer title="选择查看的区域" visible={visible} onClose={onClose}>
+        <Button type="primary" onClick={onClickOne}>
+          全球
+        </Button>
+      </Drawer>
     </div>
   );
 }

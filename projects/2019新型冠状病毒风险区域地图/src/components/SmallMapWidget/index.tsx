a4f35a5221087522f43fb65e004688f8dd2a3c44
SunDawning <dpmeichen@gmail.com>
2022-03-14 21:57:51 +0800

    小地图与大地图同步视角

--- projects/2019新型冠状病毒风险区域地图/src/components/SmallMapWidget/index.tsx
+++ projects/2019新型冠状病毒风险区域地图/src/components/SmallMapWidget/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-import { SceneMode } from 'cesium';
+import { SceneMode, Cartesian3 } from 'cesium';
 import createWidget from '@/modules/createDefaultWidget';
 import Amap_webrd_Imagery from '@/modules/Amap_webrd_Imagery';
 
@@ -8,11 +8,12 @@ import styles from './index.less';
 
 const className = styles.container;
 let scene;
+let camera;
 
 /**
  * 小地图
  */
-export default function IndexPage() {
+export default function IndexPage({ worldPosition, distance }) {
   useEffect(function () {
     const container = document.querySelector('.' + className);
     if (container === null) {
@@ -20,11 +21,25 @@ export default function IndexPage() {
     }
     const widget = createWidget(container);
     scene = widget.scene;
-    scene.mode = SceneMode.SCENE2D;
+    camera = widget.camera;
+    // scene.mode = SceneMode.SCENE2D;
     scene.imageryLayers.addImageryProvider(Amap_webrd_Imagery);
     return function () {
       widget.destroy();
     };
   }, []);
+  useEffect(
+    function () {
+      if (worldPosition === undefined) {
+        return;
+      }
+      if (distance === undefined) {
+        return;
+      }
+      // console.log(worldPosition, distance);
+      camera.lookAt(worldPosition, new Cartesian3(0.0, 0.0, distance));
+    },
+    [worldPosition, distance],
+  );
   return <div className={styles.container}></div>;
 }
--- projects/2019新型冠状病毒风险区域地图/src/components/TemporaryCesiumWidget/index.tsx
+++ projects/2019新型冠状病毒风险区域地图/src/components/TemporaryCesiumWidget/index.tsx
@@ -1,5 +1,6 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
+import { Cartesian2, defined, Cartesian3 } from 'cesium';
 import createWidget from '@/modules/createDefaultWidget';
 import ArcGIS_World_Imagery from '@/modules/ArcGIS_World_Imagery';
 
@@ -11,11 +12,28 @@ import SmallMapWidget from '@/components/SmallMapWidget';
 let scene;
 let camera;
 const className = styles.container;
+let canvas;
 
 /**
  * 基于CesiumJS默认的简化界面所配置的界面
  */
 export default function IndexPage() {
+  const [worldPosition, setWorldPosition] = useState();
+  const [distance, setDistance] = useState();
+  function onChanged() {
+    if (canvas === undefined) {
+      return;
+    }
+    const viewCenter = new Cartesian2(
+      Math.floor(canvas.clientWidth / 2),
+      Math.floor(canvas.clientHeight / 2),
+    );
+    const newWorldPosition = camera.pickEllipsoid(viewCenter);
+    if (defined(newWorldPosition)) {
+      setWorldPosition(newWorldPosition);
+      setDistance(Cartesian3.distance(newWorldPosition, camera.positionWC));
+    }
+  }
   useEffect(function () {
     const container = document.querySelector('.' + className);
     // Argument of type 'Element | null' is not assignable to parameter of type 'Element'.
@@ -26,15 +44,20 @@ export default function IndexPage() {
     const widget = createWidget(container);
     scene = widget.scene;
     camera = widget.camera;
+    canvas = widget.canvas;
     scene.imageryLayers.addImageryProvider(ArcGIS_World_Imagery);
     scene.skyAtmosphere = Sky_Atmosphere;
+    camera.changed.addEventListener(onChanged);
     return function () {
       widget.destroy();
     };
   }, []);
   return (
     <div className={styles.container}>
-      <SmallMapWidget></SmallMapWidget>
+      <SmallMapWidget
+        worldPosition={worldPosition}
+        distance={distance}
+      ></SmallMapWidget>
     </div>
   );
 }

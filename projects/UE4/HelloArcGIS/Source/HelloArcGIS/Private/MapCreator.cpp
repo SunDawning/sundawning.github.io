758b8528c0ed2cd40f022fc8274b21210c70d383
SunDawning <dpmeichen@gmail.com>
2022-03-16 19:47:23 +0800

    ArcGIS API 4.x for Js 点击地图获取经纬度 https://blog.csdn.net/qq_41615323/article/details/115202387

---
+++ projects/UE4/HelloArcGIS/Source/HelloArcGIS/Private/MapCreator.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MapCreator.h"
+
+// Sets default values
+AMapCreator::AMapCreator()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMapCreator::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMapCreator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+

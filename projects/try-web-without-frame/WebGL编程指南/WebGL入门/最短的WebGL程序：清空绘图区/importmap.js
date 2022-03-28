3ca533ecb389b3c6fdec6aaa88a990db71291fdd
SunDawning <dpmeichen@gmail.com>
2022-03-28 21:31:15 +0800

    分离出importmapHelper.js，便于在其它地方使用。

--- projects/try-web-without-frame/WebGL编程指南/WebGL入门/最短的WebGL程序：清空绘图区/importmap.js
+++ projects/try-web-without-frame/WebGL编程指南/WebGL入门/最短的WebGL程序：清空绘图区/importmap.js
@@ -1,15 +1,7 @@
 /**
- * 添加外部importmap
- * @param {object} content importmap的内容，一个JSON对象。
- * @see https://github.com/WICG/import-maps/issues/235#issuecomment-1002340944
+ * import映射，一个网页里只允许存在一个。
  */
-function importmap(content) {
-  const script = document.createElement("script");
-  script.type = "importmap";
-  script.textContent = JSON.stringify(content);
-  document.currentScript.after(script);
-}
-importmap({
+window.importmap({
   imports: {
     Canvas: "./Canvas.js",
   },

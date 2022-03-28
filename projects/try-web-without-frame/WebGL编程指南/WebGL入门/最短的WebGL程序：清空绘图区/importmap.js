b7ee7712d83155ded626cfdc7d64f70566f8ef73
SunDawning <dpmeichen@gmail.com>
2022-03-28 21:23:31 +0800

    分离出importmap功能，便于添加其他importmap。

--- projects/try-web-without-frame/WebGL编程指南/WebGL入门/最短的WebGL程序：清空绘图区/importmap.js
+++ projects/try-web-without-frame/WebGL编程指南/WebGL入门/最短的WebGL程序：清空绘图区/importmap.js
@@ -1,8 +1,16 @@
-const script = document.createElement("script");
-script.type = "importmap";
-script.textContent = JSON.stringify({
+/**
+ * 添加外部importmap
+ * @param {object} content importmap的内容，一个JSON对象。
+ * @see https://github.com/WICG/import-maps/issues/235#issuecomment-1002340944
+ */
+function importmap(content) {
+  const script = document.createElement("script");
+  script.type = "importmap";
+  script.textContent = JSON.stringify(content);
+  document.currentScript.after(script);
+}
+importmap({
   imports: {
     Canvas: "./Canvas.js",
   },
 });
-document.currentScript.after(script);

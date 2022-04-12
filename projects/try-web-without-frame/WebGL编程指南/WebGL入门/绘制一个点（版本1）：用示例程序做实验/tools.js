fe637011886e709a01087053e39594aa05c9be73
sundawning <dpmeichen@gmail.com>
2022-04-12 21:32:03 +0800

    新增“绘制一个点（版本1）：用示例程序做实验”

---
+++ projects/try-web-without-frame/WebGL编程指南/WebGL入门/绘制一个点（版本1）：用示例程序做实验/tools.js
@@ -0,0 +1,26 @@
+/**
+ * 创建WebGL程序
+ * @param {object} gl WebGL对象
+ * @param {string} vertex 顶点着色器的文本
+ * @param {string} fragment 片元着色器的文本
+ */
+function initShaders(gl, vertex, fragment) {
+  const program = gl.createProgram();
+  [
+    {
+      type: gl.VERTEX_SHADER,
+      source: vertex,
+    },
+    {
+      type: gl.FRAGMENT_SHADER,
+      source: fragment,
+    },
+  ].forEach(function ({ type, source }) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    gl.attachShader(program, shader);
+  });
+  gl.linkProgram(program);
+  gl.useProgram(program);
+}

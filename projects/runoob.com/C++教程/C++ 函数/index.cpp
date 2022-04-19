61b5ed38f4d2c50777efbb0f8c0cde211124ed58
sundawning <dpmeichen@gmail.com>
2022-04-19 21:36:11 +0800

    使用函数max来比较两个数字的大小

--- projects/runoob.com/C++教程/C++ 函数/index.cpp
+++ projects/runoob.com/C++教程/C++ 函数/index.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include <string>
+int max(int m, int n)
+{
+    if (m > n)
+    {
+        return m;
+    }
+    else
+    {
+        return n;
+    }
+}
 int main()
 {
     int m = 1;
     int n = 2;
-    std::string out = "Max value is:" + std::to_string(m) + std::to_string(n);
+    std::string out = "Max value is:" + std::to_string(max(m, n));
     std::cout << out << std::endl;
     getchar();
 }

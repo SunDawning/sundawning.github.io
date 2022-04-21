d15028bd900c6e55cc87ac4f32c515e99745d30b
sundawning <dpmeichen@gmail.com>
2022-04-21 21:37:04 +0800

    显示地图

--- projects/node/npm/vue/HelloMars3D/src/components/Map.vue
+++ projects/node/npm/vue/HelloMars3D/src/components/Map.vue
@@ -1,7 +1,26 @@
 <script setup>
 import { onMounted } from "vue";
 function init(container) {
-  new mars3d.Map(container);
+  const map = new mars3d.Map(container, {
+    scene: {
+      center: {
+        lat: 22.530534267425537,
+        lng: 113.9517791569233,
+        alt: 500,
+        heading: 0,
+        pitch: -90,
+      },
+    },
+    basemaps: [
+      {
+        type: "xyz",
+        url: "http://webrd02.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}",
+        minimumLevel: 3,
+        maximumLevel: 18,
+        show: true,
+      },
+    ],
+  });
 }
 onMounted(function () {
   init(document.querySelector("#map"));

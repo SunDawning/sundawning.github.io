6ba3ca062cb70a6c8ddd08e68c3e345cbeb658b4
sundawning <dpmeichen@gmail.com>
2022-04-22 19:31:30 +0800

    复制node_modules包到public

--- projects/我的地球第一版/vite.config.js
+++ projects/我的地球第一版/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+const plugins = [vue()];
+
+/**
+ * 复制node_modules包到public
+ */
+import copy from "rollup-plugin-copy";
+plugins.push(
+  copy({
+    targets: ["mars3d-cesium", "turf", "mars3d", "mars3d-space"].map(function (
+      item
+    ) {
+      return {
+        src: `./node_modules/${item}/*`,
+        dest: `./public/node_modules/${item}`,
+      };
+    }),
+  })
+);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: plugins,
+});

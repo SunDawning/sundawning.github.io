6a98b7e8a87d730893946929d989d33c2454f9b1
sundawning <dpmeichen@gmail.com>
2022-04-22 20:06:20 +0800

    虚拟、现实

---
+++ projects/我的地球第一版/src/components/Reality.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref, onMounted } from "vue";
+const mapRef = ref(null);
+onMounted(function () {
+  init(mapRef.value);
+});
+</script>
+<script>
+function init(container) {
+  const map = new mars3d.Map(container, {
+    scene: {
+      center: {
+        lat: 22.530534267425537,
+        lng: 113.9517791569233,
+        alt: 500,
+        heading: 0,
+        pitch: -90,
+      },
+    },
+    basemaps: [
+      {
+        type: "xyz",
+        url: "http://webrd02.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}",
+        minimumLevel: 3,
+        maximumLevel: 18,
+        show: true,
+      },
+    ],
+  });
+}
+</script>
+
+<template>
+  <div ref="mapRef"></div>
+</template>
+
+<style></style>
---
+++ projects/我的地球第一版/src/components/Virtualization.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <div>我的地球第一版</div>
+</template>
+<style scoped>
+</style>

49a4a7b6960bfa08f7d332886697fa44ec36779f
sundawning <dpmeichen@gmail.com>
2022-04-23 21:25:25 +0800

    使用mars3d的在线包

--- projects/清远三日游攻略/vue/vite.config.js
+++ projects/清远三日游攻略/vue/vite.config.js
@@ -3,23 +3,6 @@ import vue from "@vitejs/plugin-vue";
 const plugins = [vue()];
 
 /**
- * 复制node_modules包到public
- */
-import copy from "rollup-plugin-copy";
-plugins.push(
-  copy({
-    targets: ["mars3d-cesium", "turf", "mars3d", "mars3d-space"].map(function (
-      item
-    ) {
-      return {
-        src: `./node_modules/${item}/*`,
-        dest: `./public/node_modules/${item}`,
-      };
-    }),
-  })
-);
-
-/**
  * ant-design-vue
  */
 import Components from "unplugin-vue-components/vite";

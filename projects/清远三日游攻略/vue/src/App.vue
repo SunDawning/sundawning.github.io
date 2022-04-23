8a0e836c66f2d62e03810b7c5088e71a0c52e3a7
sundawning <dpmeichen@gmail.com>
2022-04-23 15:11:49 +0800

    显示地球，具有地形、地图和城市白膜

--- projects/清远三日游攻略/vue/src/App.vue
+++ projects/清远三日游攻略/vue/src/App.vue
@@ -1,9 +1,54 @@
 <template>
-  <a-col>
-    <a-row>
-      <a-col :span="6">顶部</a-col>
-    </a-row>
-    <a-row>内容</a-row>
-    <a-row>底部</a-row>
-  </a-col>
+  <a-col> <div ref="mapRef"></div></a-col>
 </template>
+<script setup>
+import { ref, onMounted } from "vue";
+const mapRef = ref(null);
+onMounted(function () {
+  init(mapRef.value);
+});
+</script>
+<script>
+function init(container) {
+  const map = new mars3d.Map(container, {
+    scene: {
+      center: {
+        lat: 22.530534267425537,
+        lng: 113.9517791569233,
+        alt: 500,
+        heading: 0,
+        pitch: -90,
+      },
+      globe: {
+        depthTestAgainstTerrain: true,
+      },
+    },
+    terrain: {
+      url: "http://data.mars3d.cn/terrain",
+      show: true,
+    },
+    basemaps: [
+      {
+        name: "高德电子",
+        type: "gaode",
+        layer: "vec",
+        show: true,
+      },
+    ],
+  });
+  /**
+   * 全球城市白膜(OSM在线)
+   * http://mars3d.cn/editor.html?id=layer-tileset/type/osmBuildings
+   */
+  {
+    const tiles3dLayer = new mars3d.layer.OsmBuildingsLayer({
+      highlight: {
+        type: "click",
+        color: "#00FF00",
+      },
+      popup: "all",
+    });
+    map.addLayer(tiles3dLayer);
+  }
+}
+</script>

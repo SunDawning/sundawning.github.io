e690b3b146871704b7dac97e1dac7ce3ee7ce052
sundawning <dpmeichen@gmail.com>
2022-04-23 13:01:32 +0800

    添加包ant-design-vue

--- projects/清远三日游攻略/vue/src/App.vue
+++ projects/清远三日游攻略/vue/src/App.vue
@@ -1,21 +1,21 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <a-col>
+    <a-row>
+      <a-col :span="6">顶部</a-col>
+    </a-row>
+    <a-row>内容</a-row>
+    <a-row>底部</a-row>
+  </a-col>
 </template>
+<script>
+const options = { components: {} };
+const { components } = options;
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+import { Row } from "ant-design-vue";
+components["a-row"] = Row;
+
+import { Col } from "ant-design-vue";
+components["a-col"] = Col;
+
+export default options;
+</script>
--- projects/清远三日游攻略/vue/vite.config.js
+++ projects/清远三日游攻略/vue/vite.config.js
@@ -19,6 +19,17 @@ plugins.push(
   })
 );
 
+/**
+ * ant-design-vue
+ */
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+plugins.push(
+  Components({
+    resolvers: [AntDesignVueResolver()],
+  })
+);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: plugins,

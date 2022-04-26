a4af1bf14c6a27f4676870991fb9e1ea8287d3e6
sundawning <dpmeichen@gmail.com>
2022-04-26 20:04:02 +0800

    修改引用变量的值后，变量的值也随即发生相同的变化

--- projects/runoob.com/C++教程/C++引用/index.cpp
+++ projects/runoob.com/C++教程/C++引用/index.cpp
@@ -5,5 +5,7 @@ int main()
     a = 1.5;
     double &r = a;
     std::cout << a << "," << r << std::endl; // 1.5,1.5
+    r = 0.5;
+    std::cout << a << "," << r << std::endl; // 0.5,0.5
     getchar();
 }

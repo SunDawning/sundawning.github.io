7f35e55d3861421c33b55be613626c5fedf30397
sundawning <dpmeichen@gmail.com>
2022-04-26 20:08:40 +0800

    把引用作为函数参数

--- projects/runoob.com/C++教程/C++引用/index.cpp
+++ projects/runoob.com/C++教程/C++引用/index.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+void swap(int &a, int &b)
+{
+    int c = a;
+    a = b;
+    b = c;
+    return;
+}
 int main()
 {
     double a;
@@ -7,5 +14,10 @@ int main()
     std::cout << a << "," << r << std::endl; // 1.5,1.5
     r = 0.5;
     std::cout << a << "," << r << std::endl; // 0.5,0.5
+    int c = 1;
+    int d = 2;
+    std::cout << "before: " << c << ',' << d << std::endl;
+    swap(c, d);
+    std::cout << "after: " << c << ',' << d << std::endl;
     getchar();
 }

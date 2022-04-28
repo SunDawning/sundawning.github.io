030dabb10ffe43503867f092b2b5bc9103de84c9
sundawning <dpmeichen@gmail.com>
2022-04-28 21:26:11 +0800

    输出child_process的错误信息

--- projects/cpp-httplib/hello/index.cpp
+++ projects/cpp-httplib/hello/index.cpp
@@ -1,7 +1,9 @@
 #include <cstdio>
-// #include "C:/Users/SunDawning/Downloads/cpp-httplib-0.10.6/httplib.h"
+#include "C:/Users/SunDawning/Downloads/cpp-httplib-0.10.6/httplib.h"
 int main()
 {
     printf("Hello, cpp-httplib\n");
-    // httplib::Client cli("http://cpp-httplib-server.yhirose.repl.co");
+    httplib::Client client("http://cpp-httplib-server.yhirose.repl.co");
+    auto response = client.Get("/hi");
+    printf("%s", response->body);
 }
--- projects/cpp-httplib/hello/index.js
+++ projects/cpp-httplib/hello/index.js
@@ -1,13 +1,23 @@
 const child_process = require("child_process");
 const fs = require("fs");
-const build_directory = "./build";
-if (fs.existsSync(build_directory) === false) {
-  fs.mkdirSync(build_directory);
+let DATABASE = {
+  build_directory: "./build",
+  exe_file_name: "index.exe",
+  cpp_httplib_include_directory:
+    "C:/Users/SunDawning/Downloads/cpp-httplib-0.10.6",
+};
+let { build_directory, exe_file_name, cpp_httplib_include_directory } =
+  DATABASE;
+{
+  if (fs.existsSync(build_directory) === false) {
+    fs.mkdirSync(build_directory);
+  }
 }
 {
-  const child = child_process.exec(
-    `g++ -g index.cpp -o ${build_directory}/index.exe && cd ./build && index.exe`
-  );
+  const command = `g++ -g index.cpp -o "${build_directory}/${exe_file_name}" -I "${cpp_httplib_include_directory}" && cd "${build_directory}" && ${exe_file_name}`;
+  console.log("执行命令", command);
+  const child = child_process.exec(command);
+  child.stderr.pipe(process.stderr);
   child.stdout.pipe(process.stdout);
   child.stdin.pipe(process.stdin);
 }

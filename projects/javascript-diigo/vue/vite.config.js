6ba3670fb0afd82e09889d603b07108bb4136c8f
sundawning <dpmeichen@gmail.com>
2022-05-01 15:51:05 +0800

    添加ant-design-vue

--- projects/javascript-diigo/vue/vite.config.js
+++ projects/javascript-diigo/vue/vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+let plugins = [vue()];
+
+/**
+ * ant-design-vue
+ */
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+plugins.push(
+  Components({
+    resolvers: [AntDesignVueResolver()],
+  })
+);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [vue()],
+  plugins: plugins,
 });

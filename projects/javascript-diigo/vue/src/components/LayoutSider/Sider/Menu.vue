47ee8e616ebebd9d45912aa6907a6ae16df2ff0a
sundawning <dpmeichen@gmail.com>
2022-05-02 09:44:27 +0800

    点击菜单栏里的“最近书签”后输出“显示最近书签”

--- projects/javascript-diigo/vue/src/components/LayoutSider/Sider/Menu.vue
+++ projects/javascript-diigo/vue/src/components/LayoutSider/Sider/Menu.vue
@@ -3,8 +3,13 @@
     <a-menu-item key="1">
       <span>新建书签</span>
     </a-menu-item>
-    <a-menu-item key="2">
+    <a-menu-item key="2" @click="displayRecent">
       <span>最近书签</span>
     </a-menu-item>
   </a-menu>
 </template>
+<script setup>
+function displayRecent() {
+  console.log("显示最近书签");
+}
+</script>

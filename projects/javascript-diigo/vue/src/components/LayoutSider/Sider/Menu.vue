590470731df5fc0a76845737335790b192eb9123
sundawning <dpmeichen@gmail.com>
2022-05-02 10:23:53 +0800

    点击菜单里的选项里输出信息

--- projects/javascript-diigo/vue/src/components/LayoutSider/Sider/Menu.vue
+++ projects/javascript-diigo/vue/src/components/LayoutSider/Sider/Menu.vue
@@ -1,15 +1,15 @@
 <template>
-  <a-menu theme="dark" mode="inline">
-    <a-menu-item key="1">
+  <a-menu theme="dark" mode="inline" @select="select">
+    <a-menu-item key="新建书签">
       <span>新建书签</span>
     </a-menu-item>
-    <a-menu-item key="2" @click="displayRecent">
+    <a-menu-item key="最近书签">
       <span>最近书签</span>
     </a-menu-item>
   </a-menu>
 </template>
 <script setup>
-function displayRecent() {
-  console.log("显示最近书签");
+function select({ item, key, selectedKeys }) {
+  console.log("{ item, key, selectedKeys }", { item, key, selectedKeys });
 }
 </script>

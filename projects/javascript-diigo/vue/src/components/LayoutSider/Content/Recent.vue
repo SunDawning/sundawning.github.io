90a6eac29945db1c9c9e52c46f4755cffb37115e
sundawning <dpmeichen@gmail.com>
2022-05-02 13:11:42 +0800

    显示真实的“最近书签”内容

--- projects/javascript-diigo/vue/src/components/LayoutSider/Content/Recent.vue
+++ projects/javascript-diigo/vue/src/components/LayoutSider/Content/Recent.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>最近书签</div>
+  <a-list :data-source="list" item-layout="horizontal">
+    <template #renderItem="{ item }">
+      <a-list-item>
+        <div>
+          <a :href="item.url" target="_blank">{{ item.title }}</a>
+          <div>{{ item.description }}</div>
+        </div>
+      </a-list-item>
+    </template>
+  </a-list>
 </template>
 <script setup>
 import axios from "axios";
+import { ref } from "vue";
+const list = ref([]);
 async function index() {
   const count = 3;
-  const { status, statusText, data } = await axios.get(
+  const { data } = await axios.get(
     "http://localhost:3001/interact_api/load_user_items",
     {
       params: {
@@ -17,6 +28,7 @@ async function index() {
     }
   );
   console.log("response.data", data);
+  list.value = data.items;
 }
 index();
 </script>

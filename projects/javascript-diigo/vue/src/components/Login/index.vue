27a493bf44fdfda4a28256721eeb77e0cd2062e4
sundawning <dpmeichen@gmail.com>
2022-05-02 21:33:19 +0800

    在验证验证的时候，显示“加载中”的状态

--- projects/javascript-diigo/vue/src/components/Login/index.vue
+++ projects/javascript-diigo/vue/src/components/Login/index.vue
@@ -1,19 +1,30 @@
 <template>
-  <a-form @submit="login">
-    <a-form-item label="Cookie" name="cookie">
-      <a-textarea auto-size></a-textarea>
-    </a-form-item>
-    <a-form-item>
-      <a-button type="primary" html-type="submit">登录</a-button>
-    </a-form-item>
-  </a-form>
+  <a-spin :spinning="status.spinning">
+    <a-form @submit="login">
+      <a-form-item label="Cookie" name="cookie">
+        <a-textarea auto-size></a-textarea>
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" html-type="submit">登录</a-button>
+      </a-form-item>
+    </a-form>
+  </a-spin>
 </template>
 <script setup>
+import { reactive } from "vue";
 import axios from "axios";
+const status = reactive({
+  spinning: true,
+});
 const emit = defineEmits(["login"]);
 if (localStorage.getItem("diigo_cookie")) {
   login();
+} else {
+  status.spinning = false;
 }
+/**
+ * 登录
+ */
 async function login() {
   const { data } = await axios.get("http://localhost:3001/outliner/list", {
     headers: {

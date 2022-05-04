445ede0a002e73fe5379486db24a56b6efc00be2
sundawning <dpmeichen@gmail.com>
2022-05-05 07:57:29 +0800

    菜单栏增加“退出登录”

--- projects/javascript-diigo/vue/src/components/LayoutSider/Sider/Menu.vue
+++ projects/javascript-diigo/vue/src/components/LayoutSider/Sider/Menu.vue
@@ -6,12 +6,20 @@
     <a-menu-item key="最近书签">
       <span>最近书签</span>
     </a-menu-item>
+    <a-menu-item key="退出登录">
+      <span>退出登录</span>
+    </a-menu-item>
   </a-menu>
 </template>
 <script setup>
-const emit = defineEmits(["displayRecent"]);
+const emit = defineEmits(["displayRecent", "login"]);
 function select({ item, key, selectedKeys }) {
-  console.log("{ item, key, selectedKeys }", { item, key, selectedKeys });
+  // console.log("{ item, key, selectedKeys }", { item, key, selectedKeys });
+  if (key === "退出登录") {
+    localStorage.removeItem("diigo_cookie");
+    window.location.reload();
+    return;
+  }
   emit("displayRecent", false);
   if (key === "最近书签") {
     emit("displayRecent", true);

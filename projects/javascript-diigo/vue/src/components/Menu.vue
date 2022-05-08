2a8d4feb638770537feaf361e6bbd13cba453163
sundawning <dpmeichen@gmail.com>
2022-05-08 09:54:58 +0800

    明确菜单栏点击之后的功能

--- projects/javascript-diigo/vue/src/components/Menu.vue
+++ projects/javascript-diigo/vue/src/components/Menu.vue
@@ -17,25 +17,25 @@ import { drop } from "../modules/auth";
 const emit = defineEmits();
 function select({ item, key, selectedKeys }) {
   // console.log("{ item, key, selectedKeys }", { item, key, selectedKeys });
+  // 1. 关闭所有内容：欢迎、新增书签、最近书签等等
   emit("displayWelcome", false);
   emit("displayEdit", false);
   emit("displayRecent", false);
-  if (key === "欢迎") {
-    emit("displayWelcome", true);
-    return;
-  }
-  if (key === "新建书签") {
-    emit("displayEdit", true);
-    return;
-  }
-  if (key === "最近书签") {
-    emit("displayRecent", true);
-    return;
-  }
+  // 2. 打开相应内容：登录、欢迎、新增书签、最近书签等等
+  // 2.1 打开相应内容：登录等等
   if (key === "退出登录") {
     drop();
     emit("login", false);
     return;
   }
+  // 2.2 打开相应内容：欢迎、新增书签、最近书签等等
+  emit(
+    {
+      欢迎: "displayWelcome",
+      新建书签: "displayEdit",
+      最近书签: "displayRecent",
+    }[key],
+    true
+  );
 }
 </script>

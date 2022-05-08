11c9b57ef22797665bc43c7f67ab74fd379925c1
sundawning <dpmeichen@gmail.com>
2022-05-08 14:39:52 +0800

    “新增书签”页面显示“网址”输入框

--- projects/javascript-diigo/vue/src/components/Edit.vue
+++ projects/javascript-diigo/vue/src/components/Edit.vue
@@ -1,3 +1,10 @@
 <template>
-  <a-row>编辑书签</a-row>
+  <a-form>
+    <a-form-item label="网址" name="url" :rules="[{ required: true }]">
+      <a-input></a-input>
+    </a-form-item>
+    <a-form-item>
+      <a-button type="primary" html-type="submit">提交</a-button>
+    </a-form-item>
+  </a-form>
 </template>

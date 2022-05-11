e782ac1bbec6bd1d73fd75dd90b92de6d3bc2170
sundawning <dpmeichen@gmail.com>
2022-05-11 20:25:04 +0800

    分离出List.vue，展示所有书签的内容

---
+++ projects/javascript-diigo/vue/src/components/List.vue
@@ -0,0 +1,35 @@
+<template>
+  <a-list :data-source="items">
+    <template #renderItem="{ item }">
+      <a-list-item>
+        <div>
+          <h1>
+            <a :href="item.url" target="_blank">{{ item.title }}</a>
+          </h1>
+          <pre>{{ item.description }}</pre>
+        </div>
+      </a-list-item>
+    </template>
+  </a-list>
+</template>
+<script setup>
+defineProps({
+  items: Array,
+});
+</script>
+<style scoped>
+.ant-list {
+  text-align: left;
+}
+pre {
+  white-space: pre-wrap;
+  word-break: break-all;
+  margin: 0;
+  border-left: 8px solid #f99f30;
+  border-radius: 8px;
+  padding: 8px;
+  background: #3f98e5;
+  color: #ffffff;
+  max-height: 480px;
+}
+</style>
--- projects/javascript-diigo/vue/src/components/Recent.vue
+++ projects/javascript-diigo/vue/src/components/Recent.vue
@@ -1,22 +1,13 @@
 <template>
-  <a-list :data-source="list">
-    <template #renderItem="{ item }">
-      <a-list-item>
-        <div>
-          <h1>
-            <a :href="item.url" target="_blank">{{ item.title }}</a>
-          </h1>
-          <pre>{{ item.description }}</pre>
-        </div>
-      </a-list-item>
-    </template>
-  </a-list>
+  <List :items="state.items"></List>
 </template>
 <script setup>
 import axios from "../modules/axios";
-import { ref } from "vue";
+import { reactive } from "vue";
 import { select } from "../modules/auth";
-const list = ref([]);
+const state = reactive({
+  items: [],
+});
 async function index() {
   const { data } = await axios({
     method: "GET",
@@ -31,23 +22,7 @@ async function index() {
     },
   });
   console.log("response.data", data);
-  list.value = data.items;
+  state.items = data.items;
 }
 index();
 </script>
-<style scoped>
-.ant-list {
-  text-align: left;
-}
-pre {
-  white-space: pre-wrap;
-  word-break: break-all;
-  margin: 0;
-  border-left: 8px solid #f99f30;
-  border-radius: 8px;
-  padding: 8px;
-  background: #3f98e5;
-  color: #ffffff;
-  max-height: 480px;
-}
-</style>

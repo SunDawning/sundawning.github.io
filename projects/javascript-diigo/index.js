3eea5f4f0332e83ffea663e747d7aa1816008922
sundawning <dpmeichen@gmail.com>
2022-05-11 20:17:35 +0800

    启动时便从远程更新本地的内容

--- projects/javascript-diigo/index.js
+++ projects/javascript-diigo/index.js
@@ -1,9 +1,9 @@
 const child_process = require("child_process");
-["cd proxy && npm run start", "cd vue && npm run dev"].forEach(function (
-  command
-) {
-  const child = child_process.exec(command);
-  ["stderr", "stdout", "stdin"].forEach(function (location) {
-    child[location].pipe(process[location]);
-  });
-});
+["git pull", "cd proxy && npm run start", "cd vue && npm run dev"].forEach(
+  function (command) {
+    const child = child_process.exec(command);
+    ["stderr", "stdout", "stdin"].forEach(function (location) {
+      child[location].pipe(process[location]);
+    });
+  }
+);

dfbeab94cfa8acbd40957996260a146ef7ab8427
sundawning <dpmeichen@gmail.com>
2022-05-12 23:03:35 +0800

    菜单栏新增“Public”项，用来打开Diigo在线的Public Library

--- projects/javascript-diigo/vue/src/components/Menu.vue
+++ projects/javascript-diigo/vue/src/components/Menu.vue
@@ -7,6 +7,9 @@
     <a-menu-item key="最近书签">
       <span>最近书签</span>
     </a-menu-item>
+    <a-menu-item key="Public">
+      <span>Public</span>
+    </a-menu-item>
     <a-menu-item key="退出登录">
       <span>退出登录</span>
     </a-menu-item>
@@ -14,12 +17,21 @@
 </template>
 <script setup>
 import { drop } from "../modules/auth";
+import { select as getAuth } from "../modules/auth";
 defineProps({
   selectedKeys: Array,
 });
 const emit = defineEmits();
 function select({ item, key, selectedKeys }) {
   // console.log("{ item, key, selectedKeys }", { item, key, selectedKeys });
+  // 链接跳转：打开Public Library
+  if (key === "Public") {
+    const username = getUserName();
+    if (username === undefined) {
+      return;
+    }
+    return window.open(`https://www.diigo.com/profile/${username}`);
+  }
   // 1. 关闭所有内容：欢迎、新增书签、最近书签等等
   emit("displaySearch", false);
   emit("displayWelcome", false);
@@ -41,5 +53,23 @@ function select({ item, key, selectedKeys }) {
     }[key],
     true
   );
+}
+/**
+ * 获取用户名
+ */
+function getUserName() {
+  let object = {};
+  getAuth()
+    .split("; ")
+    .forEach(function (item) {
+      const [key, value] = item.split("=");
+      object[key] = value;
+    });
+  // console.log("object", object);
+  const diigo_and_login_cookie = object["diigoandlogincookie"];
+  if (diigo_and_login_cookie === undefined) {
+    return;
+  }
+  return diigo_and_login_cookie.split("-.-")[1];
 }
 </script>

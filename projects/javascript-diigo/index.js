811f47df16191b4744cda1e6e8d9422c31a0353a
sundawning <dpmeichen@gmail.com>
2022-05-14 10:14:54 +0800

    启动后端服务器和前端服务器之后打开浏览器

--- projects/javascript-diigo/index.js
+++ projects/javascript-diigo/index.js
@@ -1,10 +1,16 @@
 const child_process = require("child_process");
-["git pull", "cd proxy && npm run start", "cd vue && npm run dev"].forEach(
-  exec
-);
-setTimeout(function () {
-  exec("title Diigo服务器");
-}, 5000);
+{
+  // 启动后端服务器和前端服务器
+  ["git pull", "cd proxy && npm run start", "cd vue && npm run dev"].forEach(
+    exec
+  );
+  // 打开浏览器
+  exec("start http://localhost:3000");
+  // 修改命令行的标题
+  setTimeout(function () {
+    exec("title Diigo服务器");
+  }, 5000);
+}
 function exec(command) {
   log("执行命令", command);
   const child = child_process.exec(command);

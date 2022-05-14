def19235dd73fa5b669e7c1c066a934c35992d01
sundawning <dpmeichen@gmail.com>
2022-05-14 10:12:23 +0800

    分离出执行命令和打印的内容

--- projects/javascript-diigo/index.js
+++ projects/javascript-diigo/index.js
@@ -1,12 +1,21 @@
 const child_process = require("child_process");
 ["git pull", "cd proxy && npm run start", "cd vue && npm run dev"].forEach(
-  function (command) {
-    const child = child_process.exec(command);
-    ["stderr", "stdout", "stdin"].forEach(function (location) {
-      child[location].pipe(process[location]);
-    });
-  }
+  exec
 );
 setTimeout(function () {
-  child_process.exec("title Diigo服务器");
+  exec("title Diigo服务器");
 }, 5000);
+function exec(command) {
+  log("执行命令", command);
+  const child = child_process.exec(command);
+  ["stderr", "stdout", "stdin"].forEach(function (location) {
+    child[location].pipe(process[location]);
+  });
+}
+/**
+ * 打印，不同于console.log，带有时间戳。
+ */
+function log() {
+  process.stdout.write(`[${new Date().toLocaleString()}] `);
+  console.log.apply(null, arguments);
+}

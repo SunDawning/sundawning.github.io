fd5521cbd89d98e3e43f5fd0809ea9c3e463656c
sundawning <dpmeichen@gmail.com>
2022-05-14 15:50:06 +0800

    列表展示书签时增加“修改”按钮，点击“修改”按钮之后，就地编辑书签

--- projects/javascript-diigo/vue/src/components/List.vue
+++ projects/javascript-diigo/vue/src/components/List.vue
@@ -1,8 +1,61 @@
 <template>
   <a-list :data-source="items" :split="false">
     <template #renderItem="{ item }">
-      <a-list-item>
+      <a-list-item v-if="item.edit">
+        <a-form
+          :model="item"
+          @finish="finish(values, item)"
+          :labelCol="{ span: 4 }"
+          :wrapperCol="{ span: 18 }"
+        >
+          <a-form-item label="网址" name="url" :rules="[{ required: true }]">
+            <a-textarea
+              auto-size
+              v-model:value="item.url"
+              allowClear
+            ></a-textarea>
+          </a-form-item>
+          <a-form-item label="标题" name="title" :rules="[{ required: true }]">
+            <a-textarea
+              auto-size
+              v-model:value="item.title"
+              allowClear
+            ></a-textarea>
+          </a-form-item>
+          <a-form-item label="标签" name="tags">
+            <a-textarea
+              auto-size
+              v-model:value="item.tags"
+              allowClear
+            ></a-textarea>
+          </a-form-item>
+          <a-form-item label="描述" name="description">
+            <a-textarea
+              auto-size
+              v-model:value="item.description"
+              allowClear
+            ></a-textarea>
+          </a-form-item>
+          <a-form-item label="私有" name="private" class="left">
+            <a-switch v-model:checked="item.private"></a-switch>
+          </a-form-item>
+          <a-form-item label="稍后再读" name="unread" class="left">
+            <a-switch v-model:checked="item.unread"></a-switch>
+          </a-form-item>
+          <a-form-item
+            :wrapperCol="{
+              span: 18,
+              xs: { offset: 0 },
+              sm: { offset: 4 },
+            }"
+          >
+            <a-button type="primary" html-type="submit">提交</a-button>
+          </a-form-item>
+        </a-form></a-list-item
+      >
+      <a-list-item v-else>
         <div>
+          <a-button @click="edit(event, item)">修改</a-button>
           <h1>
             <a :href="item.url" target="_blank">{{ item.title }}</a>
           </h1>
@@ -13,9 +66,20 @@
   </a-list>
 </template>
 <script setup>
+import { reactive } from "vue";
+const state = reactive({
+  edit: false,
+});
 defineProps({
   items: Array,
 });
+function edit(event, item) {
+  console.log("修改", item);
+  item.edit = true;
+}
+function finish(values, item) {
+  item.edit = false;
+}
 </script>
 <style scoped>
 .ant-list {
@@ -41,5 +105,8 @@ pre {
   background: #3f98e5;
   color: #ffffff;
   max-height: 480px;
+}
+form {
+  width: 100%;
 }
 </style>

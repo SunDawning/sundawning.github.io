2e46e104919c0d865ac1cd0c32af705440e93920
sundawning <dpmeichen@gmail.com>
2022-05-15 17:39:01 +0800

    拆分代理服务器的内容

--- projects/javascript-diigo/proxy/index.js
+++ projects/javascript-diigo/proxy/index.js
@@ -3,50 +3,32 @@ const app = new Koa();
 const cors = require("@koa/cors");
 const axios = require("axios");
 const child_process = require("child_process");
-app.use(cors()); // CORS
-app.use(async function (context) {
+const Router = require("koa-router");
+const router = new Router();
+// 检查版本
+router.get(/^\/api\/check-new-version/, async function (context) {
+  const message = child_process.execSync("git pull", {
+    cwd: __dirname,
+    encoding: "utf-8",
+  });
+  log(message);
+  context.response.body = { message };
+  context.response.headers = { "content-type": "application/json" };
+});
+// GET http或https，用于CORS请求
+router.get(/^\/https?:\/\//, async function (context) {
   const { method, url, headers, params } = context.request;
-  // console.log("context.request", context.request);
-  log(`${method} ${url}`);
-  // 以“/api”开头的地址
-  if (url.startsWith("/api")) {
-    {
-      const message = child_process.execSync("git pull", {
-        cwd: __dirname,
-        encoding: "utf-8",
-      });
-      log(message);
-      const response = {
-        data: {
-          message,
-        },
-        headers: {
-          "content-type": "application/json",
-        },
-      };
-      context.response.body = response.data;
-      context.response.headers = response.headers;
-    }
-    return;
-  }
-  // 以“/http:”或“/https:”开头的地址
-  if (url.match(/^\/https?:\/\//) === null) {
-    log("无法代理该网址，仅支持“/http://”或“/https://”开头的网址：", url);
-    return;
-  }
   let realURL = url.substring(1);
   log("realURL", realURL);
   if (headers) {
     ["referer", "host", "origin"].forEach(function (key) {
       delete headers[key];
     });
-    if (method === "GET") {
-      ["content-length", "content-type", "accept-encoding"].forEach(function (
-        key
-      ) {
-        delete headers[key];
-      });
-    }
+    ["content-length", "content-type", "accept-encoding"].forEach(function (
+      key
+    ) {
+      delete headers[key];
+    });
     ["Cookie"].forEach(function (key) {
       key = key.toLowerCase();
       if (headers[`_${key}`] === undefined) {
@@ -62,11 +44,6 @@ app.use(async function (context) {
     params,
     headers,
   };
-  if (method === "POST") {
-    const data = await paresPostData(context);
-    log("data", data);
-    Object.assign(options, { data });
-  }
   log("options", options);
   let response;
   try {
@@ -84,6 +61,39 @@ app.use(async function (context) {
   context.response.body = response.data;
   context.response.headers = response.headers;
 });
+// POST https://www.diigo.com
+router.post(/^\/https:\/\/www.diigo.com/, async function (context) {
+  const { method, url, headers, params } = context.request;
+  // console.log("context.request", context.request);
+  let realURL = url.substring(1);
+  log("realURL", realURL);
+  if (headers) {
+    ["referer", "host", "origin"].forEach(function (key) {
+      delete headers[key];
+    });
+    ["Cookie"].forEach(function (key) {
+      key = key.toLowerCase();
+      if (headers[`_${key}`] === undefined) {
+        return;
+      }
+      headers[key] = headers[`_${key}`];
+      delete headers[`_${key}`];
+    });
+  }
+  let options = {
+    url: realURL,
+    method,
+    params,
+    headers,
+    data: await paresPostData(context),
+  };
+  log("options", options);
+  let response = await axios(options);
+  context.response.body = response.data;
+  context.response.headers = response.headers;
+});
+app.use(router.routes());
+app.use(cors());
 app.listen(3001);
 /**
  * 打印，不同于console.log，带有时间戳。

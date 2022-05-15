8f3efba7c97224badd08ca959a713509597002be
sundawning <dpmeichen@gmail.com>
2022-05-15 10:35:43 +0800

    反馈页面增加表单输入

--- projects/javascript-diigo/vue/src/components/Feedback.vue
+++ projects/javascript-diigo/vue/src/components/Feedback.vue
@@ -1,1 +1,16 @@
-<template><a-row>反馈</a-row></template>
+<template>
+  <a-form :model="formState">
+    <a-form-item label="描述" name="description" :rules="[{ required: true }]">
+      <a-textarea auto-size v-model:value="formState.description"></a-textarea>
+    </a-form-item>
+    <a-form-item>
+      <a-button type="primary" html-type="submit">提交</a-button>
+    </a-form-item>
+  </a-form>
+</template>
+<script setup>
+import { reactive } from "vue";
+const formState = reactive({
+  description: "",
+});
+</script>

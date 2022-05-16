f40dc39918dceb6da518a12dd44a7f989be06a83
sundawning <dpmeichen@gmail.com>
2022-05-16 22:12:58 +0800

    browser：代理服务器

--- projects/javascript-os/browser/vite.config.js
+++ projects/javascript-os/browser/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+const fs = require("fs");
+const log = require("../server/modules/log");
+
+let proxy = {};
+const port = JSON.parse(fs.readFileSync("../server/server.json")).port;
+log("代理服务器端口", port);
+["/http://", "/https://", "/api"].forEach(function (item) {
+  proxy[item] = {
+    target: "http://localhost:" + port,
+    changeOrigin: true,
+  };
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  server: {
+    proxy: proxy,
+  },
+});

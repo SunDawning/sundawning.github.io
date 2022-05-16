9fb31eb785637677621a95b624b715151e3e8a25
jobsimi <jobsimi@qq.com>
2022-05-16 12:37:12 +0800

    搜索：同时搜索“ue4”和“#ue4”，并取出唯一的。

--- projects/javascript-diigo/vue/src/components/Search.vue
+++ projects/javascript-diigo/vue/src/components/Search.vue
@@ -14,11 +14,33 @@ watch(props, function (newProps) {
   index(newProps);
 });
 async function index(props) {
-  const items = await searchBookmarkItems(props.search);
-  console.log("items", items);
+  let items = {};
+  // 搜索“ue4”和“#ue4”，并取出唯一的。
+  const searches = [
+    Object.assign({}, props.search),
+    Object.assign({}, props.search, {
+      what: "#" + props.search.what,
+    }),
+  ];
+  for (let c = 0; c < searches.length; c = c + 1) {
+    const search = searches[c];
+    const searched_items = await searchBookmarkItems(search);
+    if (searched_items === undefined) {
+      continue;
+    }
+    console.log("search", search, searched_items.length);
+    searched_items.forEach(function (item) {
+      const { url } = item;
+      if (items[url] === undefined) {
+        items[url] = item;
+      }
+    });
+  }
+  const items_values = Object.values(items);
+  console.log("items", items_values);
   // 回到顶部
   scrollTo(0, 0);
-  state.items = items;
+  state.items = items_values;
 }
 index(props);
 </script>

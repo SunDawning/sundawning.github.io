417f4be6777636efc542e5144a65df6c439e5229
jobsimi <jobsimi@qq.com>
2022-05-17 09:42:47 +0800

    browser：从外部设置服务器端口和代理服务器的端口

--- projects/javascript-os/browser/vite.config.js
+++ projects/javascript-os/browser/vite.config.js
@@ -2,16 +2,21 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 const fs = require("fs");
 const log = require("../server/modules/log");
+const minimist = require("minimist");
+const argv = minimist(minimist(process.argv)["_"]);
+const { proxy_port } = argv;
 
 let proxy = {};
-const port = JSON.parse(fs.readFileSync("../server/server.json")).port;
-log("代理服务器端口", port);
-["/http://", "/https://", "/api"].forEach(function (item) {
-  proxy[item] = {
-    target: "http://localhost:" + port,
-    changeOrigin: true,
-  };
-});
+
+log("代理服务器端口", proxy_port);
+if (proxy_port) {
+  ["/http://", "/https://", "/api"].forEach(function (item) {
+    proxy[item] = {
+      target: "http://localhost:" + proxy_port,
+      changeOrigin: true,
+    };
+  });
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({

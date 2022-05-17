ec6f072349edb2c19d30f95736003184d223b5dc
sundawning <dpmeichen@gmail.com>
2022-05-17 20:43:42 +0800

    manager：每隔60秒打印一次系统信息

--- projects/javascript-os/manager/index.js
+++ projects/javascript-os/manager/index.js
@@ -2,6 +2,7 @@ const exec = require("manager/modules/exec");
 const check_new_version = require("manager/modules/check-new-version");
 const get_port = require("manager/modules/get-port");
 const get_random_port = require("manager/modules/get-random-port");
+const log = require("server/modules/log");
 index();
 async function index() {
   const browser_port = await get_port({ port: 3000 });
@@ -13,9 +14,17 @@ async function index() {
     `cd ../browser && pnpm run dev -- --port ${browser_port} -- --proxy_port ${server_port}`,
   ].forEach(exec);
   setTimeout(function () {
-    // 打开浏览器
-    // exec(`start http://localhost:${browser_port}`);
+    // 提示浏览器
+    log(`浏览器 http://localhost:${browser_port}`);
     // 修改命令行的标题
     exec("title OS服务器");
   }, 3000);
+  setInterval(function () {
+    log(`
+系统信息：
+
+- 浏览器 http://localhost:${browser_port}
+- 服务器 http://localhost:${server_port}
+`);
+  }, 60 * 1000);
 }

aafc940a48873d77e3eebe4ff1d8bd9ef4671b60
jobsimi <jobsimi@qq.com>
2022-05-17 14:24:49 +0800

    manager：修复get-random-port只是使用随机端口却没有检测是否可用的问题

--- projects/javascript-os/manager/modules/get-random-port/index.js
+++ projects/javascript-os/manager/modules/get-random-port/index.js
@@ -1,7 +1,8 @@
 const get_random_int = require("manager/modules/get-random-int");
+const get_port = require("manager/modules/get-port");
 /**
  * 获取随机可用的端口
  */
 module.exports = async function () {
-  return await get_random_int({ min: 3000, max: 65535 });
+  return await get_port({ port: get_random_int({ min: 3000, max: 65535 }) });
 };

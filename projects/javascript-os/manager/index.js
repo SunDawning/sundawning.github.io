affdbc6876d761c4caddff1338d582fbe41b7a7e
jobsimi <jobsimi@qq.com>
2022-05-17 09:45:20 +0800

    manager：设置server的端口和browser的端口

--- projects/javascript-os/manager/index.js
+++ projects/javascript-os/manager/index.js
@@ -1,14 +1,15 @@
 const exec = require("./modules/exec");
 const server_port = 3001;
+const browser_port = 3000;
 {
   // 启动后端服务器和前端服务器
   [
     "git pull && pnpm install",
-    `cd ../server && npm run start -- --port ${server_port}`,
-    "cd ../browser && npm run dev",
+    `cd ../server && pnpm run start -- --port ${server_port}`,
+    `cd ../browser && pnpm run dev -- --port ${browser_port} -- --proxy_port ${server_port}`,
   ].forEach(exec);
   // 打开浏览器
-  exec("start http://localhost:3000");
+  exec(`start http://localhost:${browser_port}`);
   // 修改命令行的标题
   setTimeout(function () {
     exec("title OS服务器");

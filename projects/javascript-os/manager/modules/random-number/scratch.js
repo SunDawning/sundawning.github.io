6ed17f9680207d2ac2b25dad59ebebc773628ebb
jobsimi <jobsimi@qq.com>
2022-05-17 14:19:47 +0800

    manager：删除random-number

--- projects/javascript-os/manager/modules/random-number/scratch.js
+++
@@ -1,5 +0,0 @@
-const log = require("server/modules/log");
-const random_number = require("./index");
-log(random_number({ min: 10 }));
-log(random_number({ max: 100 }));
-log(random_number({ min: 20, max: 100 }));

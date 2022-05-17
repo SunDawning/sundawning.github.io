d8cd5afaaed79e7624228dd2ac2797490448b989
jobsimi <jobsimi@qq.com>
2022-05-17 13:53:14 +0800

    manager：给get-port增加函数自变量的详情说明

--- projects/javascript-os/manager/modules/get-port/index.js
+++ projects/javascript-os/manager/modules/get-port/index.js
@@ -2,8 +2,9 @@ const portfinder = require("portfinder");
 const log = require("server/modules/log");
 /**
  * 查找可用的端口
- * @param {number} port 开始的端口
- * @param {number} stopPort 结束的端口
+ * @param {object} options
+ * @param {number} options.port 开始的端口
+ * @param {number} options.stopPort 结束的端口
  */
 module.exports = function ({ port, stopPort } = {}) {
   return new Promise(function (resolve, reject) {

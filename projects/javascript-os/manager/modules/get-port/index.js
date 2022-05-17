6bd1885698dcef501478e137a37f18ecda37a3ac
jobsimi <jobsimi@qq.com>
2022-05-17 13:51:27 +0800

    manager：给get-port增加注释

--- projects/javascript-os/manager/modules/get-port/index.js
+++ projects/javascript-os/manager/modules/get-port/index.js
@@ -1,5 +1,10 @@
 const portfinder = require("portfinder");
 const log = require("server/modules/log");
+/**
+ * 查找可用的端口
+ * @param {number} port 开始的端口
+ * @param {number} stopPort 结束的端口
+ */
 module.exports = function ({ port, stopPort } = {}) {
   return new Promise(function (resolve, reject) {
     portfinder.getPort({ port, stopPort }, function (error, port) {

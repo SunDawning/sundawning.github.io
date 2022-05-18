29c2de6ed8ced131a4540f8040d26f4e7a52e0ab
sundawning <dpmeichen@gmail.com>
2022-05-18 21:37:31 +0800

    从本地文件“API_KEY.json”里读取API_KEY

--- projects/javascript-ngrok/index.js
+++ projects/javascript-ngrok/index.js
@@ -1,5 +1,21 @@
 const axios = require("axios");
-
+const fs = require("fs");
+const API_KEY = JSON.parse(fs.readFileSync("API_KEY.json")).token;
+// console.log("API_KEY", API_KEY);
+// get_api_key();
+async function get_api_key() {
+  const response = await axios({
+    method: "GET",
+    url: "/api_keys/ak_29L5yWTwnh8aXVxj4GN7yoetl5F",
+    baseURL: "https://api.ngrok.com",
+    headers: {
+      "ngrok-version": 2,
+      authorization: `Bearer ${API_KEY}`,
+    },
+  });
+  console.log(response.data);
+}
+// get_api_keys();
 async function get_api_keys() {
   const response = await axios({
     method: "GET",
@@ -10,10 +26,10 @@ async function get_api_keys() {
       authorization: `Bearer ${API_KEY}`,
     },
   });
+  console.log(response.data);
 }
 index();
 async function index() {
-  const API_KEY = "29L5yWTwnh8aXVxj4";
   try {
     const response = await axios({
       method: "GET",

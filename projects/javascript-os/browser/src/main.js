b77bba215b089b4e0bab7bd4a60deea87aa81537
jobsimi <jobsimi@qq.com>
2022-05-18 12:41:35 +0800

    browser：使用router.beforeEach对每个vue-router进行处理

--- projects/javascript-os/browser/src/main.js
+++ projects/javascript-os/browser/src/main.js
@@ -6,16 +6,19 @@ const app = createApp(App);
  * 路由
  */
 import { createRouter, createWebHistory } from "vue-router";
-app.use(
-  createRouter({
-    history: createWebHistory(), // 指定路由的模式
-    routes: [
-      {
-        path: "/",
-        component: { template: "<div>首页</div>" }, // runtime-core.esm-bundler.js:38 [Vue warn]: Component provided template option but runtime compilation is not supported in this build of Vue. Configure your bundler to alias "vue" to "vue/dist/vue.esm-bundler.js".
-      },
-    ],
-  })
-);
+const router = createRouter({
+  history: createWebHistory(), // 指定路由的模式
+  routes: [
+    {
+      path: "/",
+      component: { template: "<div>首页</div>" }, // runtime-core.esm-bundler.js:38 [Vue warn]: Component provided template option but runtime compilation is not supported in this build of Vue. Configure your bundler to alias "vue" to "vue/dist/vue.esm-bundler.js".
+    },
+  ],
+});
+router.beforeEach(function (to, from, next) {
+  console.log("访问", to);
+  next();
+});
+app.use(router);
 
 app.mount("#app");

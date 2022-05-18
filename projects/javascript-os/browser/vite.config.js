eb9f8850eb97bd4d7807b67b870f3f3efde3fe54
jobsimi <jobsimi@qq.com>
2022-05-18 13:45:12 +0800

    browser：安装和配置ant-design-vue

--- projects/javascript-os/browser/vite.config.js
+++ projects/javascript-os/browser/vite.config.js
@@ -5,9 +5,12 @@ const log = require("server/modules/log");
 const minimist = require("minimist");
 const argv = minimist(minimist(process.argv)["_"]);
 const { proxy_port } = argv;
+const plugins = [vue()];
 
+/**
+ * 代理
+ */
 let proxy = {};
-
 log("代理服务器端口", proxy_port);
 if (proxy_port) {
   ["/http://", "/https://", "/api"].forEach(function (item) {
@@ -18,9 +21,20 @@ if (proxy_port) {
   });
 }
 
+/**
+ * ant-design-vue
+ */
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+plugins.push(
+  Components({
+    resolvers: [AntDesignVueResolver()],
+  })
+);
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: plugins,
   server: {
     proxy: proxy,
   },

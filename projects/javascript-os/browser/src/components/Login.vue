f2e68873b8e47377279fc0a49381435fc9a78a2a
jobsimi <jobsimi@qq.com>
2022-05-19 12:32:32 +0800

    browser：登录页面Login显示“密码”输入框和“登录”按钮

--- projects/javascript-os/browser/src/components/Login.vue
+++ projects/javascript-os/browser/src/components/Login.vue
@@ -1,3 +1,18 @@
 <template>
-  <a-input></a-input>
+  <a-form>
+    <a-form-item label="密码">
+      <a-textarea auto-size allowClear></a-textarea>
+    </a-form-item>
+    <a-form-item>
+      <a-button type="primary" html-type="submit">登录</a-button>
+    </a-form-item>
+  </a-form>
 </template>
+<style scoped>
+form {
+  padding: 64px;
+}
+.ant-form-item {
+  text-align: center;
+}
+</style>

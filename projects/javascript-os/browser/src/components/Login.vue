ce0b28ca2921dced30141326a1f7efa257ef0572
jobsimi <jobsimi@qq.com>
2022-05-19 12:52:14 +0800

    browser：登录页面点击“登录”后能打印到终端

--- projects/javascript-os/browser/src/components/Login.vue
+++ projects/javascript-os/browser/src/components/Login.vue
@@ -1,13 +1,26 @@
 <template>
-  <a-form>
-    <a-form-item label="密码">
-      <a-textarea auto-size allowClear></a-textarea>
+  <a-form @finish="finish" :model="form">
+    <a-form-item label="密码" name="password" :rules="[{ required: true }]">
+      <a-textarea
+        auto-size
+        allowClear
+        v-model:value="form.password"
+      ></a-textarea>
     </a-form-item>
     <a-form-item>
       <a-button type="primary" html-type="submit">登录</a-button>
     </a-form-item>
   </a-form>
 </template>
+<script setup>
+import { reactive } from "vue";
+const form = reactive({
+  password: "",
+});
+function finish(values) {
+  console.log("finish", values);
+}
+</script>
 <style scoped>
 form {
   padding: 64px;

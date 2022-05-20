d2d933c850c33a9949ebffbad5ede6f3eb7486ce
sundawning <dpmeichen@gmail.com>
2022-05-20 21:52:00 +0800

    browser：延迟加载的路由里只需要填写component的路径

--- projects/javascript-os/browser/src/main.js
+++ projects/javascript-os/browser/src/main.js
@@ -6,26 +6,31 @@ const app = createApp(App);
  * 路由
  */
 import { createRouter, createWebHistory } from "vue-router";
+const routes = [];
+[
+  {
+    path: "/login",
+    component: "./components/Login.vue",
+  },
+  {
+    path: "/Diigo",
+    component: "./components/Diigo/App.vue",
+  },
+].forEach(function ({ path, component }) {
+  routes.push({
+    path,
+    component: function () {
+      return import(component);
+    },
+  });
+});
+routes.push({
+  path: "/",
+  component: { template: "<div>首页</div>" }, // runtime-core.esm-bundler.js:38 [Vue warn]: Component provided template option but runtime compilation is not supported in this build of Vue. Configure your bundler to alias "vue" to "vue/dist/vue.esm-bundler.js".
+});
 const router = createRouter({
   history: createWebHistory(), // 指定路由的模式
-  routes: [
-    {
-      path: "/",
-      component: { template: "<div>首页</div>" }, // runtime-core.esm-bundler.js:38 [Vue warn]: Component provided template option but runtime compilation is not supported in this build of Vue. Configure your bundler to alias "vue" to "vue/dist/vue.esm-bundler.js".
-    },
-    {
-      path: "/login",
-      component: function () {
-        return import("./components/Login.vue");
-      },
-    },
-    {
-      path: "/Diigo",
-      component: function () {
-        return import("./components/Diigo/App.vue");
-      },
-    },
-  ],
+  routes,
 });
 import { select as getLoginToken } from "browser/src/modules/login";
 router.beforeEach(function (to, from, next) {

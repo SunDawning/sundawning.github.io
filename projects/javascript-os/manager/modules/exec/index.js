45cc14c5e3a27fa0c0fcde461de9df1b3a43b5dd
sundawning <dpmeichen@gmail.com>
2022-05-21 07:42:42 +0800

    manager：exec支持options

--- projects/javascript-os/manager/modules/exec/index.js
+++ projects/javascript-os/manager/modules/exec/index.js
@@ -3,10 +3,11 @@ const child_process = require("child_process");
 /**
  * 异步执行命令
  * @param {string} command
+ * @param {object} options
  */
-module.exports = function (command) {
+module.exports = function (command, options) {
   log("执行命令", command);
-  const child = child_process.exec(command);
+  const child = child_process.exec(command, options);
   ["stderr", "stdout", "stdin"].forEach(function (location) {
     child[location].pipe(process[location]);
   });
---
+++ projects/javascript-os/manager/modules/exec/scratch.js
@@ -0,0 +1,5 @@
+const exec = require("./index");
+// "C:\Users\SunDawning\Downloads\ngrok-v3-stable-windows-amd64\ngrok.exe"
+exec(
+  "c:/users/sundawning/downloads/ngrok-v3-stable-windows-amd64/ngrok http 3000"
+);

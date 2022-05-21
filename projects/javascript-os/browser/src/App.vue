92874480bcaf3960b63c89c1965426d0d79ea48d
sundawning <dpmeichen@gmail.com>
2022-05-21 14:46:41 +0800

    browser：增加顶部栏，显示“首页”按钮

--- projects/javascript-os/browser/src/App.vue
+++ projects/javascript-os/browser/src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <router-view></router-view>
+  <a-layout>
+    <a-layout-header>
+      <a-row :style="{ height: '100%', alignItems: 'center' }">
+        <a-button><a href="/">首页</a></a-button>
+      </a-row>
+    </a-layout-header>
+    <a-layout-content>
+      <router-view></router-view>
+    </a-layout-content>
+  </a-layout>
 </template>
 <script>
 document.title = "系统";

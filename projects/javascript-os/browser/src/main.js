af47c56e4ad1426a7aa224c6d35c4415259688e9
sundawning <dpmeichen@gmail.com>
2022-05-21 16:46:42 +0800

    browser：修复在“没有权限，需要登录”时多次点击“首页”按钮修改了页面标题和图标的问题

--- projects/javascript-os/browser/src/main.js
+++ projects/javascript-os/browser/src/main.js
@@ -48,6 +48,26 @@ import { select as getLoginToken } from "browser/src/modules/login";
 router.beforeEach(function (to, from, next) {
   console.log("访问", to.fullPath, to);
   const { meta } = to;
+  const { fullPath, path } = to;
+  // 1. 登录页面不需要登录
+  if (path === "/Login") {
+    setPageMeta(meta); // 修改页面标题和图标
+    return next();
+  }
+  // 2. 其他页面都要登录
+  const token = getLoginToken();
+  if (token === null) {
+    console.log("没有权限，需要登录"); // 不需要修改页面标题和图标
+    return next("/Login?redirect=" + encodeURIComponent(fullPath)); // 跳转到登录页面
+  }
+  setPageMeta(meta); // 修改页面标题和图标
+  next();
+});
+/**
+ * 修改页面标题和图标
+ * @param {object} meta
+ */
+function setPageMeta(meta) {
   if (meta) {
     const { title, icon } = meta;
     if (title) {
@@ -57,19 +77,7 @@ router.beforeEach(function (to, from, next) {
       document.querySelector("link[rel='icon']").href = icon;
     }
   }
-  const { fullPath, path } = to;
-  // 1. 登录页面不需要登录
-  if (path === "/Login") {
-    return next();
-  }
-  // 2. 其他页面都要登录
-  const token = getLoginToken();
-  if (token === null) {
-    console.log("没有权限，需要登录");
-    return next("/Login?redirect=" + encodeURIComponent(fullPath)); // 跳转到登录页面
-  }
-  next();
-});
+}
 app.use(router);
 
 app.mount("#app");

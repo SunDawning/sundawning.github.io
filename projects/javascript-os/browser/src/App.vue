7c7ece1f3107ffa4c545c828b8cda158f0a5ae79
sundawning <dpmeichen@gmail.com>
2022-05-21 15:09:07 +0800

    browser：增加“应用”菜单栏，跳转到“/Diigo”路由

--- projects/javascript-os/browser/src/App.vue
+++ projects/javascript-os/browser/src/App.vue
@@ -2,7 +2,17 @@
   <a-layout>
     <a-layout-header>
       <a-row :style="{ height: '100%', alignItems: 'center' }">
-        <a-button><a href="/">首页</a></a-button>
+        <a-button><router-link to="/">首页</router-link></a-button>
+        <a-dropdown>
+          <template #overlay>
+            <a-menu>
+              <a-menu-item key="Diigo">
+                <router-link to="/Diigo"> Diigo </router-link>
+              </a-menu-item>
+            </a-menu>
+          </template>
+          <a-button>应用</a-button>
+        </a-dropdown>
       </a-row>
     </a-layout-header>
     <a-layout-content>

81c2f38be9026fd1bd2fb6c810e806432ff97ba9
sundawning <dpmeichen@gmail.com>
2022-05-21 13:16:05 +0800

    manager：exec支持callback

--- projects/javascript-os/manager/modules/exec/index.js
+++ projects/javascript-os/manager/modules/exec/index.js
@@ -4,10 +4,11 @@ const child_process = require("child_process");
  * 异步执行命令
  * @param {string} command
  * @param {object} options
+ * @param {function} callback
  */
-module.exports = function (command, options) {
+module.exports = function (command, options, callback) {
   log("执行命令", command);
-  const child = child_process.exec(command, options);
+  const child = child_process.exec(command, options, callback);
   ["stderr", "stdout", "stdin"].forEach(function (location) {
     child[location].pipe(process[location]);
   });

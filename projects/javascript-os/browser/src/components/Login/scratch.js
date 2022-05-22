ce289a3bef24a435240d652dfd016bc2c86f1797
sundawning <dpmeichen@gmail.com>
2022-05-22 22:06:54 +0800

    browser：登录组件：可以下载Git在线页面里的ngrok服务器列表

--- projects/javascript-os/browser/src/components/Login/scratch.js
+++ projects/javascript-os/browser/src/components/Login/scratch.js
@@ -1,6 +1,13 @@
 const axios = require("axios");
 index();
 async function index() {
+  {
+    const response = await axios({
+      method: "GET",
+      url: "https://raw.githubusercontent.com/SunDawning/javascript-ngrok-server-list/main/server.json",
+    });
+    console.log("response.data", response.data);
+  }
   const response = await axios({
     method: "POST",
     url: "/api/javascript-os/login",

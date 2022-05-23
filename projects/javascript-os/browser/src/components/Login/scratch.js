397f7be412c22f13bc0f8ed6a6badaab0836cd83
sundawning <dpmeichen@gmail.com>
2022-05-23 21:42:45 +0800

    browser：增加bitbucket地址来获取登录授权的服务器列表

--- projects/javascript-os/browser/src/components/Login/scratch.js
+++ projects/javascript-os/browser/src/components/Login/scratch.js
@@ -5,13 +5,14 @@ async function index() {
     const response = await axios({
       method: "GET",
       url: "https://raw.githubusercontent.com/SunDawning/javascript-ngrok-server-list/main/server.json",
+      // https://bitbucket.org/SunDawning/javascript-ngrok-server-list/raw/main/server.json
     });
     console.log("response.data", response.data);
   }
   const response = await axios({
     method: "POST",
     url: "/api/javascript-os/login",
-    baseURL: "http://sundawning.vaiwan.cn",
+    baseURL: "https://b898-183-240-6-199.ngrok.io",
     data: {
       password: "123456",
     },

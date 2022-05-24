282ce86592ea70f778e303c2a525d0992146895f
sundawning <dpmeichen@gmail.com>
2022-05-24 08:08:27 +0800

    browser：登录里可以使用login来获取在线的授权服务器并进行登录

--- projects/javascript-os/browser/src/components/Login/index.vue
+++ projects/javascript-os/browser/src/components/Login/index.vue
@@ -30,17 +30,7 @@ async function finish(values) {
   console.log("finish", values);
   const { password } = values;
   try {
-    const baseURL = [
-      "http://sundawning.vaiwan.cn",
-      "https://f0e2-120-229-49-229.ngrok.io",
-    ][0];
-    const response = await axios({
-      method: "POST",
-      url: `${baseURL}/api/javascript-os/login`,
-      data: {
-        password,
-      },
-    });
+    const response = await login({ password });
     const { data } = response;
     const { success } = data;
     if (success === false) {
@@ -73,6 +63,74 @@ function redirect_to(router) {
   }
   console.log("重定向到", redirect);
   router.push(redirect);
+}
+/**
+ * 将密码发送到授权服务器，如果登录成功则返回数据，否则抛出错误
+ * @param {number} options.password
+ * @returns
+ */
+async function login({ password = 123456 } = {}) {
+  const serverList = await getServerList();
+  console.log("serverList", serverList);
+  if (serverList === undefined) {
+    throw { message: "无法获取授权服务器列表" };
+  }
+  const baseURLs = makeArrayToRandom(serverList);
+  const total = baseURLs.length;
+  for (let c = 0; c < total; c = c + 1) {
+    const baseURL = baseURLs[c];
+    console.log("baseURL", baseURL);
+    try {
+      const response = await axios({
+        method: "POST",
+        url: `${baseURL}/api/javascript-os/login`,
+        data: { password },
+      });
+      return response.data;
+    } catch (error) {
+      continue;
+    }
+  }
+  throw { message: "授权服务器无法连接" };
+}
+/**
+ * 获取在线服务器的列表
+ */
+async function getServerList() {
+  const urls = makeArrayToRandom([
+    "https://raw.githubusercontent.com/SunDawning/javascript-ngrok-server-list/main/server.json",
+    "https://bitbucket.org/SunDawning/javascript-ngrok-server-list/raw/main/server.json",
+  ]);
+  const total = urls.length;
+  for (let c = 0; c < total; c = c + 1) {
+    const url = urls[c];
+    console.log("url", url);
+    try {
+      const response = await axios({
+        method: "GET",
+        url,
+      });
+      return response.data;
+    } catch (error) {
+      continue;
+    }
+  }
+  return;
+}
+/**
+ * 将数组打乱
+ * @see https://blog.csdn.net/xm_law/article/details/83691125
+ * @param {array} arr
+ * @returns array
+ */
+function makeArrayToRandom(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    var iRand = parseInt(arr.length * Math.random());
+    var temp = arr[i];
+    arr[i] = arr[iRand];
+    arr[iRand] = temp;
+  }
+  return arr;
 }
 </script>
 <style scoped>

e10b8c47e529c192a113cb8aa497cdb4242fc8fb
sundawning <dpmeichen@gmail.com>
2022-06-01 22:36:00 +0800

    博客模式：使用markdown编辑器

--- projects/javascript-diigo/vue/src/components/BlogMode.vue
+++ projects/javascript-diigo/vue/src/components/BlogMode.vue
@@ -1,8 +1,13 @@
 <template>
-  <a-row>写博客</a-row>
+  <md-editor v-model="text"></md-editor>
 </template>
 <script setup>
 // diigo:mastermeichen:blog:hello-world
 // 程序名：用户名：博客：文章标题
 // #diigo #mastermeichen #blog
+import { ref } from "vue";
+import MdEditor from "md-editor-v3";
+import "md-editor-v3/lib/style.css";
+
+const text = ref("");
 </script>

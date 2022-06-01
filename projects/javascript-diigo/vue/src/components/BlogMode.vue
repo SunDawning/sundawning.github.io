6534efac86fd5645783522a2a84c5b3ccd2a9a84
sundawning <dpmeichen@gmail.com>
2022-06-01 22:46:37 +0800

    博客模式：使用github样式来预览

--- projects/javascript-diigo/vue/src/components/BlogMode.vue
+++ projects/javascript-diigo/vue/src/components/BlogMode.vue
@@ -1,13 +1,21 @@
 <template>
-  <md-editor v-model="text"></md-editor>
+  <md-editor
+    v-model="state.text"
+    :preview-theme="state.preview_theme"
+    :code-theme="state.code_theme"
+  ></md-editor>
 </template>
 <script setup>
 // diigo:mastermeichen:blog:hello-world
 // 程序名：用户名：博客：文章标题
 // #diigo #mastermeichen #blog
-import { ref } from "vue";
+import { reactive } from "vue";
 import MdEditor from "md-editor-v3";
 import "md-editor-v3/lib/style.css";
 
-const text = ref("");
+const state = reactive({
+  text: "",
+  preview_theme: "github",
+  code_theme: "github",
+});
 </script>

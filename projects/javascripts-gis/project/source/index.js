4d75579d33af764ed56af11ba6942858dc014fe4
sundawning <dpmeichen@gmail.com>
2022-06-14 22:35:13 +0800

    修复Uncaught TypeError: Cannot read properties of null (reading 'createElement')

--- projects/javascripts-gis/project/source/index.js
+++ projects/javascripts-gis/project/source/index.js
@@ -1,17 +1,30 @@
 async function index() {
-  window.CESIUM_BASE_URL =
-    "https://cdnjs.cloudflare.com/ajax/libs/cesium/1.94.3";
-  await import(`${CESIUM_BASE_URL}/Cesium.js`);
-  const container = document.createElement("div");
-  document.body.appendChild(container);
   {
-    const response = await fetch(`${CESIUM_BASE_URL}/Widgets/widgets.css`);
+    window.CESIUM_BASE_URL =
+      "https://cdnjs.cloudflare.com/ajax/libs/cesium/1.94.3";
     {
-      const style = document.createElement("style");
-      style.innerText = await response.text();
-      container.appendChild(style);
+      const container = document.createElement("div");
+      {
+        const response = await fetch(`${CESIUM_BASE_URL}/Widgets/widgets.css`);
+        {
+          const style = document.createElement("style");
+          style.innerText = await response.text();
+          container.appendChild(style);
+        }
+      }
+      {
+        await import(`${CESIUM_BASE_URL}/Cesium.js`);
+      }
+      {
+        new Cesium.Viewer(container);
+      }
+      {
+        const body = document.body;
+        {
+          body.appendChild(container);
+        }
+      }
     }
   }
-  new Cesium.Viewer(container);
 }
 index();

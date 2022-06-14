5f244bf073704da9514331c37797d684036351df
sundawning <dpmeichen@gmail.com>
2022-06-14 22:38:48 +0800

    CESIUM_BASE_URL和container是所必需的外部数据

--- projects/javascripts-gis/project/source/index.js
+++ projects/javascripts-gis/project/source/index.js
@@ -2,27 +2,25 @@ async function index() {
   {
     window.CESIUM_BASE_URL =
       "https://cdnjs.cloudflare.com/ajax/libs/cesium/1.94.3";
+    const container = document.createElement("div");
     {
-      const container = document.createElement("div");
+      const response = await fetch(`${CESIUM_BASE_URL}/Widgets/widgets.css`);
       {
-        const response = await fetch(`${CESIUM_BASE_URL}/Widgets/widgets.css`);
-        {
-          const style = document.createElement("style");
-          style.innerText = await response.text();
-          container.appendChild(style);
-        }
+        const style = document.createElement("style");
+        style.innerText = await response.text();
+        container.appendChild(style);
       }
-      {
-        await import(`${CESIUM_BASE_URL}/Cesium.js`);
-      }
+    }
+    {
+      await import(`${CESIUM_BASE_URL}/Cesium.js`);
       {
         new Cesium.Viewer(container);
       }
+    }
+    {
+      const body = document.body;
       {
-        const body = document.body;
-        {
-          body.appendChild(container);
-        }
+        body.appendChild(container);
       }
     }
   }

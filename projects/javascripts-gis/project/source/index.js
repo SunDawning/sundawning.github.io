b8c9003e9e20f48b21a4c796ef39c182accb5bbc
sundawning <dpmeichen@gmail.com>
2022-06-15 12:30:06 +0800

    使用shadowRoot来将css和js隔离

--- projects/javascripts-gis/project/source/index.js
+++ projects/javascripts-gis/project/source/index.js
@@ -3,18 +3,19 @@ async function index() {
     window.CESIUM_BASE_URL =
       "https://cdnjs.cloudflare.com/ajax/libs/cesium/1.94.3";
     const container = document.createElement("div");
+    container.attachShadow({ mode: "open" });
     {
       const response = await fetch(`${CESIUM_BASE_URL}/Widgets/widgets.css`);
       {
         const style = document.createElement("style");
         style.innerText = await response.text();
-        container.appendChild(style);
+        container.shadowRoot.appendChild(style);
       }
     }
     {
       await import(`${CESIUM_BASE_URL}/Cesium.js`);
       {
-        new Cesium.Viewer(container);
+        new Cesium.Viewer(container.shadowRoot);
       }
     }
     {

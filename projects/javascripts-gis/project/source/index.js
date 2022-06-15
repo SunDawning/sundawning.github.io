ade4980c91edbc423046a4b8fc0a6ede34d46c7d
sundawning <dpmeichen@gmail.com>
2022-06-15 22:31:35 +0800

    全局的函数、全局的变量

--- projects/javascripts-gis/project/source/index.js
+++ projects/javascripts-gis/project/source/index.js
@@ -4,7 +4,7 @@ async function index() {
     window.CESIUM_BASE_URL =
       "https://cdnjs.cloudflare.com/ajax/libs/cesium/1.94.3";
     await import("./library/getCesiumViewerCreateByBaseURL.js");
-    await getCesiumViewerCreateByBaseURL(root, CESIUM_BASE_URL);
+    window.viewer = await getCesiumViewerCreateByBaseURL(root, CESIUM_BASE_URL);
   }
 }
 index();

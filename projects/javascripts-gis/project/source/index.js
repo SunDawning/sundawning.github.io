95790c39f390b544c740c235aa532414c712e1b4
sundawning <dpmeichen@gmail.com>
2022-06-17 20:48:24 +0800

    分离出onload

--- projects/javascripts-gis/project/source/index.js
+++ projects/javascripts-gis/project/source/index.js
@@ -12,62 +12,75 @@ body{
   `
   );
 }
-async function onLoadWindow() {
+/**
+ * @param {sttring} CESIUM_BASE_URL
+ */
+async function getImageryProvider(CESIUM_BASE_URL) {
+  await import(
+    "./library/getGeoqChinaOnlineStreetPurplishBlueImageryProviderByBaseURL.js"
+  );
+
+  return await getGeoqChinaOnlineStreetPurplishBlueImageryProviderByBaseURL(
+    CESIUM_BASE_URL
+  );
+}
+/**
+ * @param {HTMLElement} root
+ * @param {function} getImageryProvider
+ * @returns Cesium.CesiumWidget
+ */
+async function createEarth(root, getImageryProvider) {
+  globalThis.CESIUM_BASE_URL =
+    "https://cdnjs.cloudflare.com/ajax/libs/cesium/1.94.3";
+  await import(
+    "./library/getCesiumCSSAndCesiumWidgetCreateByBaseURLInShadowRoot.js"
+  );
+  globalThis.cesiumWidget =
+    await getCesiumCSSAndCesiumWidgetCreateByBaseURLInShadowRoot(
+      root,
+      CESIUM_BASE_URL,
+      { imageryProvider: await getImageryProvider(CESIUM_BASE_URL) }
+    );
+  return globalThis.cesiumWidget;
+}
+/**
+ * @param {HTMLElement} root
+ * @param {Cesium.CesiumWidget} cesiumWidget
+ */
+async function printCesiumCameraPosition(root, cesiumWidget) {
+  await import("./library/getShadowRootContainerCreateAndAppend.js");
+  const container = await getShadowRootContainerCreateAndAppend(root);
+  {
+    await import("./library/appendStyleText.js");
+    appendStyleText(
+      container.shadowRoot,
+      `
+div{
+position: absolute;
+top: 0;
+right: 0;
+color: white;
+white-space: pre;
+}
+    `
+    );
+  }
+  const div = document.createElement("div");
+  function printCameraPositionOnMoveEnd() {
+    div.innerHTML = JSON.stringify(cesiumWidget.camera.position, null, 4);
+  }
+  printCameraPositionOnMoveEnd();
+  {
+    cesiumWidget.camera.moveEnd.addEventListener(
+      printCameraPositionOnMoveEnd,
+      "cameara_position"
+    );
+  }
+  appendChild(container.shadowRoot, div);
+}
+globalThis.onload = async function () {
   const root = document.body;
   appendRootStyle(root);
-  {
-    globalThis.CESIUM_BASE_URL =
-      "https://cdnjs.cloudflare.com/ajax/libs/cesium/1.94.3";
-    let imageryProvider;
-    {
-      await import(
-        "./library/getGeoqChinaOnlineStreetPurplishBlueImageryProviderByBaseURL.js"
-      );
-      imageryProvider =
-        await getGeoqChinaOnlineStreetPurplishBlueImageryProviderByBaseURL(
-          CESIUM_BASE_URL
-        );
-    }
-    await import(
-      "./library/getCesiumCSSAndCesiumWidgetCreateByBaseURLInShadowRoot.js"
-    );
-    globalThis.cesiumWidget =
-      await getCesiumCSSAndCesiumWidgetCreateByBaseURLInShadowRoot(
-        root,
-        CESIUM_BASE_URL,
-        { imageryProvider }
-      );
-    {
-      await import("./library/getShadowRootContainerCreateAndAppend.js");
-      const container = await getShadowRootContainerCreateAndAppend(root);
-      {
-        await import("./library/appendStyleText.js");
-        appendStyleText(
-          container.shadowRoot,
-          `
-  div{
-    position: absolute;
-    top: 0;
-    right: 0;
-    color: white;
-    white-space: pre;
-  }
-        `
-        );
-      }
-      const div = document.createElement("div");
-      function printCameraPositionOnMoveEnd() {
-        div.innerHTML = JSON.stringify(cesiumWidget.camera.position, null, 4);
-      }
-      printCameraPositionOnMoveEnd();
-      {
-        cesiumWidget.camera.moveEnd.addEventListener(
-          printCameraPositionOnMoveEnd,
-          "cameara_position"
-        );
-      }
-      appendChild(container.shadowRoot, div);
-    }
-  }
-}
-globalThis.onload = onLoadWindow;
+  const cesiumWidget = await createEarth(root, getImageryProvider);
+  printCesiumCameraPosition(root, cesiumWidget);
+};

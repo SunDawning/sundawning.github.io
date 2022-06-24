a924e8a2bde565be49533feab6f07831afa692c4
jobsimi <jobsimi@qq.com>
2022-06-24 13:44:03 +0800

    detail_offsetElement重命名为offsetElement

--- projects/javascripts-gis/project/source/library/createDateTimeDetailManager.js
+++ projects/javascripts-gis/project/source/library/createDateTimeDetailManager.js
@@ -3,20 +3,20 @@ if (globalThis.SunDawningGIS === undefined) {
 }
 /**
  * 创建时间日期详情面板
- * @param {HTMLElement} detail_offsetElement
+ * @param {HTMLElement} offsetElement 相对于某个元素进行偏移，该元素即是offsetElement
  * @param {function} onPointerDown 点击时间日期详情之外的区域时，触发点击事件
  * @returns HTMLElement
  */
 SunDawningGIS.createDateTimeDetailManager = async function ({
-  detail_offsetElement = document.body,
+  offsetElement = document.body,
   onPointerDown,
 } = {}) {
   const SELF = {};
   await import("./createDateTimeDetailContainer.js");
   let container = await SunDawningGIS.createDateTimeDetailContainer({
-    bottom: detail_offsetElement.offsetHeight,
+    bottom: offsetElement.offsetHeight,
   });
-  detail_offsetElement.offsetParent.appendChild(container);
+  offsetElement.offsetParent.appendChild(container);
   /**
    * 阻止事件冒泡
    * @see https://www.cnblogs.com/Jacob98/p/11932174.html 三种方法教你HTML实现点击某一个元素之外触发事件
@@ -29,10 +29,7 @@ SunDawningGIS.createDateTimeDetailManager = async function ({
     // 点击时间日期详情之内的区域时，不触发点击事件
     container.addEventListener("pointerdown", stopPropagation);
     // 点击时间日期详情之外的区域时，触发点击事件
-    detail_offsetElement.offsetParent.addEventListener(
-      "pointerdown",
-      onPointerDown
-    );
+    offsetElement.offsetParent.addEventListener("pointerdown", onPointerDown);
   }
   // 时间
   await import("./createLocaleTimeElementManager.js");
@@ -46,7 +43,7 @@ SunDawningGIS.createDateTimeDetailManager = async function ({
   SELF.destroy = function () {
     localeTimeElementManager.destroy();
     // 关闭点击时间日期详情之外的区域时的点击事件
-    detail_offsetElement.offsetParent.removeEventListener(
+    offsetElement.offsetParent.removeEventListener(
       "pointerdown",
       onPointerDown
     );

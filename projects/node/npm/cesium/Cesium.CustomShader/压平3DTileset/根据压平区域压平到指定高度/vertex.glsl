8eecfdef251fce8ca83614722ea093fa4b24fa83
jobsimi <jobsimi@qq.com>
2022-07-12 10:22:12 +0800

    u_tilesetHeight

--- projects/node/npm/cesium/Cesium.CustomShader/压平3DTileset/根据压平区域压平到指定高度/vertex.glsl
+++ projects/node/npm/cesium/Cesium.CustomShader/压平3DTileset/根据压平区域压平到指定高度/vertex.glsl
@@ -1,29 +1,29 @@
 // 5个元素的数组
 vec2 points_5[5];
 bool isPointInPolygon_5(vec2 point){
-int nCross = 0; // 交点数
-const int n = 5; // Loop index cannot be compared with non-constant expression
-for(int i = 0; i < n; i++){
-  vec2 p1 = points_5[i];
-  vec2 p2 = points_5[int(mod(float(i+1),float(n)))];
-  // 求解 y=p.y 与 p1 p2 的交点
-  // p1p2 与 y=p0.y平行
-  if(p1[1] == p2[1]){
-    continue;
+  int nCross = 0; // 交点数
+  const int n = 5; // Loop index cannot be compared with non-constant expression
+  for(int i = 0; i < n; i++){
+    vec2 p1 = points_5[i];
+    vec2 p2 = points_5[int(mod(float(i+1),float(n)))];
+    // 求解 y=p.y 与 p1 p2 的交点
+    // p1p2 与 y=p0.y平行
+    if(p1[1] == p2[1]){
+      continue;
+    }
+    // 交点在p1p2延长线上
+    if(point[1] < min(p1[1], p2[1])){
+      continue;
+    }
+    if(point[1] >= max(p1[1], p2[1])){
+      continue;
+    }
+    float x = p1[0] + ((point[1] - p1[1]) * (p2[0] - p1[0])) / (p2[1] - p1[1]);
+    if(x > point[0]){
+      nCross++;
+    }
   }
-  // 交点在p1p2延长线上
-  if(point[1] < min(p1[1], p2[1])){
-    continue;
-  }
-  if(point[1] >= max(p1[1], p2[1])){
-    continue;
-  }
-  float x = p1[0] + ((point[1] - p1[1]) * (p2[0] - p1[0])) / (p2[1] - p1[1]);
-  if(x > point[0]){
-    nCross++;
-  }
-}
-return int(mod(float(nCross), float(2))) == 1;
+  return int(mod(float(nCross), float(2))) == 1;
 }        
 void vertexMain(VertexInput vsInput, inout czm_modelVertexOutput vsOutput){
   vec3 modelMC = vsInput.attributes.positionMC;
@@ -31,7 +31,7 @@ void vertexMain(VertexInput vsInput, inout czm_modelVertexOutput vsOutput){
   vec4 tileset_local_position = u_tileset_worldToLocalMatrix * czm_model * model_local_position;
 
   vec2 point = vec2(tileset_local_position.x,tileset_local_position.y);
-  float ground_z = 0.0 + (- 174.45775292080066);
+  float ground_z = 0.0 + (- u_tilesetHeight);
 
   points_5[0] = vec2(463.68057972565293, -890.5733635588585);
   points_5[1] = vec2(476.2839835048653, -1106.1302583012766);

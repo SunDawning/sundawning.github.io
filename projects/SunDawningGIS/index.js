b2dc71d35f68baa2dfad85596e99541ca4203e85
sundawning <dpmeichen@gmail.com>
2022-07-18 21:55:47 +0800

    getCesiumCameraView

--- projects/SunDawningGIS/index.js
+++ projects/SunDawningGIS/index.js
@@ -1,2 +1,17 @@
 const VERSION = "0.0.1";
 console.log(VERSION);
+/**
+ * 得到相机的视角，使用camera.setView
+ * @param {Cesium.Camera} camera
+ * @returns object
+ */
+function getCesiumCameraView(camera) {
+  return {
+    destination: camera.position,
+    orientation: {
+      heading: camera.heading,
+      pitch: camera.pitch,
+      roll: camera.roll,
+    },
+  };
+}

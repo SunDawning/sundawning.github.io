0a15c205760c55d9ffbde73552838f0e34cbcebb
jobsimi <jobsimi@qq.com>
2022-07-25 14:14:47 +0800

    修复Module parse failed: Unexpected token (16:29)

--- projects/node/npm/@umijs/hello/.umirc.ts
+++ projects/node/npm/@umijs/hello/.umirc.ts
@@ -6,4 +6,10 @@ export default defineConfig({
   },
   routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
+  // Module parse failed: Unexpected token (16:29)
+  // @see https://github.com/umijs/umi/issues/4834
+  chainWebpack(memo) {
+    memo.module.rule('ts-in-node_modules').include.clear();
+    return memo;
+  },
 });

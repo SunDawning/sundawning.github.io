044ffec89f115d6f1c21bbab4917d305afdf4fdb
jobsimi <jobsimi@qq.com>
2022-07-25 14:52:35 +0800

    npm/@umijs/mode-server

---
+++ projects/node/npm/@umijs/mock-server/.umirc.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  devServer: { port: 8081 },
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [{ path: '/', component: '@/pages/index' }],
+  fastRefresh: {},
+  // Module parse failed: Unexpected token (16:29)
+  // @see https://github.com/umijs/umi/issues/4834
+  chainWebpack(memo) {
+    memo.module.rule('ts-in-node_modules').include.clear();
+    return memo;
+  },
+});
---
+++ projects/node/npm/@umijs/mock-server/README.md
@@ -0,0 +1,21 @@
+mock server
+
+将其他 mock 文件夹 symlink-link 到 mock 文件夹，并复制内部的文件夹到该路径下（umijs 不能嵌套地处理 symlink 文件夹）达到模拟后端接口的作用。
+
+2022-07-25 14:16:51
+
+# umi project
+
+## Getting Started
+
+Install dependencies,
+
+```bash
+$ yarn
+```
+
+Start the dev server,
+
+```bash
+$ yarn start
+```

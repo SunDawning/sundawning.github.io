fd0bc299eb2f555094bc591db1324d922bc87d80
jobsimi <jobsimi@qq.com>
2022-08-03 17:41:23 +0800

    重命名“npm/nodejs”代码保护为“npm/nodejs-code-protect”

---
+++ projects/node/nodejs-code-protect/index.js
@@ -0,0 +1,19 @@
+index();
+async function index() {
+  console.log("以下是价值上千万的代码:");
+  // const sharp = require("sharp");
+  // // 生成图片
+  // const buffer = await sharp({
+  //   create: {
+  //     width: 256,
+  //     height: 256,
+  //     channels: 4,
+  //     background: { r: 255, g: 0, b: 0, alpha: 0.5 },
+  //   },
+  // })
+  //   .png()
+  //   .toBuffer();
+  // // 保存图片
+  // const fs = require("fs");
+  // fs.writeFileSync("output.png", buffer);
+}

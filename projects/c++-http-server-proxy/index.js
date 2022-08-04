45f8ffc27af5e1706d689b8b55ba965b6553232b
jobsimi <jobsimi@qq.com>
2022-08-04 11:06:22 +0800

    修复生成exe在其他电脑上运行时缺失dll，直接复制dll到exe所在文件夹下即可

--- projects/c++-http-server-proxy/index.js
+++ projects/c++-http-server-proxy/index.js
@@ -4,7 +4,16 @@ let DATABASE = {
   build_directory: "./build",
   exe_file_name: "index.exe",
   cpp_httplib_include_directory: "./cpp-httplib-v0.10.6",
+  cpp_httplib_library_directory: "C:/msys64/mingw64/bin",
   cpp_httplib_library_files: ["ws2_32", "libssl", "libcrypto", "crypt32"],
+  // 生成exe后运行时缺失dll
+  cpp_httplib_library_files_copy: [
+    "libssl-1_1-x64", // libssl-1_1-x64.dll
+    "libcrypto-1_1-x64", // libcrypto-1_1-x64.dll
+    "libgcc_s_seh-1", // libgcc_s_seh-1.dll
+    "libwinpthread-1", // libwinpthread-1.dll
+    "libstdc++-6", // libstdc++-6.dll
+  ],
   opencv_include_directory: "./OpenCV-MinGW-Build-OpenCV-4.5.5-x64/include",
   opencv_library_directory:
     "./OpenCV-MinGW-Build-OpenCV-4.5.5-x64/x64/mingw/bin",
@@ -19,7 +28,9 @@ let {
   build_directory,
   exe_file_name,
   cpp_httplib_include_directory,
+  cpp_httplib_library_directory,
   cpp_httplib_library_files,
+  cpp_httplib_library_files_copy,
   opencv_include_directory,
   opencv_library_directory,
   opencv_library_files,
@@ -32,22 +43,12 @@ let {
 /**
  * 复制dll
  */
-{
-  const copy_files = [];
-  opencv_library_files.forEach(function (file) {
-    const fileName = `${file}.dll`;
-    const destination = `${build_directory}/${fileName}`;
-    if (fs.existsSync(destination) === true) {
-      return;
-    }
-    copy_files.push(
-      `cp ${opencv_library_directory}/${fileName} "${build_directory}"`
-    );
-  });
-  if (copy_files.length > 0) {
-    exec(copy_files.join(" && "));
-  }
-}
+copyDLLs(
+  cpp_httplib_library_directory,
+  cpp_httplib_library_files_copy,
+  build_directory
+);
+copyDLLs(opencv_library_directory, opencv_library_files, build_directory);
 /**
  * 编译成exe并执行exe
  * g++ -g index.cpp -o "./build/index.exe" -I "C:/Users/jobsimi/Documents/tuihuiinfo/github.com/yhirose/cpp-httplib/cpp-httplib-v0.10.6" -l ws2_32 -l libssl -l libcrypto -l crypt32 -I "C:/Users/jobsimi/Downloads/OpenCV-MinGW-Build-OpenCV-4.5.5-x64/include" -L "C:/Users/jobsimi/Downloads/OpenCV-MinGW-Build-OpenCV-4.5.5-x64/x64/mingw/bin" -l libopencv_core455 -l libopencv_imgproc455 
@@ -60,6 +61,30 @@ let {
     opencv_library_files
   )} && cd "${build_directory}" && ${exe_file_name}`;
   exec(command);
+}
+/**
+ * 将文件夹directory_from下的文件files复制到文件夹directory_to下
+ * @param {string} directory_from
+ * @param {array} files
+ * @param {string} directory_to
+ */
+function copyDLLs(directory_from, files, directory_to) {
+  const copy_files = [];
+  files.forEach(function (file) {
+    const fileName = `${file}.dll`;
+    const from = `${directory_from}/${fileName}`;
+    if (fs.existsSync(from) === false) {
+      return;
+    }
+    const destination = `${directory_to}/${fileName}`;
+    if (fs.existsSync(destination) === true) {
+      return;
+    }
+    copy_files.push(`cp "${from}" "${directory_to}"`);
+  });
+  if (copy_files.length > 0) {
+    exec(copy_files.join(" && "));
+  }
 }
 /**
  * 拼接动态链接库

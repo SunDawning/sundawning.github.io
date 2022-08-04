908440c0d4bcca2cce6ca368ba59feab9af2be81
jobsimi <jobsimi@qq.com>
2022-08-04 17:39:19 +0800

    点击扩展的按钮，输出要做的事情

--- projects/chrome/extensions/syncLocalStorage/popup.js
+++ projects/chrome/extensions/syncLocalStorage/popup.js
@@ -3,9 +3,18 @@ function index() {
   document.querySelector("#read").addEventListener("click", read);
   document.querySelector("#write").addEventListener("click", write);
 }
-
 function read() {
-  console.log("读");
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        data: "读",
+      });
+    }
+  );
 }
 function write() {
   console.log("写");

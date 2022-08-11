8c9292b46eade03badbb46dab86a6fbc9b4855d6
sundawning <dpmeichen@gmail.com>
2022-08-11 21:54:24 +0800

    标题跟随输入框变化

--- projects/node/npm/@umijs/deep-clone/src/pages/components/Form/index.tsx
+++ projects/node/npm/@umijs/deep-clone/src/pages/components/Form/index.tsx
@@ -2,10 +2,15 @@ import { Form, Input } from 'antd';
 const { Item } = Form;
 export default function IndexPage({ dataBase, setDataBase }) {
   const [form] = Form.useForm();
+  function onChangeTitle(event) {
+    dataBase.title = event.target.value;
+    // console.log('dataBase', dataBase);
+    setDataBase({ ...dataBase });
+  }
   return (
     <Form form={form}>
       <Item label="标题" name="title">
-        <Input></Input>
+        <Input onChange={onChangeTitle}></Input>
       </Item>
     </Form>
   );

9fcb84c57f7a2fff7a1c2a65f42f28a12fa65cc1
sundawning <dpmeichen@gmail.com>
2022-08-11 21:47:21 +0800

    从dataBase里读取数据

--- projects/node/npm/@umijs/deep-clone/src/pages/index.tsx
+++ projects/node/npm/@umijs/deep-clone/src/pages/index.tsx
@@ -1,9 +1,15 @@
 import styles from './index.less';
+import { useState } from 'react';
 
 export default function IndexPage() {
+  const [dataBase, setDataBase] = useState(initialDataBase);
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>{dataBase.title}</h1>
     </div>
   );
 }
+// 初始的数据库
+const initialDataBase = {
+  title: 'Page index',
+};

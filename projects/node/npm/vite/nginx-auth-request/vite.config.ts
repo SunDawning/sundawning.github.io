97cd011ba5da1b57aeef20bf4211cfc0f8e1d65e
sundawning <dpmeichen@gmail.com>
2022-08-12 21:53:59 +0800

    打包后网站root是当前目录

--- projects/node/npm/vite/nginx-auth-request/vite.config.ts
+++ projects/node/npm/vite/nginx-auth-request/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  base: "./", // 打包后网站root是当前目录
+  plugins: [react()],
+});

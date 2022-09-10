64fd8f8167b8d24081a21463be557f9612301ca5
sundawning <dpmeichen@gmail.com>
2022-09-10 10:39:00 +0800

    新增startFile，来自pm2.start

--- projects/node/npm/@sundawning/pm2-start-javascript/index.js
+++ projects/node/npm/@sundawning/pm2-start-javascript/index.js
@@ -1,7 +1,9 @@
-module.exports = { start, startFunction };
 const fs = require("fs-extra");
 const path = require("path");
 const pm2 = require("@sundawning/pm2-start-js-file");
+
+module.exports = { start, startFunction, startFile: pm2.start };
+
 /**
  * 使用pm2来运行一段javascript文本
  * @param {string} options.name pm2程序的名称
@@ -32,7 +34,7 @@ async function start({ name, javascript, args, cwd = __dirname } = {}) {
 /**
  * 运行函数
  * @param {function} fun 有函数名称的函数，函数名称即是程序名，是一个函数定义，如果需要使用外部变量，需要提前计算。
- * @param {string} args 函数的自变量
+ * @param {string|object} args 函数的自变量
  * @param {string} cwd
  */
 async function startFunction({ fun, args, cwd = __dirname } = {}) {

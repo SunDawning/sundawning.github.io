f932dc04d1e0648c5ea71390d713908d1e4fc119
sundawning <dpmeichen@gmail.com>
2022-09-21 21:32:09 +0800

    POST /api/databases

--- projects/node/npm/@sundawning/sqlite-database-server/scratch.js
+++ projects/node/npm/@sundawning/sqlite-database-server/scratch.js
@@ -1,6 +1,17 @@
 const Koa = require("koa");
 const Router = require("koa-router");
-async function index({ port = 28080 } = {}) {
+const sqlite = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const koa_body = require("koa-body");
+const fs = require("fs-extra");
+async function index({
+  port = 28080,
+  databases_directory = path.resolve(__dirname, "./databases"),
+} = {}) {
+  // 文件夹
+  await fs.ensureDir(databases_directory);
+  // 服务器
   const app = new Koa();
   console.log("app", app);
   const server = app.listen(port);
@@ -11,6 +22,28 @@ async function index({ port = 28080 } = {}) {
   app.use(router.routes());
   router.get("/hello", async function (context) {
     context.response.body = "hello";
+  });
+  router.post("/api/databases", koa_body(), async function (context) {
+    console.log("context.request.body", context.request.body);
+    const { name: database_name } = context.request.body;
+    if (database_name === undefined) {
+      return (context.response.status = 400);
+    }
+    const database_filename = path.resolve(
+      databases_directory,
+      `./${database_name}.db`
+    );
+    console.log("database_filename", database_filename);
+    if ((await fs.pathExists(database_filename)) === true) {
+      context.response.status = 201;
+      return;
+    }
+    const database = await sqlite.open({
+      filename: database_filename,
+      driver: sqlite3.Database,
+    });
+    await database.close();
+    context.response.status = 201;
   });
 }
 index();

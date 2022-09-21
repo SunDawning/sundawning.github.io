508781fb1bd61bcf929ca8ebfad3ee3bfae8d03a
sundawning <dpmeichen@gmail.com>
2022-09-21 20:54:51 +0800

    GET /hello

--- projects/node/npm/@sundawning/sqlite-database-server/scratch.js
+++ projects/node/npm/@sundawning/sqlite-database-server/scratch.js
@@ -1,9 +1,16 @@
 const Koa = require("koa");
-async function index({ port = 8080 } = {}) {
+const Router = require("koa-router");
+async function index({ port = 28080 } = {}) {
   const app = new Koa();
   console.log("app", app);
   const server = app.listen(port);
   console.log("server", JSON.stringify(server));
   console.log("server.address()", server.address());
+  const router = new Router();
+  console.log("router", JSON.stringify(router));
+  app.use(router.routes());
+  router.get("/hello", async function (context) {
+    context.response.body = "hello";
+  });
 }
 index();

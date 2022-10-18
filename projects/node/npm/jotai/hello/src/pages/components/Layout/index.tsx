98fc920189130e205610d46f2dd45d1e83bc9145
jobsimi <jobsimi@qq.com>
2022-10-18 17:47:04 +0800

    调整Layout组件的内容

--- projects/node/npm/jotai/hello/src/pages/components/Layout/index.tsx
+++ projects/node/npm/jotai/hello/src/pages/components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import { Layout, Menu, Input, Space } from 'antd';
+import { Layout } from 'antd';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
@@ -6,12 +6,8 @@ export default function IndexPage() {
   return (
     <Layout>
       <Header></Header>
-      <Content style={{ minHeight: '480px' }}></Content>
-      {/* 底部 */}
-      <Footer>
-        Copyright © 2022 SunDawning
-        <a href="mailto:jobsimi@qq.com">jobsimi@qq.com</a>, All Rights Reserved
-      </Footer>
+      <Content></Content>
+      <Footer></Footer>
     </Layout>
   );
 }

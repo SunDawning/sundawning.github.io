c89247049560922a56aafc5f75a13b67362b131e
jobsimi <jobsimi@qq.com>
2022-10-18 18:00:40 +0800

    Header居中

--- projects/node/npm/jotai/hello/src/pages/components/Layout/components/Header/index.tsx
+++ projects/node/npm/jotai/hello/src/pages/components/Layout/components/Header/index.tsx
@@ -1,11 +1,16 @@
-import { Layout, Input, Space } from 'antd';
+import styles from './index.less';
+import { Layout, Input, Space, Button } from 'antd';
 const { Header } = Layout;
 const { Search } = Input;
 export default function IndexPage() {
   return (
     <Header>
-      <Space>
-        <Search></Search>
+      <Space
+        style={{ width: '100%', height: '100%', justifyContent: 'center' }}
+        className={styles.space}
+      >
+        <Search style={{ width: '100%' }} size="large" width="100%"></Search>
+        <Button>属性</Button>
       </Space>
     </Header>
   );

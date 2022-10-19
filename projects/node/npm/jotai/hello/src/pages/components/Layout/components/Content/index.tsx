41c6fbf251ab5fd69dc0b7e53549eb14f41a4080
jobsimi <jobsimi@qq.com>
2022-10-19 10:40:54 +0800

    使用Descriptions组件展示数据

--- projects/node/npm/jotai/hello/src/pages/components/Layout/components/Content/index.tsx
+++ projects/node/npm/jotai/hello/src/pages/components/Layout/components/Content/index.tsx
@@ -1,18 +1,22 @@
-import { Layout } from 'antd';
+import { Layout, Descriptions } from 'antd';
 import { useAtom } from 'jotai';
 import {
   search_keyword_atom,
   search_keywords_atom,
 } from '../Header/components/Search';
 const { Content } = Layout;
+const { Item } = Descriptions;
 export default function IndexPage() {
   const [search_keyword_atom_value] = useAtom(search_keyword_atom);
   const [search_keywords_atom_value] = useAtom(search_keywords_atom);
   return (
     <Content style={{ minHeight: '480px' }}>
-      搜索的关键字：{search_keyword_atom_value}{' '}
-      所有搜索的关键字Atom，在localStorage里存储。
-      {JSON.stringify(search_keywords_atom_value)}
+      <Descriptions column={1}>
+        <Item label="搜索的关键字">{search_keyword_atom_value}</Item>
+        <Item label="所有搜索的关键字Atom，在localStorage里存储">
+          {JSON.stringify(search_keywords_atom_value)}
+        </Item>
+      </Descriptions>
     </Content>
   );
 }

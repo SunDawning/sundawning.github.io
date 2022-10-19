924ceba009dd162708e15f2c0f70ae05b12fb91a
jobsimi <jobsimi@qq.com>
2022-10-19 09:42:25 +0800

    功能Buttons组件里使用icons图标

--- projects/node/npm/jotai/hello/src/pages/components/Layout/components/Header/components/Buttons/index.tsx
+++ projects/node/npm/jotai/hello/src/pages/components/Layout/components/Header/components/Buttons/index.tsx
@@ -1,8 +1,10 @@
 import { Space, Button } from 'antd';
+import { SettingOutlined, LogoutOutlined } from '@ant-design/icons';
 export default function IndexPage() {
   return (
     <Space style={{ width: '100%', height: '100%', justifyContent: 'center' }}>
-      <Button>属性</Button>
+      <Button title="设置" icon={<SettingOutlined></SettingOutlined>}></Button>
+      <Button title="注销" icon={<LogoutOutlined></LogoutOutlined>}></Button>
     </Space>
   );
 }

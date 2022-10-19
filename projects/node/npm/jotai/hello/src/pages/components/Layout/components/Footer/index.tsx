f3d4cfb42ee955362cfb505ce9eea2ba22b97d2a
jobsimi <jobsimi@qq.com>
2022-10-19 09:31:30 +0800

    调整底部版权居中

--- projects/node/npm/jotai/hello/src/pages/components/Layout/components/Footer/index.tsx
+++ projects/node/npm/jotai/hello/src/pages/components/Layout/components/Footer/index.tsx
@@ -2,11 +2,9 @@ import { Layout, Space } from 'antd';
 const { Footer } = Layout;
 export default function IndexPage() {
   return (
-    <Footer>
-      <Space>
-        Copyright © 2022 SunDawning
-        <a href="mailto:jobsimi@qq.com">jobsimi@qq.com</a>, All Rights Reserved
-      </Space>
+    <Footer style={{ textAlign: 'center' }}>
+      Copyright © 2022 SunDawning
+      <a href="mailto:jobsimi@qq.com"> jobsimi@qq.com</a>, All Rights Reserved
     </Footer>
   );
 }

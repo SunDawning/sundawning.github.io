45b8a72de736eedc2af4fc0216bf1dfac6153322
jobsimi <jobsimi@qq.com>
2022-10-19 09:45:11 +0800

    用户管理

--- projects/node/npm/jotai/hello/src/pages/components/Layout/components/Header/components/Buttons/index.tsx
+++ projects/node/npm/jotai/hello/src/pages/components/Layout/components/Header/components/Buttons/index.tsx
@@ -1,10 +1,13 @@
 import { Space, Button } from 'antd';
-import { SettingOutlined, LogoutOutlined } from '@ant-design/icons';
+import { SettingOutlined, UserOutlined } from '@ant-design/icons';
 export default function IndexPage() {
   return (
     <Space style={{ width: '100%', height: '100%', justifyContent: 'center' }}>
-      <Button title="设置" icon={<SettingOutlined></SettingOutlined>}></Button>
-      <Button title="注销" icon={<LogoutOutlined></LogoutOutlined>}></Button>
+      <Button
+        title="基本设置"
+        icon={<SettingOutlined></SettingOutlined>}
+      ></Button>
+      <Button title="用户管理" icon={<UserOutlined></UserOutlined>}></Button>
     </Space>
   );
 }

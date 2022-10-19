ec17d19a7a059b1be59617eb7f56d3959bdb8ce3
jobsimi <jobsimi@qq.com>
2022-10-19 10:18:11 +0800

    使用jotai创建search_keyword_atom搜索的关键字Atom

--- projects/node/npm/jotai/hello/src/pages/components/Layout/components/Content/index.tsx
+++ projects/node/npm/jotai/hello/src/pages/components/Layout/components/Content/index.tsx
@@ -1,5 +1,10 @@
 import { Layout } from 'antd';
+import { useAtom } from 'jotai';
+import { search_keyword_atom } from '../Header/components/Search';
 const { Content } = Layout;
 export default function IndexPage() {
-  return <Content style={{ minHeight: '480px' }}></Content>;
+  const [keyword, setKeyword] = useAtom(search_keyword_atom);
+  return (
+    <Content style={{ minHeight: '480px' }}>搜索的关键字：{keyword}</Content>
+  );
 }
--- projects/node/npm/jotai/hello/src/pages/components/Layout/components/Header/components/Search/index.tsx
+++ projects/node/npm/jotai/hello/src/pages/components/Layout/components/Header/components/Search/index.tsx
@@ -1,17 +1,21 @@
+import { atom, useAtom } from 'jotai';
 import { Input, AutoComplete } from 'antd';
 import { useState } from 'react';
 const { Search } = Input;
+export const search_keyword_atom = atom(''); // 搜索的关键字Atom
 export default function IndexPage() {
   const [keywords, setKeywords] = useState([]); // 存储所搜索的关键字
+  const [keyword, setKeyword] = useAtom(search_keyword_atom);
   // 每当搜索时，去重存储关键字。
   function onSearch(value, event) {
     if (value === '') {
       return;
     }
+    setKeyword(value); // 更新全局搜索关键字
     if (keywords.includes(value) === true) {
       return;
     }
-    setKeywords([...keywords, value]);
+    setKeywords([...keywords, value]); // 存储所搜索的关键字
   }
   // 自动补全时过滤关键字
   function filterOption(inputValue, option) {

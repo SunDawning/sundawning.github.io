e3ff74375755a94b293d20b89953d587d0e4d2bb
jobsimi <jobsimi@qq.com>
2022-10-19 10:25:51 +0800

    search_keyword_atom对应[search_keyword_atom_value, set_search_keyword_atom_value]

--- projects/node/npm/jotai/hello/src/pages/components/Layout/components/Header/components/Search/index.tsx
+++ projects/node/npm/jotai/hello/src/pages/components/Layout/components/Header/components/Search/index.tsx
@@ -5,13 +5,14 @@ const { Search } = Input;
 export const search_keyword_atom = atom(''); // 搜索的关键字Atom
 export default function IndexPage() {
   const [keywords, setKeywords] = useState([]); // 存储所搜索的关键字
-  const [keyword, setKeyword] = useAtom(search_keyword_atom);
+  const [search_keyword_atom_value, set_search_keyword_atom_value] =
+    useAtom(search_keyword_atom);
   // 每当搜索时，去重存储关键字。
   function onSearch(value, event) {
+    set_search_keyword_atom_value(value); // 更新全局搜索关键字
     if (value === '') {
       return;
     }
-    setKeyword(value); // 更新全局搜索关键字
     if (keywords.includes(value) === true) {
       return;
     }

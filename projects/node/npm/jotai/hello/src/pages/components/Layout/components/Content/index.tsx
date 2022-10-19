c9a0c68c07ab32c039030ad4910612ddd239f2b1
jobsimi <jobsimi@qq.com>
2022-10-19 10:37:11 +0800

    Content组件展示search_keywords_atom的值

--- projects/node/npm/jotai/hello/src/pages/components/Layout/components/Content/index.tsx
+++ projects/node/npm/jotai/hello/src/pages/components/Layout/components/Content/index.tsx
@@ -1,10 +1,18 @@
 import { Layout } from 'antd';
 import { useAtom } from 'jotai';
-import { search_keyword_atom } from '../Header/components/Search';
+import {
+  search_keyword_atom,
+  search_keywords_atom,
+} from '../Header/components/Search';
 const { Content } = Layout;
 export default function IndexPage() {
-  const [keyword, setKeyword] = useAtom(search_keyword_atom);
+  const [search_keyword_atom_value] = useAtom(search_keyword_atom);
+  const [search_keywords_atom_value] = useAtom(search_keywords_atom);
   return (
-    <Content style={{ minHeight: '480px' }}>搜索的关键字：{keyword}</Content>
+    <Content style={{ minHeight: '480px' }}>
+      搜索的关键字：{search_keyword_atom_value}{' '}
+      所有搜索的关键字Atom，在localStorage里存储。
+      {JSON.stringify(search_keywords_atom_value)}
+    </Content>
   );
 }
